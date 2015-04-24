9d698e0c8a86847a948ff5cdd85e8dfa7e260008
Emanuel Jöbstl <emiswelt@gmail.com>
2015-04-24 19:26:41 +0200

    Fixed syntax errors. Set standard to c++11.

--- tests/c7a/test1.cpp
+++ tests/c7a/test1.cpp
@@ -5,6 +5,9 @@
  ******************************************************************************/
 
 #include <c7a/api/dia.hpp>
+#include <c7a/communication/communication_manager.hpp>
+#include <c7a/communication/flow_control_channel.hpp>
+#include <c7a/communication/system_control_channel.hpp>
 
 int main()
 {
