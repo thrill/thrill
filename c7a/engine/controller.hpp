24e7b11b037b12c1f1161be4dfd2951179a1701f
Robert Hangu <robert.hangu@gmail.com>
2015-04-24 21:31:34 +0200

    Start execution engine

--- c7a/engine/controller.hpp
+++ c7a/engine/controller.hpp
@@ -6,15 +6,50 @@
 #ifndef C7A_ENGINE_CONTROLLER_HEADER
 #define C7A_ENGINE_CONTROLLER_HEADER
 
+#include <vector>
+
 namespace c7a {
 namespace engine {
 
 //todo make it virtual!
+template <typename T>
 class Controller
 {
 public:
-    void foo();
-    //todo take exec graph and execute it
+    Controller(int id, const std::vector<int> &otherWorkers) : 
+        _id(id), _otherWorkers(otherWorkers);
+
+    // First reduce with reduce() function
+    //
+    // Shuffle
+    //
+    // Second reduce with reduce() function
+
+    void shuffle() {
+        // 
+    }
+
+//    template <typename K, V>
+    // This is the final reduce
+    T reduce(std::function<T(T, T)> ReduceFn) {
+        T res;
+        // Iterate over 
+        for (auto it = _data.getIt(); it.hasNext(); it = it.next())
+            res = ReduceFn(*it, res);
+        // Alternativ<F3>
+        for (auto x : _data)
+            res = ReduceFn(x, res);
+        
+    }
+
+    void populateData(std::vector<std::pair<K, V>>) {
+        
+    }
+private:
+    int _id;
+    // The worker needs to know the ids of all other workers
+    std::vector<int> _otherWorkers;
+    std::vector<std::pair<K, V>> _data;
 };
 
 class MasterController : public Controller
