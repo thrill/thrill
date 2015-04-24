7b91b8016aec2e7f5c3520eba50dcb119c6bbdb9
Tobias Sturm <tobstu@gmail.com>
2015-04-24 21:08:02 +0200

    Implement Block Iterator
    data manager returns block iterators to access DIAs that are in mem
    future: remote access / external memory access
    
    write tests for thos buggers
    
    update test

---
+++ tests/data/test_serializer.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "c7a/data/serializer.hpp"
+
+using namespace c7a::data;
+
+TEST(Serializer, StringSerializeDeserialize) {
+    ASSERT_EQ("foo", Deserialize<std::string>(Serialize<std::string>("foo")));
+}
+
+TEST(Serializer, IntSerializeDeserialize) {
+    ASSERT_EQ(-123, Deserialize<int>(Serialize<int>(-123)));
+}
+
+TEST(Serializer, StringIntPairSerializeDeserialize) {
+    auto t = std::make_pair("foo", 123);
+    auto serialized = Serialize<std::pair<std::string, int>>(t);
+    auto result = Deserialize<std::pair<std::string, int>>(serialized);
+    ASSERT_EQ(std::get<0>(t), std::get<0>(result));
+    ASSERT_EQ(std::get<1>(t), std::get<1>(result));
+}
