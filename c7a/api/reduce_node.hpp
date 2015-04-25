5e7e1617e34e767e0d50b09bf2399b6dcd56769b
Sebastian Lamm <seba.lamm@googlemail.com>
2015-04-25 21:25:58 +0200

    LOp chaining.

--- c7a/api/reduce_node.hpp
+++ c7a/api/reduce_node.hpp
@@ -11,28 +11,61 @@
 
 namespace c7a {
 
-template <typename T, typename KeyExtractor, typename ReduceFunction>
+template <typename T, typename L, typename KeyExtractor, typename ReduceFunction>
 class ReduceNode : public DOpNode<T> {
 //! Hash elements of the current DIA onto buckets and reduce each bucket to a single value.
 public: 
     ReduceNode(const std::vector<DIABase*>& parents,
+               L lambda,
                KeyExtractor key_extractor,
                ReduceFunction reduce_function)
         : DOpNode<T>(parents),
+        local_lambda_(lambda),
         key_extractor_(key_extractor),
-        reduce_function_(reduce_function) {};
+        reduce_function_(reduce_function) { };
 
-    std::string ToString() override
-    {
+    std::string ToString() override {
         using key_t = typename FunctionTraits<KeyExtractor>::result_type;
         std::string str = std::string("[ReduceNode/Type=[") + typeid(T).name() + "]/KeyType=[" + typeid(key_t).name() + "]";
         return str;
     }
 
+    void execute() override {
+        PreOp(T());
+        MainOp();
+    }
+
+    auto get_pre_op() {
+        return [=](T t) {
+            return PreOp(t);
+        };
+    }
+
+    auto get_post_op() {
+        return [=](T t) {
+            return PostOp(t);
+        };
+    }
 
 private: 
+    L local_lambda_;
     KeyExtractor key_extractor_;
     ReduceFunction reduce_function_;
+    
+    auto PreOp(T t) {
+        local_lambda_(t);
+        std::cout << "PreOp" << std::endl;
+        return t;
+    }
+
+    auto MainOp() {
+        std::cout << "MainOp" << std::endl;
+    }
+
+    auto PostOp(T t) {
+        std::cout << "PostOp" << std::endl;
+        return t;
+    }
 };
 
 } // namespace c7a
