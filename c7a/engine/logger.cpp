691e740d064b25e44baad457b0be97bfc026bf48
Matthias Stumpp <mstumpp@gmail.com>
2015-04-25 20:48:27 +0200

    further work on worker / reduce operation

---
+++ c7a/engine/logger.cpp
@@ -0,0 +1,24 @@
+#include <stdexcept>
+#include "Logger.hpp"
+
+using namespace std;
+
+Logger* Logger::pInstance = nullptr;
+
+mutex Logger::sMutex;
+
+Logger& Logger::instance()
+{
+    lock_guard<mutex> guard(sMutex);
+    if (pInstance == nullptr)
+        pInstance = new Logger();
+    return *pInstance;
+}
+
+Logger::Logger() { }
+
+void Logger::log(const string& inMessage)
+{
+    lock_guard<mutex> guard(sMutex);
+    std::cout << inMessage << std::endl;
+}
--- c7a/engine/mock-network.hpp
+++ c7a/engine/mock-network.hpp
@@ -9,105 +9,102 @@
 #include <deque>
 
 namespace c7a {
+namespace engine {
 
-class MockNetwork {
-public:
+    class MockNetwork {
+    public:
 
-    //! Register a valid receiver
-    void add_client(size_t id)
-    {
-        if (clients_.size() < id+1) {
-            valid_client_.resize(id+1);
-            clients_.resize(id+1);
+        //! Register a valid receiver
+        void add_client(size_t id) {
+            if (clients_.size() < id + 1) {
+                valid_client_.resize(id + 1);
+                clients_.resize(id + 1);
+            }
+
+            valid_client_[id] = true;
         }
 
-        valid_client_[id] = true;
-    }
+        //! "Send" a message to id.
+        void sendToWorker(size_t from_id, size_t dest_id, const std::string &data) {
+            //assert(dest_id < valid_client_.size() && valid_client_[dest_id]);
 
-    //! "Send" a message to id.
-    void sendToWorker(size_t from_id, size_t dest_id, const std::string& data)
-    {
-        assert(dest_id < valid_client_.size() && valid_client_[dest_id]);
-
-        std::unique_lock<std::mutex> lock(mutex_);
-        assert(dest_id < clients_.size());
-        clients_[dest_id].push_back(std::make_tuple(from_id, data));
-    }
-
-    //! Receive any message for for_id, deliver sender_id (source id) and data.
-    bool receiveFromAny(size_t for_id, size_t* sender_id, std::string* out_data)
-    {
-        assert(for_id < valid_client_.size() && valid_client_[for_id]);
-
-        // TODO(tb): this is an evil busy waiting loop
-        do {
             std::unique_lock<std::mutex> lock(mutex_);
+            //assert(dest_id < clients_.size());
+            clients_[dest_id].push_back(std::make_tuple(from_id, data));
+        }
 
-            if (clients_[for_id].size()) {
-                std::tie(*sender_id, *out_data) = clients_[for_id].front();
-                clients_[for_id].pop_front();
-                break;
-            }
-        } while (1);
+        //! Receive any message for for_id, deliver sender_id (source id) and data.
+        bool receiveFromAny(size_t for_id, size_t *sender_id, std::string *out_data) {
+            //assert(for_id < valid_client_.size() && valid_client_[for_id]);
 
-        return true;
-    }
+            // TODO(tb): this is an evil busy waiting loop
+            do {
+                std::unique_lock<std::mutex> lock(mutex_);
 
-private:
-    //! Mutex to lock access to message queues
-    std::mutex mutex_;
+                if (clients_[for_id].size()) {
+                    std::tie(*sender_id, *out_data) = clients_[for_id].front();
+                    clients_[for_id].pop_front();
+                    break;
+                }
+            } while (1);
 
-    //! vector of valid client ids (use add_client to set to valid)
-    std::vector<char> valid_client_;
+            return true;
+        }
 
-    //! type of message queue items
-    typedef std::tuple<size_t, std::string> DataPair;
+    private:
+        //! Mutex to lock access to message queues
+        std::mutex mutex_;
 
-    //! type of message queue
-    typedef std::deque<DataPair> DataVector;
+        //! vector of valid client ids (use add_client to set to valid)
+        std::vector<char> valid_client_;
 
-    //! message queue for each of the registered clients
-    std::vector<DataVector> clients_;
-};
+        //! type of message queue items
+        typedef std::tuple<size_t, std::string> DataPair;
 
-class MockSelect
-{
-public:
-    MockSelect(MockNetwork& network, size_t my_id)
-        : network_(network),
-          my_id_(my_id)
-    {
-        network_.add_client(my_id);
-    }
+        //! type of message queue
+        typedef std::deque<DataPair> DataVector;
 
-    void sendToWorkerString(size_t dest_id, const std::string& data) {
-        network_.sendToWorker(my_id_, dest_id, data);
-    }
+        //! message queue for each of the registered clients
+        std::vector<DataVector> clients_;
+    };
 
-    template<typename T>
-    void sendToWorker(size_t dest_id, const T& data) {
-        // TODO(ms): use serialize and call sendToWorkerString
-        abort();
-    }
+    class MockSelect {
+    public:
+        MockSelect(MockNetwork &network, size_t my_id)
+                : network_(network),
+                  my_id_(my_id) {
+            network_.add_client(my_id);
+        }
 
-    bool receiveFromAnyString(size_t* sender_id, std::string* out_data) {
-        return network_.receiveFromAny(my_id_, sender_id, out_data);
-    }
+        void sendToWorkerString(size_t dest_id, const std::string &data) {
+            network_.sendToWorker(my_id_, dest_id, data);
+        }
 
-    template<typename T>
-    bool receiveFromAny(size_t* sender_id, T* out_data) {
-        // TODO(ms): use serialize and call receiveFromAny
-        abort();
-    }
+        template<typename T>
+        void sendToWorker(size_t dest_id, const T &data) {
+            // TODO(ms): use serialize and call sendToWorkerString
+            abort();
+        }
 
-protected:
-    //! reference to common network.
-    MockNetwork& network_;
+        bool receiveFromAnyString(size_t *sender_id, std::string *out_data) {
+            return network_.receiveFromAny(my_id_, sender_id, out_data);
+        }
 
-    //! my client id in network.
-    size_t my_id_;
-};
+        template<typename T>
+        bool receiveFromAny(size_t *sender_id, T *out_data) {
+            // TODO(ms): use serialize and call receiveFromAny
+            abort();
+        }
 
+    protected:
+        //! reference to common network.
+        MockNetwork &network_;
+
+        //! my client id in network.
+        size_t my_id_;
+    };
+
+} // engine
 } // namespace c7a
 
 #endif //C7A_DUMMY_COMMUNICATOR_HPP
