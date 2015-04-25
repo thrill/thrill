a900ca3b07a744aad19c61e54ed433e8ca2c74cf
Alexander Noe <aleexnoe@gmail.com>
2015-04-25 09:15:27 +0200

    ADAC - Merge to Hell

--- c7a/api/reduce_node.hpp
+++ c7a/api/reduce_node.hpp
@@ -1,1 +1,25 @@
-M-x 
+/*******************************************************************************
+ * c7a/api/dop_node.hpp
+ *
+ * Model real-time or backtesting Portfolio with Positions, TradeLog and more.
+ ******************************************************************************/
+
+#ifndef C7A_API_REDUCE_NODE_HEADER
+#define C7A_API_REDUCE_NODE_HEADER
+
+
+
+template <typename T, typename KeyExtractor, typename ReduceFunction>
+class ReduceNode : public DIANode<T> {
+//! Hash elements of the current DIA onto buckets and reduce each bucket to a single value.
+   public: 
+    ReduceNode(std::vector<DIABase*> parents, KeyExtractor key_extractor, ReduceFunction reduce_function) : DIANode<T>::parents_(parents), key_extractor_(key_extractor), reduce_function_(reduce_function) {};
+
+private: 
+    KeyExtractor key_extractor_;
+    ReduceFunction reduce_function_;
+};
+
+#endif // !C7A_API_REDUCE_NODE_HEADER
+
+/******************************************************************************/
