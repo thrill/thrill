0a6c24e914f6e16aac6e3df0dcf96b3da0d5c6c5
Tobias Sturm <mail@tobiassturm.de>
2015-04-25 09:42:13 +0200

    Update BlockIterator internals
    to allow iterators to consume elements that have been pushed *after*
    the iterator was created, I use a reference to the vector + position

--- tests/data/test_data_manager.cpp
+++ tests/data/test_data_manager.cpp
@@ -12,3 +12,34 @@ TEST(DataManager, GetLocalEmitter_FailsIfNotFound) {
     DataManager manager;
     ASSERT_ANY_THROW(manager.GetLocalEmitter<int>(0));
 }
+
+TEST(DataManager, GetLocalEmitter_CanCallEmitter) {
+    DataManager manager;
+    auto e = manager.GetLocalEmitter<int>(manager.AllocateDIA());
+    ASSERT_NO_THROW(e(123));
+}
+
+TEST(DataManager, EmittAndIterate_CorrectOrder) {
+    DataManager manager;
+    auto id = manager.AllocateDIA();
+    auto emitFn = manager.GetLocalEmitter<int>(id);
+    emitFn(123);
+    emitFn(22);
+    auto it = manager.GetLocalBlocks<int>(id);
+    ASSERT_EQ(123, it.Next());
+    ASSERT_EQ(22, it.Next());
+}
+
+TEST(DataManager, EmittAndIterate_ConcurrentAccess) {
+    DataManager manager;
+    auto id = manager.AllocateDIA();
+    auto emitFn = manager.GetLocalEmitter<int>(id);
+    auto it = manager.GetLocalBlocks<int>(id);
+    emitFn(123);
+    ASSERT_EQ(123, it.Next());
+    ASSERT_FALSE(it.HasNext());
+
+    emitFn(22);
+    ASSERT_TRUE(it.HasNext());
+    ASSERT_EQ(22, it.Next());
+}
