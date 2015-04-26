dd43ab4d33613294805a4b0bf0dec851b757292d
Tobias Sturm <mail@tobiassturm.de>
2015-04-26 17:49:48 +0200

    start working on channel multiplexer

---
+++ tests/net/test-channel-multiplexer.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * tests/communication/test_net_dispatcher.cpp
+ *
+ * Part of Project c7a.
+ *
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#include "gtest/gtest.h"
+#include <c7a/net/channel-multiplexer.hpp>
+
+using namespace c7a::net;
+
+TEST(ChannelMultiplexer, CompileTest) {
+    ChannelMultiplexer candidate();
+    ASSERT_TRUE(true);
+}
+
