da39eb2cbf23c0c194506e0e365bf47d974daa58
Matthias Stumpp <mstumpp@gmail.com>
2015-04-26 11:21:40 +0200

    further work on reduce operation

--- examples/worker_main.cpp
+++ examples/worker_main.cpp
@@ -2,6 +2,8 @@
 #include "../c7a/engine/worker.hpp"
 #include "../c7a/engine/mock-network.hpp"
 
+static const int num_workers = 5;
+
 std::vector<int> remove(std::vector<int> v, int i) {
     std::vector<int> newv(v);
     auto it = std::find(newv.begin(), newv.end(), i);
@@ -24,13 +26,14 @@ int main()
 {
     c7a::engine::MockNetwork net;
     std::vector<int> workers = {0, 1, 2, 3, 4};
+
     std::vector<std::string> words = {"word0", "word1", "word2", "word3", "word4", "word4"};
 
-    c7a::engine::Worker w0(workers.at(0), remove(workers, workers.at(0)), net);
-    c7a::engine::Worker w1(workers.at(1), remove(workers, workers.at(1)), net);
-    c7a::engine::Worker w2(workers.at(2), remove(workers, workers.at(2)), net);
-    c7a::engine::Worker w3(workers.at(3), remove(workers, workers.at(3)), net);
-    c7a::engine::Worker w4(workers.at(4), remove(workers, workers.at(4)), net);
+    c7a::engine::Worker w0(0, 5, net);
+    c7a::engine::Worker w1(1, 5, net);
+    c7a::engine::Worker w2(2, 5, net);
+    c7a::engine::Worker w3(3, 5, net);
+    c7a::engine::Worker w4(4, 5, net);
     std::thread t0([&]{ w0.reduce<std::string, int>(words); });
     std::thread t1([&]{ w1.reduce<std::string, int>(words); });
     std::thread t2([&]{ w2.reduce<std::string, int>(words); });
@@ -42,24 +45,24 @@ int main()
     t3.join();
     t4.join();
 
-    /*
-    std::vector<c7a::engine::Worker> workerObjs;
+    /*std::vector<c7a::engine::Worker> workerObjs;
     std::vector<std::thread*> threads;
 
-    for (int n=0; n<workers.size(); n++) {
-        std::cout << workers.at(n) << std::endl;
-        workerObjs.push_back(c7a::engine::Worker(workers.at(n), remove(workers, workers.at(n)), net));
-        workerObjs.at(n).print();
+    for (int n=0; n<num_workers; n++) {
+        c7a::engine::Worker worker(n, num_workers, net);
+        workerObjs.push_back(worker);
 
-        std::cout << "reduce" << std::endl;
         threads[n] = new std::thread([&]{
-            std::cout << "reduce in" << std::endl;
-            workerObjs.at(n).print();
-            //workerObjs.at(n).reduce<std::string, int>(words);
+            worker.reduce<std::string, int>(words);
         });
+
+        std::thread *t = threads[n];
+        t->join();
+
+        std::cout << "reduce 1" << std::endl;
     }
 
-    for (auto& th : threads) { th->join(); };*/
+    //for (auto& th : threads) { th->join(); };*/
 
     return 0;
 }
