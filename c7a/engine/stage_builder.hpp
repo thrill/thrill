325ede0377dbc3f8ae0510b4605b548427c04ab9
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-04-26 18:32:54 +0200

    make find stages better

--- c7a/engine/stage_builder.hpp
+++ c7a/engine/stage_builder.hpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <utility>
 #include <algorithm>
+#include <set>
 
 namespace c7a { 
 namespace engine {
@@ -35,20 +36,23 @@ private:
 // Returns a list of stages of graph scope
 void  FindStages(DIABase* action, std::vector<Stage> & stages_result) {
     SpacingLogger(true) << "FINDING stages:";
-
+    std::set<DIABase*> stages_found;
     // GOAL: Returns a vector with stages
     // TEMP SOLUTION: Every node is a stage
     std::stack<DIABase*> dia_stack;
     dia_stack.push(action);
+    stages_found.insert(action);
     while (!dia_stack.empty()) {
         DIABase* curr = dia_stack.top();
-        // SpacingLogger(true) << curr;
         dia_stack.pop();
         stages_result.emplace_back(Stage(curr));
         std::vector<DIABase*> parents = curr->get_parents();
         for (DIABase* p : parents) {
-            // SpacingLogger(true) << "PTR IS" << p; 
-            if (p) dia_stack.push(p);
+
+            if (p && (stages_found.find(p) == stages_found.end())) {
+                dia_stack.push(p);
+                stages_found.insert(p);
+            }
             else SpacingLogger(true) << "OMG NULLPTR";
         }
     }
