8550f426b0a3fb2f270bbb8507bc38c9576e7701
Sebastian Lamm <seba.lamm@googlemail.com>
2015-04-26 20:38:59 +0200

    Improved test case.

--- tests/api/test_simple_api.cpp
+++ tests/api/test_simple_api.cpp
@@ -45,14 +45,7 @@ TEST(DISABLED_DIASimple, InputTest1Write) {
 }
 
 TEST(DIASimple, ReduceStringEquality) {
-
-    using c7a::DIA;
-    using c7a::Context;
     using c7a::FunctionStack;
-
-    // auto doubles = Context().ReadFromFileSystem("tests/inputs/test1", [](std::string line) {
-    //         return std::stod(line);
-    //     });
 
     auto key_ex = [](double in) { return (int) in; };
     auto red_fn = [](double in1, double in2) { return in1 + in2; };
@@ -60,56 +53,19 @@ TEST(DIASimple, ReduceStringEquality) {
             std::cout << "Map" << std::endl;
             return input;
         };
-    // auto map2_fn = [](double input) {
-    //         std::cout << "Map2" << std::endl;
-    //         return input;
-    //     };
     auto fmap_fn = [](double input, std::function<void(double)> emit_func) {
-            std::cout << "FlatMap" << std::endl;
+            std::cout << "FlatMap(Double)" << std::endl;
             emit_func(input);
             emit_func(input);
         };
-
-    // auto duplicates = doubles.Map(map_fn);
-    // auto duplicates2 = duplicates.Map(map_fn);
-    // auto red_duplicates = duplicates2.Reduce(key_ex, red_fn);
-    // auto red_duplicates2 = duplicates.Reduce(key_ex, red_fn);
-
 
     std::cout << "==============" << std::endl;
     std::cout << "FunctionStack" << std::endl;
     std::cout << "==============" << std::endl;
     FunctionStack<> stack;
-    auto new_stack = stack.push(
-        [](int i, std::function<void(double)> emit_func) {
-            std::cout << "hello1" << std::endl;
-            emit_func(i + 1);
-        });
-    auto new_stack2 = new_stack.push(
-        [](int i, std::function<void(double)> emit_func){
-            std::cout << "hello2" << std::endl;
-            emit_func(i + 1);
-        });
-    //auto new_stack2 = new_stack.push(map2_fn);
-    //auto pair = new_stack2.pop();
+    auto new_stack = stack.push(fmap_fn);
+    auto new_stack2 = new_stack.push(fmap_fn);
     auto composed_function = new_stack2.emit();
     composed_function(42);
     return;
-
-    // std::cout << "==============" << std::endl;
-    // std::cout << "Tree" << std::endl;
-    // std::cout << "==============" << std::endl;
-    // duplicates.PrintNodes();
-    // std::cout << std::endl;
-
-    // std::cout << "==============" << std::endl;
-    // std::cout << "Execution" << std::endl;
-    // std::cout << "==============" << std::endl;
-    // std::cout << "First Reduce:" << std::endl;
-    // (red_duplicates.get_node())->execute();
-    // std::cout << std::endl;
-    // std::cout << "Second Reduce:" << std::endl;
-    // (red_duplicates2.get_node())->execute();
-    // auto duplicates3 = red_duplicates.Map(map_fn);
-    // auto red_duplicates2 = duplicates3.Reduce(key_ex, red_fn);
 }
