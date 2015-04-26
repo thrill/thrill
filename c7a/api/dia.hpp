252efc7ba441ae961e3f2c8cd1519671d9b6266e
Sebastian Lamm <seba.lamm@googlemail.com>
2015-04-26 12:10:56 +0200

    Fixed network for OS X.

--- c7a/api/dia.hpp
+++ c7a/api/dia.hpp
@@ -124,7 +124,6 @@ public:
             for (int i = 0; i < depth - 1; ++i) {
                 std::cout << "│   ";
             }
-
             if (is_end && depth > 0) std::cout << "└── ";
             else if (depth > 0) std::cout << "├── ";
             std::cout << node->ToString() << std::endl;
