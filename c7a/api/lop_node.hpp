1b252d064f5d6bcbe6c16eb411135c410c876d33
Sebastian Lamm <seba.lamm@googlemail.com>
2015-04-26 11:44:04 +0200

    Cleaning up.

--- c7a/api/lop_node.hpp
+++ c7a/api/lop_node.hpp
@@ -14,15 +14,22 @@ namespace c7a {
 template <typename T, typename LOpFunction>
 class LOpNode : public DIANode<T> {
 public:
-
-    LOpNode(data::DataManager &data_manager, const std::vector<DIABase*>& parents, LOpFunction lop_function) : DIANode<T>(data_manager, parents), lop_function_(lop_function) {};
+    LOpNode(data::DataManager &data_manager, 
+            const std::vector<DIABase*>& parents, 
+            LOpFunction lop_function) 
+        : DIANode<T>(data_manager, parents), 
+        lop_function_(lop_function) {};
     virtual ~LOpNode() {}
 
     void execute() {};
 
     std::string ToString() override {
-        using key_t = typename FunctionTraits<LOpFunction>::result_type;
-        std::string str = std::string("[LOpNode/Type=[") + typeid(T).name() + "]";
+        // Extract type
+        using key_t 
+            = typename FunctionTraits<LOpFunction>::result_type;
+        // Create string
+        std::string str 
+            = std::string("[LOpNode/Type=[") + typeid(T).name() + "]";
         return str;
     }
 
