b5e5e1e75865518ea1938f4295d9223e35910b88
Timo Bingmann <tbgit@panthema.net>
2015-04-28 08:55:41 +0200

    Renaming ExecutionEndpoint to NetEndpoint.

--- c7a/net/net-endpoint.hpp
+++ c7a/net/net-endpoint.hpp
@@ -1,50 +1,48 @@
 /*******************************************************************************
- * c7a/net/execution_endpoint.hpp
+ * c7a/net/net-endpoint.hpp
  *
  * Part of Project c7a.
  *
+ * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
  *
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
 
-#ifndef C7A_NET_EXECUTION_ENDPOINT_HEADER
-#define C7A_NET_EXECUTION_ENDPOINT_HEADER
+#ifndef C7A_NET_NET_ENDPOINT_HEADER
+#define C7A_NET_NET_ENDPOINT_HEADER
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 
 namespace c7a {
 
-class ExecutionEndpoint;
-typedef std::vector<ExecutionEndpoint> ExecutionEndpoints;
-
 /*!
- * Identifies a worker, contains its host and port.
+ * Identifies a remote worker in a NetGroup, currently contains only its host
+ * address. In future, the master/worker coordination classes should use this to
+ * build a NetGroup, or rebuild it after a network failure.
  */
-class ExecutionEndpoint
+class NetEndpoint
 {
 public:
     //store some kind of endpoint information here
-    const unsigned int id;
     const std::string hostport;
 
-    ExecutionEndpoint(unsigned int id, const std::string& hostport)
-        : id(id), hostport(hostport) { }
+    NetEndpoint(const std::string& hostport)
+        : hostport(hostport) { }
 
-    static ExecutionEndpoints ParseEndpointList(std::string str)
+    static std::vector<NetEndpoint> ParseEndpointList(std::string str)
     {
         std::stringstream stream;
         stream << str;
-        ExecutionEndpoints endpoints;
+        std::vector<NetEndpoint> endpoints;
 
         std::string hostport;
-        int workerId = 0;
 
         while (stream >> hostport) {
-            endpoints.push_back(ExecutionEndpoint(workerId, hostport));
-            workerId++;
+            endpoints.push_back(NetEndpoint(hostport));
         }
         return endpoints;
     }
@@ -52,6 +50,6 @@ public:
 
 } // namespace c7a
 
-#endif // !C7A_NET_EXECUTION_ENDPOINT_HEADER
+#endif // !C7A_NET_NET_ENDPOINT_HEADER
 
 /******************************************************************************/
