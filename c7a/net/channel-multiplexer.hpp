a518ccbfd25f540f5834a768636374eebd333448
Tobias Sturm <tobstu@gmail.com>
2015-04-30 17:12:58 +0200

    uncrustify channel-multiplexer.hpp

--- c7a/net/channel-multiplexer.hpp
+++ c7a/net/channel-multiplexer.hpp
@@ -8,9 +8,7 @@
 
 namespace c7a {
 namespace net {
-
 #define BUFFER_SIZE 1024
-
 
 //! Block header is sent before a sequence of blocsk
 //! it indicates the number of elements and their
@@ -22,8 +20,9 @@ struct BlockHeader {
     size_t num_elements;
     size_t boundaries[];
 
-    template<class SocketType>
-    bool ReadFromSocket(SocketType& socket) {
+    template <class SocketType>
+    bool ReadFromSocket(SocketType& socket)
+    {
         int flags = 0;
 
         //how many elements are in the header & which channel number?
@@ -57,11 +56,12 @@ class ChannelSink;
 //! 3. consume reactivates when 2nd header is read
 //! 4. consume will read elem3
 //! 5. consume will read 0-Header closes stream
-class Stream {
+class Stream
+{
 public:
     Stream(struct BlockHeader header, std::shared_ptr<ChannelSink> channel)
         : channel_(channel),
-          current_head_(header) {}
+          current_head_(header) { }
 
     bool IsActive() { return BytesRemaining() > 0; }
     bool IsClosed() { return current_head_.num_elements == 0; }
@@ -72,65 +72,70 @@ private:
     struct BlockHeader current_head_;
     size_t read_; //# bytes read
     bool closed_;
-    size_t BytesRemaining() {
+    size_t BytesRemaining()
+    {
         return current_head_.boundaries[current_head_.num_elements - 1] - read_;
     }
-
 };
 
 //! Data channel for receiving data from other workers
-class ChannelSink : std::enable_shared_from_this<ChannelSink> {
+class ChannelSink : std::enable_shared_from_this<ChannelSink>
+{
 public:
     //! Creates instance that has num_senders active senders
     //! Writes received data to targetDIA
-    ChannelSink() {
-
-    }
+    ChannelSink() { }
 
     //! Creates a stream in this Channel and returns a reference to it
-    std::shared_ptr<Stream> AddStream(int fd, BlockHeader header) {
+    std::shared_ptr<Stream> AddStream(int fd, BlockHeader header)
+    {
         auto stream_ptr = std::make_shared<Stream>(header, shared_from_this());
         active_streams_.insert(std::make_pair(fd, stream_ptr));
         return stream_ptr;
     }
 
-    std::shared_ptr<Stream> GetStream(int fd) {
+    std::shared_ptr<Stream> GetStream(int fd)
+    {
         if (!HasStreamOn(fd))
             return std::shared_ptr<Stream>();
         return active_streams_[fd];
     }
 
-    bool HasStreamOn(int fd) {
+    bool HasStreamOn(int fd)
+    {
         return active_streams_.find(fd) != active_streams_.end();
     }
 
-    template<class SocketType>
-    void Consume(SocketType& socket) {}
+    template <class SocketType>
+    void Consume(SocketType& socket) { }
 
 private:
-    std::map<int, std::shared_ptr<Stream>> active_streams_;
+    std::map<int, std::shared_ptr<Stream> > active_streams_;
 };
 
 //! Multiplexes virtual Connections on NetDispatcher
-template<class SocketType>
-class ChannelMultiplexer {
+template <class SocketType>
+class ChannelMultiplexer
+{
 public:
     //! Called by the network dispatcher
-    void Consume(SocketType& connection) {
+    void Consume(SocketType& connection)
+    {
         int fd = connection.GetFileDescriptor();
 
         //new socket: -> is a new stream
-        if ( !KnowsSocket(fd) || !HasActiveStream(fd) ) {
-            auto header = ReadBlockHeader( connection );
+        if (!KnowsSocket(fd) || !HasActiveStream(fd)) {
+            auto header = ReadBlockHeader(connection);
             ChannelPtr channel;
-            if ( !HasChannel(header.channel_id) ) {
+            if (!HasChannel(header.channel_id)) {
                 channel = CreateChannel(fd, header.channel_id);
-            } else {
+            }
+            else {
                 channel = channels_[header.channel_id];
             }
 
             StreamPtr stream;
-            if( !channel->HasStreamOn(fd) )
+            if (!channel->HasStreamOn(fd))
                 SetActiveStream(fd, channel->AddStream(fd, header));
             else
                 SetActiveStream(fd, channel->GetStream(fd));
@@ -139,11 +144,13 @@ public:
         GetActiveStream(fd)->GetChannel()->Consume(connection);
     }
 
-    bool HasChannel(int id) {
+    bool HasChannel(int id)
+    {
         return channels_.find(id) != channels_.end();
     }
 
-    std::shared_ptr<ChannelSink> PickupChannel(int id) {
+    std::shared_ptr<ChannelSink> PickupChannel(int id)
+    {
         return channels_[id];
     }
 
@@ -156,38 +163,44 @@ private:
 
     //!Streams per Socket
     std::map<int, StreamPtr> active_stream_on_fd_;
-    std::map<int, std::map<int, ChannelPtr>> channels_on_fd_;
+    std::map<int, std::map<int, ChannelPtr> > channels_on_fd_;
 
-    ChannelPtr CreateChannel(int fd, int channel_id) {
+    ChannelPtr CreateChannel(int fd, int channel_id)
+    {
         //create channel and make accessible via ID
         std::shared_ptr<ChannelSink> channel;
-        channels_.insert( std::make_pair(channel_id, channel) );
+        channels_.insert(std::make_pair(channel_id, channel));
 
         //associate channel with fd
         assert(KnowsSocket(fd));
-        channels_on_fd_[fd].insert( std::make_pair(channel_id, channel) );
+        channels_on_fd_[fd].insert(std::make_pair(channel_id, channel));
         return channel;
     }
 
-    void SetActiveStream(int fd, StreamPtr stream) {
+    void SetActiveStream(int fd, StreamPtr stream)
+    {
         assert(KnowsSocket(fd));
         active_stream_on_fd_[fd] = stream;
     }
 
-    bool HasActiveStream(int fd) {
+    bool HasActiveStream(int fd)
+    {
         return GetActiveStream(fd) != nullptr;
     }
 
-    std::shared_ptr<Stream> GetActiveStream(int fd) {
+    std::shared_ptr<Stream> GetActiveStream(int fd)
+    {
         return active_stream_on_fd_[fd];
     }
 
-    bool KnowsSocket(int fd) {
+    bool KnowsSocket(int fd)
+    {
         return channels_on_fd_.find(fd) != channels_on_fd_.end();
     }
 
     //!Reads, creates, and returns the header of a block
-    BlockHeader ReadBlockHeader(SocketType& socket) {
+    BlockHeader ReadBlockHeader(SocketType& socket)
+    {
         //TODO we assume that the header can ALWAYS be read. Handle edge case
         //where this does not happen
 
@@ -199,4 +212,5 @@ private:
         return header;
     }
 };
-}}
+}
+}
