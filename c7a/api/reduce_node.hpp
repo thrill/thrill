6a05eee92985ea184d54975204281174edafee3b
Tobias Sturm <tobstu@gmail.com>
2015-05-05 12:25:25 +0200

    run uncrustify

--- c7a/api/reduce_node.hpp
+++ c7a/api/reduce_node.hpp
@@ -15,13 +15,12 @@
 #include "../common/logger.hpp"
 
 namespace c7a {
-
 //! \addtogroup api Interface
 //! \{
 
 /*!
  * A DIANode which performs a Reduce operation. Reduce groups the elements in a DIA by their
- * key and reduces every key bucket to a single element each. The ReduceNode stores the 
+ * key and reduces every key bucket to a single element each. The ReduceNode stores the
  * key_extractor and the reduce_function UDFs. The chainable LOps ahead of the Reduce operation
  * are stored in the Stack. The ReduceNode has the type T, which is the result type of the
  * reduce_function.
@@ -32,10 +31,9 @@ namespace c7a {
  * \tparam ReduceFunction Type of the reduce_function
  */
 template <typename T, typename Stack, typename KeyExtractor, typename ReduceFunction>
-class ReduceNode : public DOpNode<T> {
-
-public: 
-
+class ReduceNode : public DOpNode<T>
+{
+public:
     /*!
      * Constructor for a ReduceNode. Sets the DataManager, parents, stack, key_extractor and reduce_function.
      *
@@ -45,28 +43,29 @@ public:
      * \param key_extractor Key extractor function
      * \param reduce_function Reduce function
      */
-    ReduceNode(Context & ctx, 
+    ReduceNode(Context& ctx,
                const std::vector<DIABase*>& parents,
                Stack& stack,
                KeyExtractor key_extractor,
                ReduceFunction reduce_function)
         : DOpNode<T>(ctx, parents),
-        local_stack_(stack),
-        key_extractor_(key_extractor),
-        reduce_function_(reduce_function) {
-            // This new DIA Allocate is needed to send data from Pre to Main
-            // TODO: use network iterate later
+          local_stack_(stack),
+          key_extractor_(key_extractor),
+          reduce_function_(reduce_function)
+    {
+        // This new DIA Allocate is needed to send data from Pre to Main
+        // TODO: use network iterate later
         post_data_id_ = (this->context_).get_data_manager().AllocateDIA();
-
-        };
+    }
 
     /*!
      * Returns "[ReduceNode]" as a string.
      * \return "[ReduceNode]"
      */
-    std::string ToString() override {
+    std::string ToString() override
+    {
         // Create string
-        std::string str 
+        std::string str
             = std::string("[ReduceNode]");
         return str;
     }
@@ -74,7 +73,8 @@ public:
     /*!
      * Actually executes the reduce operation. Uses the member functions PreOp, MainOp and PostOp.
      */
-    void execute() override {
+    void execute() override
+    {
         PreOp();
         MainOp();
         PostOp();
@@ -84,21 +84,18 @@ public:
      * TODO: I have no idea...
      */
     auto ProduceStack() {
-        using reduce_t 
-            = typename FunctionTraits<ReduceFunction>::result_type;
+        using reduce_t
+                  = typename FunctionTraits<ReduceFunction>::result_type;
 
-
-        auto id_fn = [=](reduce_t t, std::function<void(reduce_t)> emit_func) {
-            return emit_func(t);
-        };
+        auto id_fn = [ = ](reduce_t t, std::function<void(reduce_t)> emit_func) {
+                         return emit_func(t);
+                     };
 
         FunctionStack<> stack;
         return stack.push(id_fn);
     }
 
-
-private: 
-
+private:
     //! Local stack
     Stack local_stack_;
     //!Key extractor function
@@ -110,7 +107,8 @@ private:
 
     //! Locally hash elements of the current DIA onto buckets and reduce each bucket to a single value,
     //! afterwards send data to another worker given by the shuffle algorithm.
-    void PreOp() {
+    void PreOp()
+    {
         using key_t = typename FunctionTraits<KeyExtractor>::result_type;
         using reduce_arg_t = typename FunctionTraits<ReduceFunction>::template arg<0>;
         std::cout << "PreOp" << std::endl;
@@ -118,29 +116,29 @@ private:
         data::DIAId pid = this->get_parents()[0]->get_data_id();
         // //get data from data manager
         data::BlockIterator<T> it = (this->context_).get_data_manager().template GetLocalBlocks<T>(pid);
-        
+
         // //run local reduce
         using key_t = typename FunctionTraits<KeyExtractor>::result_type;
         std::unordered_map<key_t, T> reduce_data;
 
         std::vector<reduce_arg_t> elements;
         auto save_fn = [&elements](reduce_arg_t input) {
-                elements.push_back(input);
-            };
+                           elements.push_back(input);
+                       };
         auto lop_chain = local_stack_.push(save_fn).emit();
 
-        // loop over input        
-        while (it.HasNext()){
+        // loop over input
+        while (it.HasNext()) {
             lop_chain(it.Next());
         }
 
-        for (auto item : elements) { 
+        for (auto item : elements) {
             key_t key = key_extractor_(item);
-            auto elem = reduce_data.find(key);            
+            auto elem = reduce_data.find(key);
             // SpacingLogger(true) << item;
 
             // is there already an element with same key?
-            if(elem != reduce_data.end()) {
+            if (elem != reduce_data.end()) {
                 auto new_elem = reduce_function_(item, elem->second);
                 reduce_data.at(key) = new_elem;
             }
@@ -152,22 +150,22 @@ private:
         //TODO get number of worker by net-group or something similar
         int number_worker = 1;
         //TODO use network emitter in future
-        std::vector<data::BlockEmitter<T>> emit_array;
+        std::vector<data::BlockEmitter<T> > emit_array;
         data::BlockEmitter<T> emit = (this->context_).get_data_manager().template GetLocalEmitter<T>(this->data_id_);
-        for (auto it = reduce_data.begin(); it != reduce_data.end(); ++it){
+        for (auto it = reduce_data.begin(); it != reduce_data.end(); ++it) {
             std::hash<T> t_hash;
             auto hashed = t_hash(it->second) % number_worker;
-            // TODO When emitting and the real network emitter is there, hashed is needed to emit 
+            // TODO When emitting and the real network emitter is there, hashed is needed to emit
             emit(it->second);
         }
     }
 
     //!Recieve elements from other workers.
-    auto MainOp() {
-    }
+    auto MainOp() { }
 
     //! Hash recieved elements onto buckets and reduce each bucket to a single value.
-    void PostOp() {
+    void PostOp()
+    {
         std::cout << "PostOp" << std::endl;
         using key_t = typename FunctionTraits<KeyExtractor>::result_type;
         std::unordered_map<key_t, T> reduce_data;
@@ -177,13 +175,13 @@ private:
         using key_t = typename FunctionTraits<KeyExtractor>::result_type;
         std::unordered_map<key_t, T> global_data;
 
-        while(it.HasNext()) {
+        while (it.HasNext()) {
             auto item = it.Next();
             key_t key = key_extractor_(item);
-            auto elem = reduce_data.find(key);            
+            auto elem = reduce_data.find(key);
 
             // is there already an element with same key?
-            if(elem != reduce_data.end()) {
+            if (elem != reduce_data.end()) {
                 auto new_elem = reduce_function_(item, elem->second);
                 reduce_data.at(key) = new_elem;
             }
@@ -193,12 +191,11 @@ private:
         }
 
         data::BlockEmitter<T> emit = (this->context_).get_data_manager().template GetLocalEmitter<T>(post_data_id_);
-        for (auto it = reduce_data.begin(); it != reduce_data.end(); ++it){
+        for (auto it = reduce_data.begin(); it != reduce_data.end(); ++it) {
             emit(it->second);
         }
     }
 };
-
 } // namespace c7a
 
 //! \}
--- c7a/data/input_line_iterator.hpp
+++ c7a/data/input_line_iterator.hpp
@@ -12,61 +12,59 @@
 
 namespace c7a {
 namespace data {
-
 //! InputLineIterator gives you access to lines of a file
 class InputLineIterator
 {
 public:
     //! Creates an instance of iterator that reads file line based
-    InputLineIterator(std::ifstream & file,
-                          size_t my_id, 
-                          size_t num_workers)
+    InputLineIterator(std::ifstream& file,
+                      size_t my_id,
+                      size_t num_workers)
         : file_(file),
           my_id_(my_id),
-          num_workers_(num_workers) {
-        
-
+          num_workers_(num_workers)
+    {
         //Find file size and save it
-        file_.seekg(0,std::ios::end);
+        file_.seekg(0, std::ios::end);
         file_size_ = file_.tellg();
 
         //Go to start of 'local part'.
         size_t per_worker = file_size_ / num_workers_;
         size_t my_start = per_worker * my_id_;
-        file_.seekg(my_start,std::ios::beg);
+        file_.seekg(my_start, std::ios::beg);
 
         //Go to next new line if the stream-pointer is not at the beginning of a line
-        if(my_id != 0) {
+        if (my_id != 0) {
             file_.unget();
             if (file_.get() != '\n') {
                 std::string str;
-                std::getline(file_,str);
-            }            
+                std::getline(file_, str);
+            }
         }
-
-        
     }
 
     //! returns the next element if one exists
     //!
     //! does no checks whether a next element exists!
-    inline const std::string Next() {
+    inline const std::string Next()
+    {
         std::string line;
         std::getline(file_, line);
         return line;
     }
 
     //! returns true, if an element is available in local part
-    inline bool HasNext() {
+    inline bool HasNext()
+    {
         size_t per_worker = file_size_ / num_workers_;
         size_t my_end = per_worker * (my_id_ + 1) - 1;
-       
+
         return (file_.tellg() <= my_end);
-     }
+    }
 
 private:
-    //!Input file stream 
-    std::ifstream & file_;
+    //!Input file stream
+    std::ifstream& file_;
     //!File size in bytes
     size_t file_size_;
     //!Worker ID
@@ -74,6 +72,5 @@ private:
     //!Total number of workers
     size_t num_workers_;
 };
-
 }
 }
