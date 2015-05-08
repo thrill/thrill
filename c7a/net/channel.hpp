662e94b595e3e8d491177001ead45aeb38e84a12
Tobias Sturm <mail@tobiassturm.de>
2015-05-08 03:56:45 +0200

    add docu

--- c7a/net/channel.hpp
+++ c7a/net/channel.hpp
@@ -4,17 +4,41 @@
 #include <c7a/net/stream.hpp>
 namespace c7a {
 namespace net {
+//! \ingroup net
+//! \{
+
+//! A Chanel is a collection of \ref Stream instances and bundles them to a
+//! logical communication channel.
+//!
+//! There exists only one stream per socket at a time.
+//! The channel keeps track of all active channels and counts the closed ones.
+//!
+//! As soon as the number of expected streams is reached, the channel is marked
+//! as finished and no more data will arrive.
+//!
+//! Block headers are put into streams that poll more data from the socket.
+//! As soon as the block is exhausted, the socket polling responsibility
+//! is transfered back to the channel multiplexer.
+//!
+//! This class is the state machine for the callback-hell from NetDispatcher
 class Channel
 {
 public:
+    //! Called to transfer the polling responsibility back to the channel multiplexer
     typedef std::function<void (Socket& s)> ReleaseSocketCallback;
 
+    //! Creates a new channel instance
     Channel(NetDispatcher& dispatcher, ReleaseSocketCallback release_callback, int id, int expected_streams)
         : dispatcher_(dispatcher),
           release_(release_callback),
           id_(id),
           expected_streams_(expected_streams) { }
 
+    //! Takes over the polling responsibility from the channel multiplexer
+    //!
+    //! This is the start state of the callback state machine.
+    //! end-of-streams are handled directly
+    //! all other block headers are parsed
     void PickupStream(Socket& s, struct StreamBlockHeader head)
     {
         struct Stream* stream = new struct Stream (s, head);
@@ -37,11 +61,13 @@ public:
         }
     }
 
-    bool Finished()
+    //! Indicates whether all streams are finished
+    bool Finished() const
     {
         return finished_streams_ == expected_streams_;
     }
 
+    //! Accesses the data of the channel
     const std::vector<std::string> & Data()
     {
         return data_;
@@ -58,6 +84,11 @@ private:
     int finished_streams_;
     std::vector<std::string> data_;
 
+    //!The first header part is read in the channel multiplexer to see which
+    //!channel the package belongs to
+    //!The second part of the header (boundaries of the block) is read here
+    //!
+    //! This is the second state of the callback state machine
     void ReadSecondHeaderPartFrom(Socket& s, const std::string& buffer, struct Stream* stream)
     {
         (void)s; //supress 'unused paramter' warning - needs to be in parameter list though
@@ -68,6 +99,8 @@ private:
         ReadFromStream(stream);
     }
 
+    //! Decides if there are more elements to read of a new stream block header
+    //! is expected (transfers control back to multiplexer)
     void ReadFromStream(struct Stream* stream)
     {
         if (stream->elements_read < stream->header.num_elements) {
@@ -82,8 +115,12 @@ private:
         }
     }
 
+    //! Expect data to arrive at the socket.
+    //! Size of element is known
     inline void ExpectData(struct Stream* stream)
     {
+        //TODO we might want to read multiple items in a batch and cut them
+        //into single pieces later...
         auto exp_size = stream->header.boundaries[stream->elements_read++];
         auto callback = std::bind(&Channel::ConsumeData, this, std::placeholders::_1, std::placeholders::_2, stream);
         LOG << "expect data with " << exp_size << " bytes on socket" << stream->socket.GetFileDescriptor() << "in channel" << id_;
@@ -97,5 +134,6 @@ private:
         ReadFromStream(stream);
     }
 };
+//! \}
 }
 }
