fe52060713c0c332de04d5ef328e1eb01a8a1f22
Alexander Noe <aleexnoe@gmail.com>
2015-05-10 13:27:35 +0200

    fix compiler error in this branch, test if jenkins still fails net tests

--- tests/data/test_dia_allocate.cpp
+++ tests/data/test_dia_allocate.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "c7a/api/dia_base.hpp"
-#include "c7a/engine/stage_builder.hpp"
+#include "c7a/core/stage_builder.hpp"
 #include "c7a/api/dia.hpp"
 #include "c7a/api/context.hpp"
 
