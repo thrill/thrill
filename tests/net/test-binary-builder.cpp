d4cdbeb38b48cc8375e18018049ed5318e5e14e9
Timo Bingmann <tbgit@panthema.net>
2015-05-12 22:19:10 +0200

    Adding BinaryBuilder and BinaryReader for simple serialization of data.

---
+++ tests/net/test-binary-builder.cpp
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * tests/net/test-binary-builder.cpp
+ *
+ * Part of Project c7a.
+ *
+ * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+ *
+ * This file has no license. Only Chunk Norris can compile it.
+ ******************************************************************************/
+
+#include <c7a/net/binary-builder.hpp>
+#include <gtest/gtest.h>
+
+using c7a::net::BinaryBuilder;
+using c7a::net::BinaryBuffer;
+using c7a::net::BinaryReader;
+
+TEST(BinaryBuilder, Test1) {
+    // construct a binary blob
+    BinaryBuilder bb;
+    {
+        bb.Put<unsigned int>(1);
+        bb.PutString("test");
+
+        bb.PutVarint(42);
+        bb.PutVarint(12345678);
+
+        // add a sub block
+        BinaryBuilder sub;
+        sub.PutString("sub block");
+        sub.PutVarint(6 * 9);
+
+        bb.PutString(sub);
+    }
+
+    // read binary block and verify content
+
+    BinaryBuffer bbr = bb;
+
+    const unsigned char bb_data[] = {
+        // bb.Put<unsigned int>(1)
+        0x01, 0x00, 0x00, 0x00,
+        // bb.PutString("test")
+        0x04, 0x74, 0x65, 0x73, 0x74,
+        // bb.PutVarint(42);
+        0x2a,
+        // bb.PutVarint(12345678);
+        0xce, 0xc2, 0xf1, 0x05,
+        // begin sub block (length)
+        0x0b,
+        // sub.PutString("sub block");
+        0x09, 0x73, 0x75, 0x62, 0x20, 0x62, 0x6c, 0x6f, 0x63, 0x6b,
+        // sub.PutVarint(6 * 9);
+        0x36,
+    };
+
+    BinaryBuffer bb_verify(bb_data, sizeof(bb_data));
+
+    if (bbr != bb_verify)
+        std::cout << bbr.ToString();
+
+    ASSERT_EQ(bbr, bb_verify);
+
+    // read binary block using binary_reader
+
+    BinaryReader br(bb);
+
+    ASSERT_EQ(br.Get<unsigned int>(), 1);
+    ASSERT_EQ(br.GetString(), "test");
+    ASSERT_EQ(br.GetVarint(), 42);
+    ASSERT_EQ(br.GetVarint(), 12345678);
+
+    {
+        BinaryReader sub_br = br.GetBinaryBuffer();
+        ASSERT_EQ(sub_br.GetString(), "sub block");
+        ASSERT_EQ(sub_br.GetVarint(), 6 * 9);
+        ASSERT_TRUE(sub_br.empty());
+    }
+
+    ASSERT_TRUE(br.empty());
+}
+
+/******************************************************************************/
