73b6135876eed5111d3e066dbcc5b0c9dba047c6
Emanuel Jöbstl <emiswelt@gmail.com>
2015-05-12 18:37:49 +0200

    Added communication manager.

---
+++ c7a/net/communication_manager.cpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * c7a/net/communication-manager.cpp
+ *
+ * Part of Project c7a.
+ *
+ *
+ * This file has no license. Only Chunk Norris can compile it.
+ ******************************************************************************/
+
+#include "communication_manager.hpp"
+
+namespace c7a {
+namespace net {
+
+} // namespace c7a
+} // namespace net
+
+/******************************************************************************/
