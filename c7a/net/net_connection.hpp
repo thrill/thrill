213e9a361ddfc7e28def81f4b6f285aefd138afb
Emanuel Jöbstl <emiswelt@gmail.com>
2015-05-13 15:15:09 +0200

    Added missing files, uncommented tests.

---
+++ c7a/net/net_connection.hpp
@@ -0,0 +1,224 @@
+/*******************************************************************************
+ * c7a/net/net-connection.hpp
+ *
+ * Contains NetConnection a richer set of network point-to-point primitives.
+ *
+ * Part of Project c7a.
+ *
+ * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#pragma once
+#ifndef C7A_NET_NET_CONNECTION_HEADER
+#define C7A_NET_NET_CONNECTION_HEADER
+
+#include <cassert>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+#include <c7a/net/lowlevel/socket.hpp>
+#include <c7a/net/lowlevel/net_exception.hpp>
+
+namespace c7a {
+namespace net {
+
+//! \addtogroup net Network Communication
+//! \{
+
+// Because Mac OSX does not know MSG_MORE.
+#ifndef MSG_MORE
+#define MSG_MORE 0
+#endif
+
+/*!
+ * NetConnection is a rich point-to-point socket connection to another client
+ * (worker, master, or whatever). Messages are fixed-length integral items or
+ * opaque byte strings with a length.
+ *
+ * If any function fails to send or receive, then a NetException is thrown
+ * instead of explicit error handling. If ever an error occurs, we probably have
+ * to rebuild the whole network explicitly.
+ */
+class NetConnection : protected lowlevel::Socket
+{
+    static const bool debug = true;
+
+    static const bool self_verify_ = true;
+
+public:
+    //! Construct NetConnection from a Socket
+    explicit NetConnection(const Socket& s = Socket())
+        : Socket(s)
+    { }
+
+#if !C7A_NETCONNECTION_COPYABLE
+    //! move-constructor
+    NetConnection(NetConnection&& other) : Socket(other)
+    { other.fd_ = -1; }
+
+    //! move assignment-operator
+    NetConnection& operator = (NetConnection&& other)
+    {
+        if (IsValid()) {
+            sLOG1 << "Assignment-destruction of valid NetConnection" << this;
+            Close();
+        }
+        Socket::operator = (other);
+        other.fd_ = -1;
+        return *this;
+    }
+#endif
+
+    //! Check whether the contained file descriptor is valid.
+    bool IsValid() const
+    { return Socket::IsValid(); }
+
+    //! Return the raw socket object for more low-level network programming.
+    Socket & GetSocket()
+    { return *this; }
+
+    //! Return the raw socket object for more low-level network programming.
+    const Socket & GetSocket() const
+    { return *this; }
+
+    //! Return the associated socket error
+    int GetError() const
+    { return Socket::GetError(); }
+
+    //! Set socket to non-blocking
+    int SetNonBlocking(bool non_blocking) const
+    { return Socket::SetNonBlocking(non_blocking); }
+
+    //! Return the socket peer address
+    std::string GetPeerAddress() const
+    { return Socket::GetPeerAddress().ToStringHostPort(); }
+
+    bool operator == (const NetConnection& c) const
+    { return GetSocket().fd() == c.GetSocket().fd(); }
+
+    //! \name Send Functions
+    //! \{
+
+    //! Send a fixed-length type T (possibly without length header).
+    template <typename T>
+    void Send(const T& value)
+    {
+        static_assert(std::is_integral<T>::value,
+                      "You only want to send integral types as raw values.");
+
+        if (self_verify_) {
+            // for communication verification, send sizeof.
+            size_t len = sizeof(value);
+            if (send(&len, sizeof(len), MSG_MORE) != sizeof(len))
+                throw lowlevel::NetException("Error during Send", errno);
+        }
+
+        if (send(&value, sizeof(value)) != (ssize_t)sizeof(value))
+            throw lowlevel::NetException("Error during Send", errno);
+    }
+
+    //! Send a string buffer
+    void SendString(const void* data, size_t len)
+    {
+        if (send(&len, sizeof(len), MSG_MORE) != sizeof(len))
+            throw lowlevel::NetException("Error during SendString", errno);
+
+        if (send(data, len) != (ssize_t)len)
+            throw lowlevel::NetException("Error during SendString", errno);
+    }
+
+    //! Send a string message.
+    void SendString(const std::string& message)
+    {
+        SendString(message.data(), message.size());
+    }
+
+    //! \}
+
+    //! \name Receive Functions
+    //! \{
+
+    //! Receive a fixed-length type, possibly without length header.
+    template <typename T>
+    void Receive(T* out_value)
+    {
+        static_assert(std::is_integral<T>::value,
+                      "You only want to receive integral types as raw values.");
+
+        if (self_verify_) {
+            // for communication verification, receive sizeof.
+            size_t len = 0;
+            if (recv(&len, sizeof(len)) != sizeof(len))
+                throw lowlevel::NetException("Error during Receive", errno);
+
+            // if this fails, then fixed-length type communication desynced.
+            die_unequal(len, sizeof(*out_value));
+        }
+
+        if (recv(out_value, sizeof(*out_value)) != (ssize_t)sizeof(*out_value))
+            throw lowlevel::NetException("Error during Receive", errno);
+    }
+
+    //! Blocking receive string message from the connected socket.
+    void ReceiveString(std::string* outdata)
+    {
+        size_t len = 0;
+
+        if (recv(&len, sizeof(len)) != sizeof(len))
+            throw lowlevel::NetException("Error during ReceiveString", errno);
+
+        if (len == 0)
+            return;
+
+        outdata->resize(len);
+
+        ssize_t ret = recv(const_cast<char*>(outdata->data()), len);
+
+        if (ret != (ssize_t)len)
+            throw lowlevel::NetException("Error during ReceiveString", errno);
+    }
+
+    //! \}
+
+    //! Destruction of NetConnection should be explicitly done by a NetGroup or
+    //! other network class.
+    ~NetConnection()
+    {
+        if (IsValid()) {
+            sLOG1 << "Destruction of valid NetConnection" << this;
+            Close();
+        }
+    }
+
+    //! Close this NetConnection
+    void Close()
+    {
+        Socket::close();
+    }
+
+    //! make ostreamable
+    friend std::ostream& operator << (std::ostream& os, const NetConnection& c)
+    {
+        os << "[NetConnection"
+           << " fd=" << c.GetSocket().fd();
+
+        if (c.IsValid())
+            os << " peer=" << c.GetPeerAddress();
+
+        return os << "]";
+    }
+};
+
+// \}
+
+} // namespace net
+} // namespace c7a
+
+#endif // !C7A_NET_NET_CONNECTION_HEADER
+
+/******************************************************************************/
---
+++ c7a/net/net_dispatcher.hpp
@@ -0,0 +1,331 @@
+/*******************************************************************************
+ * c7a/net/net-dispatcher.hpp
+ *
+ * Asynchronous callback wrapper around select(), epoll(), or other kernel-level
+ * dispatchers.
+ *
+ * Part of Project c7a.
+ *
+ * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#pragma once
+#ifndef C7A_NET_NET_DISPATCHER_HEADER
+#define C7A_NET_NET_DISPATCHER_HEADER
+
+#include <c7a/net/net_connection.hpp>
+#include <c7a/net/lowlevel/socket.hpp>
+#include <c7a/net/lowlevel/select_dispatcher.hpp>
+//#include <c7a/net/lowlevel/epoll-dispatcher.hpp>
+
+#include <string>
+#include <deque>
+#include <queue>
+#include <ctime>
+
+namespace c7a {
+namespace net {
+
+//! \addtogroup net Network Communication
+//! \{
+
+/**
+ * NetDispatcher is a high level wrapper for asynchronous callback
+ * processing.. One can register NetConnection objects for readability and
+ * writability checks, buffered reads and writes with completion callbacks, and
+ * also timer functions.
+ */
+class NetDispatcher
+{
+    static const bool debug = false;
+
+protected:
+    //! switch between different low-level dispatchers
+    typedef lowlevel::SelectDispatcher<NetConnection&> Dispatcher;
+    //typedef lowlevel::EPollDispatcher Dispatcher;
+
+    //! import into class namespace
+    typedef lowlevel::Socket Socket;
+
+public:
+    //! \name Timeout Callbacks
+    //! \{
+
+    //! callback signature for timer events
+    typedef std::function<bool ()> TimerCallback;
+
+    //! Register a relative timeout callback
+    void AddRelativeTimeout(double timeout, const TimerCallback& cb)
+    {
+        timer_pq_.emplace(GetClock() + timeout, timeout, cb);
+    }
+
+    //! \}
+
+    //! \name NetConnection Callbacks
+    //! \{
+
+    //! callback signature for socket readable/writable events
+    typedef std::function<bool (NetConnection&)> ConnectionCallback;
+
+    //! Register a buffered read callback and a default exception callback.
+    void AddRead(NetConnection& c, const ConnectionCallback& read_cb)
+    {
+        return dispatcher_.AddRead(c.GetSocket().fd(), c, read_cb);
+    }
+
+    //! Register a buffered write callback and a default exception callback.
+    void AddWrite(NetConnection& c, const ConnectionCallback& write_cb)
+    {
+        return dispatcher_.AddWrite(c.GetSocket().fd(), c, write_cb);
+    }
+
+    //! Register a buffered write callback and a default exception callback.
+    void AddReadWrite(
+        NetConnection& c,
+        const ConnectionCallback& read_cb, const ConnectionCallback& write_cb)
+    {
+        return dispatcher_.AddReadWrite(
+            c.GetSocket().fd(), c, read_cb, write_cb);
+    }
+
+    //! \}
+
+    //! \name Asynchronous Data Reader/Writer Callbacks
+    //! \{
+
+    //! callback signature for async read callbacks
+    typedef std::function<void (NetConnection& c,
+                                const std::string& buffer)> AsyncReadCallback;
+
+    //! asynchronously read n bytes and deliver them to the callback
+    virtual void AsyncRead(NetConnection& c, size_t n,
+                           AsyncReadCallback done_cb)
+    {
+        LOG << "async read on read dispatcher";
+        if (n == 0) {
+            if (done_cb) done_cb(c, std::string());
+            return;
+        }
+
+        // add new async reader object
+        async_read_.emplace_back(n, done_cb);
+
+        // register read callback
+        dispatcher_.AddRead(c.GetSocket().fd(), c, async_read_.back());
+    }
+
+    //! callback signature for async write callbacks
+    typedef std::function<void (NetConnection&)> AsyncWriteCallback;
+
+    //! asynchronously write buffer and callback when delivered
+    void AsyncWrite(NetConnection& c, const std::string& buffer,
+                    AsyncWriteCallback done_cb = nullptr)
+    {
+        if (buffer.size() == 0) {
+            if (done_cb) done_cb(c);
+            return;
+        }
+
+        // add new async writeer object
+        async_write_.emplace_back(buffer, done_cb);
+
+        // register write callback
+        dispatcher_.AddWrite(c.GetSocket().fd(), c, async_write_.back());
+    }
+
+    //! asynchronously write buffer and callback when delivered. Copies the data
+    //! into a std::string buffer!
+    void AsyncWrite(NetConnection& c, const void* buffer, size_t size,
+                    AsyncWriteCallback done_cb = NULL)
+    {
+        return AsyncWrite(
+            c, std::string(reinterpret_cast<const char*>(buffer), size),
+            done_cb);
+    }
+
+    //! \}
+
+    //! \name Dispatch
+    //! \{
+
+    //! dispatch one or more events
+    void Dispatch()
+    {
+        // process timer events that lie in the past
+        double now = GetClock();
+
+        while (!timer_pq_.empty() && timer_pq_.top().next_timeout <= now)
+        {
+            const Timer& top = timer_pq_.top();
+            if (top.cb()) {
+                // requeue timeout event again.
+                timer_pq_.emplace(top.next_timeout + top.timeout,
+                                  top.timeout, top.cb);
+            }
+            timer_pq_.pop();
+        }
+
+        // calculate time until next timer event
+        if (timer_pq_.empty()) {
+            dispatcher_.Dispatch(INFINITY);
+        }
+        else {
+            dispatcher_.Dispatch(timer_pq_.top().next_timeout - now);
+        }
+    }
+
+    //! \}
+
+protected:
+    //! get a current monotonic clock
+    static double GetClock()
+    {
+        struct timespec ts;
+        ::clock_gettime(CLOCK_MONOTONIC, &ts);
+        return ts.tv_sec + ts.tv_nsec * 1e-9;
+    }
+
+    //! low-level file descriptor async processing
+    Dispatcher dispatcher_;
+
+    //! struct for timer callbacks
+    struct Timer
+    {
+        //! timepoint of next timeout
+        double        next_timeout;
+        //! relative timeout for restarting
+        double        timeout;
+        //! callback
+        TimerCallback cb;
+
+        Timer(double _next_timeout, double _timeout, const TimerCallback& _cb)
+            : next_timeout(_next_timeout), timeout(_timeout), cb(_cb)
+        { }
+
+        bool operator < (const Timer& b) const
+        { return next_timeout < b.next_timeout; }
+    };
+
+    //! priority queue of timer callbacks
+    typedef std::priority_queue<Timer> TimerPQ;
+
+    //! priority queue of timer callbacks, obviously kept in timeout
+    //! order. Currently not addressable.
+    TimerPQ timer_pq_;
+
+    /**************************************************************************/
+
+    class AsyncReadBuffer
+    {
+    public:
+        //! Construct buffered reader with callback
+        AsyncReadBuffer(size_t buffer_size, const AsyncReadCallback& callback)
+            : callback_(callback),
+              buffer_(buffer_size, 0)
+        { }
+
+        //! Should be called when the socket is readable
+        bool operator () (NetConnection& c)
+        {
+            int r = c.GetSocket().recv_one(
+                const_cast<char*>(buffer_.data() + size_),
+                buffer_.size() - size_);
+
+            if (r < 0)
+                throw lowlevel::NetException("AsyncReadBuffer() error in recv", errno);
+
+            size_ += r;
+
+            if (size_ == buffer_.size()) {
+                if (callback_) callback_(c, buffer_);
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+    private:
+        //! total size currently read
+        size_t size_ = 0;
+
+        //! functional object to call once data is complete
+        AsyncReadCallback callback_;
+
+        //! Receive buffer
+        std::string buffer_;
+    };
+
+    //! deque of asynchronous readers
+    std::deque<AsyncReadBuffer> async_read_;
+
+    /**************************************************************************/
+
+    class AsyncWriteBuffer
+    {
+    public:
+        //! Construct buffered writer with callback
+        AsyncWriteBuffer(const std::string& buffer,
+                         const AsyncWriteCallback& callback)
+            : callback_(callback),
+              buffer_(buffer)
+        { }
+
+        //! Should be called when the socket is writable
+        bool operator () (NetConnection& c)
+        {
+            int r = c.GetSocket().send_one(
+                const_cast<char*>(buffer_.data() + size_),
+                buffer_.size() - size_);
+
+            if (r < 0)
+                throw lowlevel::NetException("AsyncWriteBuffer() error in send", errno);
+
+            size_ += r;
+
+            if (size_ == buffer_.size()) {
+                if (callback_) callback_(c);
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+    private:
+        //! total size currently written
+        size_t size_ = 0;
+
+        //! functional object to call once data is complete
+        AsyncWriteCallback callback_;
+
+        //! Receive buffer
+        std::string buffer_;
+    };
+
+    //! deque of asynchronous writers
+    std::deque<AsyncWriteBuffer> async_write_;
+
+    /**************************************************************************/
+
+    //! Default exception handler
+    static bool ExceptionCallback(NetConnection& s)
+    {
+        // exception on listen socket ?
+        throw lowlevel::NetException(
+                  "NetDispatcher() exception on socket fd "
+                  + std::to_string(s.GetSocket().fd()) + "!", errno);
+    }
+};
+
+//! \}
+
+} // namespace net
+} // namespace c7a
+
+#endif // !C7A_NET_NET_DISPATCHER_HEADER
+
+/******************************************************************************/
