0dac1d4fe7ba1034334d922561cdd66bf04da898
Tobias Sturm <tobstu@gmail.com>
2015-05-15 13:25:16 +0200

    CA-104 #comment update socket headers to not include element boundaries, which is now done via buffer.PutString

---
+++ c7a/data/socket_target.hpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * c7a/data/socket_target.hpp
+ *
+ * Part of Project c7a.
+ *
+ *
+ * This file has no license. Only Chunk Norris can compile it.
+ ******************************************************************************/
+
+#pragma once
+#ifndef C7A_DATA_SOCKET_TARGET_HEADER
+#define C7A_DATA_SOCKET_TARGET_HEADER
+
+namespace c7a {
+namespace data {
+#include <c7a/net/buffer.hpp>
+#include <c7a/net/stream.hpp>
+
+class SocketTarget
+{
+public:
+    SocketTarget(net::NetDispatcher& dispatcher, net::NetConnection& connection, size_t channel_id)
+        : dispatcher_(dispatcher),
+          connection_(connection),
+          id_(channel_id) { }
+
+    void Append(net::BinaryBuffer buffer)
+    {
+        SendHeader(buffer.size());
+
+        net::Buffer payload_buf = buffer.ToBuffer();
+        dispatcher_.AsyncWrite(connection_, std::move(payload_buf));
+    }
+
+    void SendHeader(size_t num_bytes)
+    {
+        net::StreamBlockHeader header;
+        header.channel_id = id_;
+        header.expected_bytes = num_bytes;
+        net::Buffer header_buffer(&header, sizeof(header));
+        dispatcher_.AsyncWrite(connection_, std::move(header_buffer));
+    }
+
+    void Close()
+    {
+        SendHeader(0);
+    }
+
+private:
+    net::NetDispatcher& dispatcher_;
+    net::NetConnection& connection_;
+    size_t id_;
+};
+} // namespace data
+} // namespace c7a
+
+#endif // !C7A_DATA_SOCKET_TARGET_HEADER
+
+/******************************************************************************/
--- tests/net/test_stream.cpp
+++ tests/net/test_stream.cpp
@@ -23,49 +23,24 @@ struct StreamTest : public::testing::Test {
     StreamTest()
     {
         candidate.channel_id = 2;
-        candidate.num_elements = 3;
-        candidate.boundaries = data;
+        candidate.expected_bytes = 4;
     }
 
-    size_t                   data[3] = { 22, 23, 55 };
     struct StreamBlockHeader candidate;
 };
 
-TEST_F(StreamTest, StreamBlockHeaderParsesAndSerializesIdAndNumElements) {
+TEST_F(StreamTest, StreamBlockHeaderParsesAndSerializesHeader) {
     auto seri = candidate.Serialize();
     struct StreamBlockHeader result;
-    result.ParseIdAndNumElem(seri);
+    result.ParseHeader(seri);
 
     ASSERT_EQ(candidate.channel_id, result.channel_id);
-    ASSERT_EQ(candidate.num_elements, result.num_elements);
-}
-
-TEST_F(StreamTest, StreamBlockHeaderParsesAndSerializesBoundaries) {
-    auto seri = candidate.Serialize();
-    std::string part1 = seri.substr(0, sizeof(size_t) * 2);
-    std::string part2 = seri.substr(part1.length(), seri.length() - part1.length());
-    struct StreamBlockHeader result;
-    result.ParseIdAndNumElem(part1);
-    result.ParseBoundaries(part2);
-
-    ASSERT_EQ(22, result.boundaries[0]);
-    ASSERT_EQ(23, result.boundaries[1]);
-    ASSERT_EQ(55, result.boundaries[2]);
-}
-
-TEST_F(StreamTest, StreamBlockHeaderParsesAndSerializesBoundariesIfEmpty) {
-    candidate.num_elements = 0;
-    auto seri = candidate.Serialize();
-    struct StreamBlockHeader result;
-    result.ParseIdAndNumElem(seri);
-    result.ParseBoundaries(seri);
-
-    ASSERT_EQ(0, result.num_elements);
+    ASSERT_EQ(candidate.expected_bytes, result.expected_bytes);
 }
 
 TEST_F(StreamTest, StreamBlockHeaderIsStreamEnd) {
     ASSERT_FALSE(candidate.IsStreamEnd());
-    candidate.num_elements = 0;
+    candidate.expected_bytes = 0;
     ASSERT_TRUE(candidate.IsStreamEnd());
 }
 
