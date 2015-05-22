2ba321498abb3ca13f34233d577cb1f2c07df15a
Tobias Sturm <mail@tobiassturm.de>
2015-05-22 14:20:36 +0200

    skelleton for job bootstrap

---
+++ c7a/api/bootstrap.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * c7a/api/bootstrap.hpp
+ *
+ * Part of Project c7a.
+ *
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#pragma once
+#ifndef C7A_API_BOOTSTRAP_HEADER
+#define C7A_API_BOOTSTRAP_HEADER
+
+#include <c7a/api/context.hpp>
+
+namespace c7a {
+
+//! Executes the given job startpoint with a context instance
+//! startpoint may be called multiple times with concurrent threads.
+static int Execute(int argc, char* argv[], std::function<int(Context&)> job_startpoint)
+{
+    Context ctx;
+    return job_startpoint(ctx);
+}
+
+} // namespace c7a
+
+#endif // !C7A_API_BOOTSTRAP_HEADER
+
+/******************************************************************************/
