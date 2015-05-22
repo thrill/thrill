c4a0b77d3acb354620a29308ec42a02359cfe0aa
Tobias Sturm <mail@tobiassturm.de>
2015-05-22 14:20:36 +0200

    skelleton for job bootstrap

---
+++ examples/word_count.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * examples/word_count.cpp
+ *
+ * Part of Project c7a.
+ *
+ *
+ * This file has no license. Only Chunk Norris can compile it.
+ ******************************************************************************/
+
+#include <c7a/api/bootstrap.hpp>
+
+int word_count(c7a::Context& context);
+
+int main(int argc, char* argv[])
+{
+    return c7a::Execute(argc, argv, word_count);
+}
+
+//! The WordCount user program
+int word_count(c7a::Context& context)
+{
+    return 0;
+}
+
+/******************************************************************************/
