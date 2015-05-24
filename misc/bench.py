6f8828b8c31fee53a9d703226a84c68ba6af4de0
Alexander Noe <aleexnoe@gmail.com>
2015-05-24 23:41:04 +0200

    adding logfiles to move them out of i10pcs, as they don't have matplotlib

--- misc/bench.py
+++ misc/bench.py
@@ -5,41 +5,40 @@ import sys
 import subprocess
 import random
 
-amounts=[10,12,14,16,18,20]
+amounts=[10,12,14,16,18,20,22,24,26,28]
 
-workers = sys.argv[1]
-modulo = sys.argv[2]
+workers = [1,10,100,1000]
+modulae = [100,1000,10000,100000]
 
 
 # Generate files using generator script and execute the sorter
-with open(workers + "_" + modulo + "_true", "w+") as file1:
-    for amount in amounts:
-    	sum = 0
-    	for _ in range(5):
-       	    process = subprocess.Popen(["./../build/examples/bench", str(pow(2,amount)), workers, modulo], stdout=subprocess.PIPE)
-            process.wait()
-	    time = process.communicate()[0]
-            sum += float(time)
-	    print time
-            avg = sum / 5
-    	print str(amount) + " " + str(avg / pow(2,amount)) + "\n"
-    	file1.write(str(amount) + " " + str(avg / pow(2,amount)) + "\n")
-    file1.close()
+for worker in workers:
+    for modulo in modulae:
+        print "Testing with " + str(worker) + " workers and integers modulo " + str(modulo)
+        with open(str(worker) + "_" + str(modulo) + "_true", "w+") as file1:
+            for amount in amounts:
+                sum = 0
+                for _ in range(3):
+                    process = subprocess.Popen(["./../build/examples/bench", str(pow(2,amount)), str(worker), str(modulo)], stdout=subprocess.PIPE)
+                    process.wait()
+                    time = process.communicate()[0]
+                    sum += float(time)
+                    print time
+                avg = sum / 3
+                print str(amount) + " " + str(avg / pow(2,amount))
+                file1.write(str(amount) + " " + str(avg / pow(2,amount)) + "\n")
+            file1.close()
 
-with open(workers + "_" + modulo + "_false", "w+") as file2:
-    for amount in amounts:
-    	sum = 0
-    	for _ in range(5):
-       	    process = subprocess.Popen(["./../build/examples/bench_ref", str(pow(2,amount)), workers, modulo], stdout=subprocess.PIPE)
-            process.wait()
-	    time = process.communicate()[0]
-            sum += float(time)
-	    print time
-            avg = sum / 5
-    	print str(amount) + " " + str(avg / pow(2,amount)) + "\n"
-    	file2.write(str(amount) + " " + str(avg / pow(2,amount)) + "\n")
-    file2.close()
-
-os.system("python graph_gen.py " + workers + "_" + modulo + " " + sys.argv[3])
-os.remove(workers + "_" + modulo + "_true")
-os.remove(workers + "_" + modulo + "_false")
+        with open(str(worker) + "_" + str(modulo) + "_false", "w+") as file2:
+            for amount in amounts:
+                sum = 0
+                for _ in range(3):
+                    process = subprocess.Popen(["./../build/examples/bench_ref", str(pow(2,amount)), str(worker), str(modulo)], stdout=subprocess.PIPE)
+                    process.wait()
+                    time = process.communicate()[0]
+                    sum += float(time)
+                    print time
+                avg = sum / 3
+                print str(amount) + " " + str(avg / pow(2,amount))
+                file2.write(str(amount) + " " + str(avg / pow(2,amount)) + "\n")
+            file2.close()
