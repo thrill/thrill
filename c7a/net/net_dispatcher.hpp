c2a939df955b79cb53fb9d5fc307517dd90435fa
Tobias Sturm <mail@tobiassturm.de>
2015-05-27 12:51:28 +0200

    add todo for TB on net_dispatcher.hpp

--- c7a/net/net_dispatcher.hpp
+++ c7a/net/net_dispatcher.hpp
@@ -19,7 +19,7 @@
 #include <c7a/net/buffer.hpp>
 #include <c7a/net/lowlevel/socket.hpp>
 #include <c7a/net/lowlevel/select_dispatcher.hpp>
-//#include <c7a/net/lowlevel/epoll-dispatcher.hpp>
+//#include <c7a/net/lowlevel/epoll-dispatcher.hpp> //TODO(tb) can we use a os switch? Do we want that?
 
 #if defined(_LIBCPP_VERSION) || defined(__clang__)
 #include <c7a/common/delegate.hpp>
