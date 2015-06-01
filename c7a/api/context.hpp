3ffa0cb29e09a129f4d2f240744deeda29575c2c
Alex Noe <alexander-noe@web.de>
2015-06-01 15:20:54 +0200

    add more doxygen and fix doxygen warnings

--- c7a/api/context.hpp
+++ c7a/api/context.hpp
@@ -8,8 +8,6 @@
  ******************************************************************************/
 
 #pragma once
-#ifndef C7A_API_CONTEXT_HEADER
-#define C7A_API_CONTEXT_HEADER
 
 #include <cassert>
 #include <fstream>
@@ -24,33 +22,49 @@
 
 namespace c7a {
 
+/*!
+ * The Context of a job is a unique structure inside a worker, which holds
+ *  references to all underlying parts of c7a. The context is able to give
+ *  references to the  \ref c7a::data::DataManager "data manager", the 
+ * \ref c7a::net::NetGroup  "net group" and to the 
+ * \ref c7a::core::JobManager "job manager". The context can also return the
+ * total number of workers and the rank of this worker.
+ */
 class Context
 {
 public:
     Context() : job_manager_() { }
     virtual ~Context() { }
 
+    //! Returns a reference to the data manager, which gives iterators and 
+    //! emitters for data.
     data::DataManager & get_data_manager() {
         return job_manager_.get_data_manager();
     }
 
+    //! Returns a reference to the net group, which is used to perform network
+    //! operations.
     net::NetGroup & get_flow_net_group() {
         return job_manager_.get_net_manager().GetFlowNetGroup();
     }
 
-    // TODO Off by one? Does size include self-connections
+    //! Returns the total number of workers.
     size_t number_worker() {
         return job_manager_.get_net_manager().Size();
     }
 
+    //!Returns the rank of this worker. Between 0 and number_worker() - 1
     size_t rank() {
         return job_manager_.get_net_manager().MyRank();
     }
 
+    //!Returns a reference to the job manager, which handles the dispatching
+    //! of messages.
     core::JobManager & job_manager() {
         return job_manager_;
     }
 
+    //!Returns the current directory.
     std::string get_current_dir() {
         char cCurrentPath[FILENAME_MAX];
 
@@ -66,7 +80,5 @@ private:
 };
 
 } // namespace c7a
-
-#endif // !C7A_API_CONTEXT_HEADER
 
 /******************************************************************************/
