4dbe979eb483bd0448de31549e44c35c5d8ed7dd
Timo Bingmann <tbgit@panthema.net>
2015-06-02 23:27:04 +0200

    Using unix signals to interrupt the select() running in the dispatch thread if a new async read/write is issued, or if termination is requested.

--- c7a/net/dispatcher_thread.hpp
+++ c7a/net/dispatcher_thread.hpp
@@ -19,6 +19,7 @@
 
 #include <thread>
 #include <mutex>
+#include <csignal>
 
 namespace c7a {
 namespace net {
@@ -32,7 +33,7 @@ namespace net {
  */
 class DispatcherThread
 {
-    static const bool debug = true;
+    static const bool debug = false;
 
 public:
     //! \name Imported Typedefs
@@ -68,7 +69,7 @@ public:
         die_unless(!running_);
         LOG << "DispatcherThread::Start(): starting";
         dispatcher_.terminate_ = false;
-        thread_ = std::thread(&Dispatcher::Loop, &dispatcher_);
+        thread_ = std::thread(&DispatcherThread::Work, this);
         running_ = true;
     }
 
@@ -77,6 +78,7 @@ public:
         die_unless(running_);
         LOG << "DispatcherThread::Stop(): stopping thread";
         dispatcher_.Terminate();
+        WakeUpThread();
         thread_.join();
         running_ = false;
     }
@@ -95,6 +97,7 @@ public:
                             const TimerCallback& cb) {
         unique_lock lock(mutex_);
         dispatcher_.AddRelativeTimeout(timeout, cb);
+        WakeUpThread();
     }
 
     //! \}
@@ -106,12 +109,14 @@ public:
     void AddRead(Connection& c, const ConnectionCallback& read_cb) {
         unique_lock lock(mutex_);
         dispatcher_.AddRead(c, read_cb);
+        WakeUpThread();
     }
 
     //! Register a buffered write callback and a default exception callback.
     void AddWrite(Connection& c, const ConnectionCallback& write_cb) {
         unique_lock lock(mutex_);
         dispatcher_.AddWrite(c, write_cb);
+        WakeUpThread();
     }
 
     //! Register a buffered write callback and a default exception callback.
@@ -120,6 +125,7 @@ public:
         const ConnectionCallback& read_cb, const ConnectionCallback& write_cb) {
         unique_lock lock(mutex_);
         dispatcher_.AddReadWrite(c, read_cb, write_cb);
+        WakeUpThread();
     }
 
     //! \}
@@ -131,6 +137,7 @@ public:
     void AsyncRead(Connection& c, size_t n, AsyncReadCallback done_cb) {
         unique_lock lock(mutex_);
         dispatcher_.AsyncRead(c, n, done_cb);
+        WakeUpThread();
     }
 
     //! asynchronously write buffer and callback when delivered. The buffer is
@@ -139,6 +146,7 @@ public:
                     AsyncWriteCallback done_cb = nullptr) {
         unique_lock lock(mutex_);
         dispatcher_.AsyncWrite(c, std::move(buffer), done_cb);
+        WakeUpThread();
     }
 
     //! asynchronously write buffer and callback when delivered. COPIES the data
@@ -147,6 +155,7 @@ public:
                         AsyncWriteCallback done_cb = NULL) {
         unique_lock lock(mutex_);
         dispatcher_.AsyncWriteCopy(c, buffer, size, done_cb);
+        WakeUpThread();
     }
 
     //! asynchronously write buffer and callback when delivered. COPIES the data
@@ -155,9 +164,44 @@ public:
                         AsyncWriteCallback done_cb = NULL) {
         unique_lock lock(mutex_);
         dispatcher_.AsyncWriteCopy(c, str, done_cb);
+        WakeUpThread();
     }
 
     //! \}
+
+protected:
+    //! signal handler: do nothing, but receiving this interrupts the select().
+    static void SignalUSR1(int) {
+        return;
+    }
+
+    //! What happens in the dispatcher thread
+    void Work() {
+        {
+            // Set USR1 signal handler
+            struct sigaction sa;
+            memset(&sa, 0, sizeof(sa));
+            sa.sa_handler = SignalUSR1;
+
+            int r = sigaction(SIGUSR1, &sa, NULL);
+            die_unless(r == 0);
+        }
+
+        dispatcher_.Loop();
+    }
+
+    //! wake up select() in dispatching thread.
+    void WakeUpThread() {
+        // there are multiple very platform-dependent ways to do this. we'll try
+        // signals for now. The signal should interrupt the select(), and as we
+        // do no processing in the signal, do nothing else.
+
+        // Another way to wake up the blocking select() in the dispatcher is to
+        // create a "self-pipe", and write one byte to it.
+
+        if (running_)
+            pthread_kill(thread_.native_handle(), SIGUSR1);
+    }
 
 private:
     //! lock all calls to dispatcher
