07ef57a92f997dac0953b3960d7d692d76d119fa
Tobias Sturm <tobstu@gmail.com>
2015-06-02 10:42:13 +0200

    Extend stats.hpp

--- c7a/api/bootstrap.hpp
+++ c7a/api/bootstrap.hpp
@@ -73,10 +73,9 @@ static int Execute(int argc, char* argv[], std::function<int(Context&)> job_star
     std::cout << "connecting to peers" << std::endl;
     ctx.job_manager().Connect(my_rank, net::Endpoint::ParseEndpointList(endpoints));
     std::cout << "starting job" << std::endl;
-    c7a::common::StatsTimer<timing> timer(true);
+    auto overall_timer = ctx.get_stats().CreateTimer("job::overall", true);
     auto job_result = job_startpoint(ctx);
-    timer.Stop();
-    std::cout << timer << std::endl;
+    overall_timer.Stop();
     return job_result;
 }
 
