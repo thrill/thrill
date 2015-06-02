7b4a300549d5ed35b854686cabb90417dc7db3dc
Timo Bingmann <tbgit@panthema.net>
2015-06-02 23:00:31 +0200

    Integrate Creation of net::ChannelMultiplexer into data::Manager.

--- c7a/data/manager.hpp
+++ c7a/data/manager.hpp
@@ -43,10 +43,20 @@ using c7a::net::ChannelId;
 class Manager
 {
 public:
-    Manager(net::ChannelMultiplexer& cmp) : cmp_(cmp) { }
+    Manager(net::Dispatcher& dispatcher) : cmp_(dispatcher) { }
 
     Manager(const Manager&) = delete;
     Manager& operator = (const Manager&) = delete;
+
+    //! Connect net::Group. Forwarded To ChannelMultiplexer.
+    void Connect(net::Group* group) {
+        cmp_.Connect(group);
+    }
+
+    //! Closes all client connections. Forwarded To ChannelMultiplexer.
+    void Close() {
+        cmp_.Close();
+    }
 
     //! returns iterator on requested partition or network channel.
     //!
@@ -131,7 +141,7 @@ public:
 
 private:
     static const bool debug = false;
-    net::ChannelMultiplexer& cmp_;
+    net::ChannelMultiplexer cmp_;
 
     BufferChainManager dias_;
 };
