a9d253d9111865a1e5b3085a0e96a82a021fa075
Alexander Noe <aleexnoe@gmail.com>
2015-06-02 22:15:33 +0200

    finished benchmark script

--- benchmarks/wordcount/bench.py
+++ benchmarks/wordcount/bench.py
@@ -4,7 +4,8 @@ import os
 import sys
 import subprocess
 import random
-#import numpy
+import numpy
+import re
 
 amounts=[10,12,14,16,18,20,22,24,26,28]
 
@@ -19,14 +20,20 @@ for worker in workers:
         for amount in amounts:
             results = []
             for _ in range(5):
-                process = subprocess.Popen(["../../build/examples/wc", "-n", str(worker), " -s ", str(pow(2, amount))], stdout=subprocess.PIPE)
+                process = subprocess.Popen(["../../build/examples/wc", "-n", str(worker), "-s", str(pow(2, amount))], stdout=subprocess.PIPE)
                 process.wait()
                 output = process.communicate()[0]
+                #print output
                 timers = [m.start() for m in re.finditer('timer', output)]
-                results.append(float(time))
-                print time
-            #median = numpy.median(results)
-            #print str(amount) + " " + str(median * 1000 / pow(2,amount))
-            #file1.write(str(amount) + " " + str(median * 1000 / pow(2,amount)) + "\n")
+                times = []
+                for timer in timers:
+                    afterTimer = output[timer+7:]
+                    spacePos = afterTimer.find(' ')
+                    times.append(int(afterTimer[:spacePos]))
+                results.append(max(times))
+                print max(times)
+            median = numpy.median(results)
+            print str(amount) + " " + str(median * 1000 / pow(2,amount))
+            file1.write(str(amount) + " " + str(median * 1000 / pow(2,amount)) + "\n")
         file1.close()
 
