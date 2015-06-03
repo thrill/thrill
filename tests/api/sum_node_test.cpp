7bcbb0f128d43de29b22a311f3e3de0e6dc7f896
Timo Bingmann <tbgit@panthema.net>
2015-06-03 16:16:36 +0200

    Correcting trivial merge problems and uncrustify.

--- tests/api/sum_node_test.cpp
+++ tests/api/sum_node_test.cpp
@@ -25,31 +25,28 @@ TEST(SumNode, SumNodeExample) {
     Context ctx;
     std::vector<std::string> self = { "127.0.0.1:1234" };
     ctx.job_manager().Connect(0, Endpoint::ParseEndpointList(self));
-    ctx.job_manager().StartDispatcher();
 
     auto map_fn = [](int in) {
-        std::cout << 2 * in << std::endl;
-        return 2 * in;
-    };
+                      std::cout << 2 * in << std::endl;
+                      return 2 * in;
+                  };
 
     auto input = ReadFromFileSystem(
-            ctx,
-            g_workpath + "/inputs/test1",
-            [](const std::string& line) {
-                return std::stoi(line);
-            });
+        ctx,
+        g_workpath + "/inputs/test1",
+        [](const std::string& line) {
+            return std::stoi(line);
+        });
 
     auto ints = input.Map(map_fn);
 
     auto sum_fn = [](int in1, int in2) {
-        return in1 + in2;
-    };
+                      return in1 + in2;
+                  };
 
     auto result = ints.Sum(sum_fn);
 
     ASSERT_EQ(272, result);
-
-    ctx.job_manager().StopDispatcher();
 }
 
 /******************************************************************************/
