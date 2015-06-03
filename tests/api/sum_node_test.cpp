ad244ca4df21f448adb78f4fba6ee8d318eb06a9
Matthias Stumpp <mstumpp@gmail.com>
2015-06-03 18:26:27 +0200

    added LocalhostTwoThread test

--- tests/api/sum_node_test.cpp
+++ tests/api/sum_node_test.cpp
@@ -16,10 +16,17 @@
 
 #include "gtest/gtest.h"
 
+#include <random>
+#include <thread>
+#include <string>
+
+#include <c7a/api/bootstrap.hpp>
+#include <c7a/api/dia.hpp>
+
 using namespace c7a::core;
 using namespace c7a::net;
 
-TEST(SumNode, SumNodeExample) {
+TEST(SumNode, LocalhostOneThread) {
     using c7a::Context;
 
     Context ctx;
@@ -27,7 +34,6 @@ TEST(SumNode, SumNodeExample) {
     ctx.job_manager().Connect(0, Endpoint::ParseEndpointList(self));
 
     auto map_fn = [](int in) {
-                      std::cout << 2 * in << std::endl;
                       return 2 * in;
                   };
 
@@ -47,6 +53,72 @@ TEST(SumNode, SumNodeExample) {
     auto result = ints.Sum(sum_fn);
 
     ASSERT_EQ(272, result);
+}
+
+int sum_node_generated(c7a::Context& ctx) {
+
+    auto map_fn = [](int in) {
+        std::cout << 2 * in << std::endl;
+        return 2 * in;
+    };
+
+    auto input = ReadFromFileSystem(
+            ctx,
+            g_workpath + "/inputs/test1",
+            [](const std::string& line) {
+                std::cout << "out: " << line << std::endl;
+                return std::stoi(line);
+            });
+
+    auto ints = input.Map(map_fn);
+
+    auto sum_fn = [](int in1, int in2) {
+        return in1 + in2;
+    };
+
+    auto result = ints.Sum(sum_fn);
+    //std::cout << result << std::endl;
+    return result;
+}
+
+TEST(SumNode, DISABLED_LocalhostTwoThreads) {
+
+    std::random_device random_device;
+    std::default_random_engine generator(random_device());
+    std::uniform_int_distribution<int> distribution(30000, 65000);
+    const size_t port_base = distribution(generator);
+
+    unsigned int workers = 2;
+
+    std::vector<std::thread> threads(workers);
+    std::vector<char**> arguments(workers);
+    std::vector<std::vector<std::string> > strargs(workers);
+
+    for (size_t i = 0; i < workers; i++) {
+
+        arguments[i] = new char*[workers + 2];
+        strargs[i].resize(workers + 2);
+
+        for (size_t j = 0; j < workers; j++) {
+            strargs[i][j + 2] += "127.0.0.1:";
+            strargs[i][j + 2] += std::to_string(port_base + j);
+            arguments[i][j + 2] = const_cast<char*>(strargs[i][j + 2].c_str());
+        }
+
+        std::function<int(c7a::Context&)> start_func = [](c7a::Context& ctx) {
+            return sum_node_generated(ctx);
+        };
+
+        strargs[i][0] = "sum node";
+        arguments[i][0] = const_cast<char*>(strargs[i][0].c_str());
+        strargs[i][1] = std::to_string(i);
+        arguments[i][1] = const_cast<char*>(strargs[i][1].c_str());
+        threads[i] = std::thread([=]() { c7a::Execute(workers + 2, arguments[i], start_func); });
+    }
+
+    for (size_t i = 0; i < workers; i++) {
+        threads[i].join();
+    }
 }
 
 /******************************************************************************/
