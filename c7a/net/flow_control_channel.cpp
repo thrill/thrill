294fa8e35925e33df949250d165e1738b27f389d
Emanuel Jöbstl <emiswelt@gmail.com>
2015-06-19 09:24:50 +0200

    Added copyright notice to net

--- c7a/net/flow_control_channel.cpp
+++ c7a/net/flow_control_channel.cpp
@@ -3,7 +3,8 @@
  *
  * Part of Project c7a.
  *
- *
+ * Copyright (C) 2015 Emanuel Jöbstl <emanuel.joebstl@gmail.com>
+ * 
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
 
