badab9c2428156dd0fbfe6508ee099b16813473b
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-07-09 00:17:24 -0400

    try serializing using cereal

--- tests/data/serializer_objects.hpp
+++ tests/data/serializer_objects.hpp
@@ -7,9 +7,21 @@ namespace serializers {
 struct TestSerializeObject;
 
 struct TestSerializeObject {
-    TestSerializeObject(int bla, int blu) : bla_(bla), blu_(blu) { }
+    TestSerializeObject(int bla, int blu) : bla_(bla), blu_(blu) { };
     int bla_;
     int blu_;
+};
+
+struct TestCerealObject {
+    // TestCerealObject();
+    // TestCerealObject(int x, int y, int z) : x_(x), y_(y), z_(z) { };
+    int x_, y_, z_;
+
+    // This method lets cereal know which data members to serialize
+    template<class Archive>
+    void serialize(Archive & archive) {
+        archive( x_, y_, z_ ); // serialize things by passing them to the archive
+    }
 };
 
 }
