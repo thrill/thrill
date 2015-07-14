76231b496f022274395de196d14e457f183d87a3
Tobias Sturm <mail@tobiassturm.de>
2015-07-14 14:09:54 +0200

    add data::Stream
    Stream is an adapter from BlockWriter target to
    callback interface (observer pattern)
    
    http://giphy.com/gifs/funny-cheezburger-fancy-sEueensqIsLpm

---
+++ c7a/data/stream.hpp
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * c7a/data/stream.hpp
+ *
+ * Part of Project c7a.
+ *
+ * Copyright (C) 2015 Tobias Sturm <tobias.sturm@student.kit.edu>
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#pragma once
+#ifndef C7A_DATA_STREAM_HEADER
+#define C7A_DATA_STREAM_HEADER
+
+#include <functional>
+#include <vector>
+#include <memory>
+
+#include <c7a/common/logger.hpp>
+#include <c7a/data/file.hpp>
+
+namespace c7a {
+namespace data {
+
+//! Stream is a BlockWriter-Target that triggers callbacks it's observer.
+//! There can be multiple observers for each stream, but at least one.
+//! Observers cannot be removed once added.
+template <size_t BlockSize>
+class Stream
+{
+public:
+    using BlockPtr = std::shared_ptr<Block<BlockSize> >;
+    using AppendCallbackFunction = std::function<void(const Stream<BlockSize>&, const BlockPtr&, size_t, size_t, size_t)>;
+    using CloseCallbackFunction = std::function<void(const Stream<BlockSize>&)>;
+
+    //! Append a block to this file, the block must contain given number of
+    //! items after the offset first.
+    void Append(const BlockPtr& block, size_t block_used,
+                size_t nitems, size_t first) const {
+        die_unless(!closed_);
+        die_unless(append_observers_.size() > 0);
+
+        for (auto& o : append_observers_) {
+            o(*this, block, block_used, nitems, first);
+        }
+    }
+
+    void OnAppend(AppendCallbackFunction callback_function) {
+        append_observers_.push_back(callback_function);
+    }
+
+    void OnClose(CloseCallbackFunction callback_function) {
+        close_observers_.push_back(callback_function);
+    }
+
+    void Close() {
+        die_unless(!closed_);
+        for (auto& o : close_observers_) {
+            o(*this);
+        }
+    }
+
+private:
+    //! closed streams won't fire callbacks nor accept 'Append' / 'Close' calls
+    bool closed_ = { false };
+
+    std::vector<AppendCallbackFunction> append_observers_;
+    std::vector<CloseCallbackFunction> close_observers_;
+};
+} // namespace data
+} // namespace c7a
+
+#endif // !C7A_DATA_STREAM_HEADER
+
+/******************************************************************************/
