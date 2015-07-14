71f33a11bc78b0e9efe3f8a376734e9fb244a57e
Timo Bingmann <tbgit@panthema.net>
2015-07-14 20:17:46 +0200

    Fixing data::Manager interface: GetNewChannel() delivers a Channel object (shptr).

--- c7a/api/reduce_to_index.hpp
+++ c7a/api/reduce_to_index.hpp
@@ -102,7 +102,7 @@ public:
           DOpNode<ValueType>(ctx, { parent }),
           key_extractor_(key_extractor),
           reduce_function_(reduce_function),
-          channel_id_(ctx.data_manager().AllocateNetworkChannel()),
+          channel_id_(ctx.data_manager().AllocateChannelId()),
           emitters_(ctx.data_manager().
                     template GetNetworkEmitters<KeyValuePair>(channel_id_)),
           reduce_pre_table_(ctx.number_worker(), key_extractor,
