ab1eb3f916119c132ebefd8baeb28a97bdc73d91
Timo Bingmann <tbgit@panthema.net>
2015-07-14 22:41:06 +0200

    Removing OrderedBufferChain.

--- c7a/net/channel_multiplexer.hpp
+++ c7a/net/channel_multiplexer.hpp
@@ -129,7 +129,11 @@ public:
 
         for (size_t worker_id = 0; worker_id < group_->Size(); worker_id++) {
             if (worker_id == group_->MyRank()) {
-                auto target = std::make_shared<data::LoopbackTarget>(chains_.Chain(id), [=]() { sLOG << "loopback closes" << id; GetOrCreateChannel(id)->CloseLoopback(); });
+                auto target = std::make_shared<data::LoopbackTarget>(
+                    chains_.Chain(id), [=]() {
+                        sLOG << "loopback closes" << id;
+                        GetOrCreateChannel(id)->CloseLoopback();
+                    });
                 result.emplace_back(data::Emitter<T>(target));
             }
             else {
