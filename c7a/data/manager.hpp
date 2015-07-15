c22b512ba6ad48c2d72da27ba1b07de13d0818ce
Tobias Sturm <mail@tobiassturm.de>
2015-07-15 12:28:15 +0200

    reset manager to use old stuff

--- c7a/data/manager.hpp
+++ c7a/data/manager.hpp
@@ -11,14 +11,17 @@
 #ifndef C7A_DATA_MANAGER_HEADER
 #define C7A_DATA_MANAGER_HEADER
 
+#include <c7a/api/input_line_iterator.hpp>
 #include <c7a/common/logger.hpp>
+#include <c7a/data/buffer_chain.hpp>
+#include <c7a/data/buffer_chain_manager.hpp>
+#include <c7a/data/emitter.hpp>
 #include <c7a/data/file.hpp>
 #include <c7a/data/iterator.hpp>
 #include <c7a/data/output_line_emitter.hpp>
 #include <c7a/data/socket_target.hpp>
 #include <c7a/data/channel.hpp>
 #include <c7a/data/channel_multiplexer.hpp>
-#include <c7a/data/repository.hpp>
 
 #include <functional>
 #include <map>
@@ -27,119 +30,145 @@
 #include <string>
 
 namespace c7a {
-namespace net {
-class Group;
-}
 namespace data {
 
-using FilePtr = std::shared_ptr<File>;
+struct BufferChain;
 
 //! Identification for DIAs
-using FilePtr = std::shared_ptr<File>
+typedef ChainId DIAId;
+
 //! Manages all kind of memory for data elements
 //!
 //!
 //! Provides Channel creation for sending / receiving data from other workers.
-                class Manager
-      {
-      public:
-          Manager(net::DispatcherThread& dispatcher)
-              : cmp_(dispatcher) { }
+class Manager
+{
+public:
+    Manager(net::DispatcherThread& dispatcher)
+        : cmp_(dispatcher) { }
 
-          //! non-copyable: delete copy-constructor
-          Manager(const Manager&) = delete;
-          //! non-copyable: delete assignment operator
-          Manager& operator = (const Manager&) = delete;
+    //! non-copyable: delete copy-constructor
+    Manager(const Manager&) = delete;
+    //! non-copyable: delete assignment operator
+    Manager& operator = (const Manager&) = delete;
 
-          //! Connect net::Group. Forwarded To ChannelMultiplexer.
-          void Connect(net::Group* group) {
-              cmp_.Connect(group);
-          }
+    //! Connect net::Group. Forwarded To ChannelMultiplexer.
+    void Connect(net::Group* group) {
+        cmp_.Connect(group);
+    }
 
-          //! Closes all client connections, files and everything.
-          void Close() {
-              for (auto& f : files) {
-                  f.second.Close();
-              }
-              cmp_.Close();
-          }
+    //! Closes all client connections. Forwarded To ChannelMultiplexer.
+    void Close() {
+        cmp_.Close();
+    }
 
-          //! returns iterator on requested partition or network channel.
-          //!
-          //! Data can be emitted into this partition / received on the channel even
-          //! after the iterator was created.
-          //!
-          //! \param id ID of the DIA / Channel - determined by AllocateDIA() / AllocateNetworkChannel()
-          template <class T>
-          Iterator<T> GetIterator(const ChainId& id) {
-              return Iterator<T>(*GetChainOrDie(id));
-          }
+    //! returns iterator on requested partition or network channel.
+    //!
+    //! Data can be emitted into this partition / received on the channel even
+    //! after the iterator was created.
+    //!
+    //! \param id ID of the DIA / Channel - determined by AllocateDIA() / AllocateNetworkChannel()
+    template <class T>
+    Iterator<T> GetIterator(const ChainId& id) {
+        return Iterator<T>(*GetChainOrDie(id));
+    }
 
-          BlockReader<BlockSize> GetBlockReader(const DataId& id) {
-              return BlockReader<BlockSize>(GetFileOrDie(id));
-          }
+    //! Returns the number of elements that are stored on this worker
+    //! Returns -1 if the channel or dia was not closed (yet)
+    //! This call is blocking until the DIA is closed
+    //! throws if id is unknown
+    size_t GetNumElements(const ChainId& id) {
+        std::shared_ptr<BufferChain> chain = GetChainOrDie(id);
+        if (!chain->IsClosed())
+            chain->WaitUntilClosed();
+        return chain->size();
+    }
 
 #if FIXUP_LATER
-          //! Docu see net::ChannelMultiplexer::Scatter()
-          template <class T>
-          void Scatter(const DataId& source, const DataId& target, std::vector<size_t> offsets) {
-              assert(source.type == LOCAL);
-              assert(target.type == NETWORK);
-              assert(files_.Contains(source));
-              cmp_.Scatter<T>(files_(source), target, offsets);
-          }
+    //! Docu see net::ChannelMultiplexer::Scatter()
+    template <class T>
+    void Scatter(const ChainId& source, const ChainId& target, std::vector<size_t> offsets) {
+        assert(source.type == LOCAL);
+        assert(target.type == NETWORK);
+        assert(dias_.Contains(source));
+        cmp_.Scatter<T>(dias_.Chain(source), target, offsets);
+    }
 #endif      // FIXUP_LATER
 
-          //! Returns a number that uniquely addresses a File
-          //! Calls to this method alter the data managers state.
-          //! Calls to this method must be in deterministic order for all workers!
-          DataId AllocateFileId() {
-              return files_.AllocateNext();
-          }
+    //! Returns a number that uniquely addresses a DIA
+    //! Calls to this method alter the data managers state.
+    //! Calls to this method must be in deterministic order for all workers!
+    DIAId AllocateDIA() {
+        return dias_.AllocateNext();
+    }
 
-          //! Returns a number that uniquely addresses a network channel
-          //! Calls to this method alter the data managers state.
-          //! Calls to this method must be in deterministic order for all workers!
-          //! \param order_preserving indicates if the channel should preserve the order of the receiving packages
-          ChannelId AllocateChannelId() {
-              return cmp_.AllocateNext();
-          }
+    //! Returns a number that uniquely addresses a network channel
+    //! Calls to this method alter the data managers state.
+    //! Calls to this method must be in deterministic order for all workers!
+    //! \param order_preserving indicates if the channel should preserve the order of the receiving packages
+    ChannelId AllocateChannelId() {
+        return cmp_.AllocateNext();
+    }
 
-          //! Returns a reference to an existing Channel.
-          std::shared_ptr<net::Channel> GetChannel(const ChannelId id) {
-              assert(cmp_.HasChannel(id));
-              return std::move(cmp_.GetOrCreateChannel(id));
-          }
+    //! Returns a reference to an existing Channel.
+    std::shared_ptr<net::Channel> GetChannel(const ChannelId id) {
+        assert(cmp_.HasChannel(id));
+        return std::move(cmp_.GetOrCreateChannel(id));
+    }
 
-          //! Returns a reference to a new Channel.
-          std::shared_ptr<net::Channel> GetNewChannel() {
-              return std::move(cmp_.GetOrCreateChannel(AllocateChannelId()));
-          }
+    //! Returns a reference to a new Channel.
+    std::shared_ptr<net::Channel> GetNewChannel() {
+        return std::move(cmp_.GetOrCreateChannel(AllocateChannelId()));
+    }
 
-          //! Returns a new, anonymous File object containing a sequence of local Blocks.
-          FilePtr GetFile() {
-              return File();
-          }
+    //! Returns an emitter that can be used to fill a DIA
+    //! Emitters can push data into DIAs even if an intertor was created before.
+    //! Data is only visible to the iterator if the emitter was flushed.
+    // template <class T>
+    // Emitter<T> GetLocalEmitter(DIAId id) {
+    //     assert(id.type == LOCAL);
+    //     if (!dias_.Contains(id)) {
+    //         throw std::runtime_error("target dia id unknown.");
+    //     }
+    //     return Emitter<T>(dias_.Chain(id));
+    // }
 
-          //! Returns a named file that has been allocated via \ref AllocateFileId()
-          FilePtr GetFile(const DataId& id) {
-              return data_(id);
-          }
+    //! Returns a new File object containing a sequence of local Blocks.
+    File GetFile() {
+        return File();
+    }
 
-          template <class T>
-          std::vector<BlockWriter<T> > GetNetworkEmitters(DataId id) {
-              assert(id.type == NETWORK);
-              if (!cmp_.HasDataOn(id)) {
-                  throw std::runtime_error("target channel id unknown.");
-              }
-              return cmp_.OpenChannel(id);
-          }
+    template <class T>
+    std::vector<Emitter> GetNetworkEmitters(ChannelId id) {
+        assert(id.type == NETWORK);
+        if (!cmp_.HasDataOn(id)) {
+            throw std::runtime_error("target channel id unknown.");
+        }
+        return cmp_.OpenChannel(id);
+    }
 
-      private:
-          static const bool debug = false;
-          net::ChannelMultiplexer cmp_;
-          DataRepository<File> data_;
-      };
+private:
+    static const bool debug = false;
+    net::ChannelMultiplexer cmp_;
+
+    BufferChainManager dias_;
+
+    std::shared_ptr<BufferChain> GetChainOrDie(const ChainId& id) {
+        if (id.type == LOCAL) {
+            if (!dias_.Contains(id)) {
+                throw std::runtime_error("target dia id unknown.");
+            }
+            return dias_.Chain(id);
+        }
+        else {
+            if (!cmp_.HasDataOn(id)) {
+                throw std::runtime_error("target channel id unknown.");
+            }
+            return cmp_.AccessData(id);
+        }
+    }
+};
+
 } // namespace data
 } // namespace c7a
 
