5c18d1de25f24599f0446cdcb52f985e0c7e54e1
Tobias Sturm <mail@tobiassturm.de>
2015-07-15 11:57:48 +0200

    block_queue test use test fixture & add licence

--- tests/data/block_queue_test.cpp
+++ tests/data/block_queue_test.cpp
@@ -3,6 +3,7 @@
  *
  * Part of Project c7a.
  *
+ * Copyright (C) 2015 Tobias Sturm <tobias.sturm@student.kit.edu>
  *
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
@@ -12,33 +13,30 @@
 
 using namespace c7a::data;
 
-struct BlockQueueTest : public::testing::Test { };
+struct BlockQueueTest : public::testing::Test {
+    BlockQueue<16> q;
+};
 
 TEST_F(BlockQueueTest, FreshQueueIsNotClosed) {
-    BlockQueue<16> q;
     ASSERT_FALSE(q.closed());
 }
 
 TEST_F(BlockQueueTest, QueueCanBeClosed) {
-    BlockQueue<16> q;
     q.Close();
     ASSERT_TRUE(q.closed());
 }
 
 TEST_F(BlockQueueTest, FreshQueueIsEmpty) {
-    BlockQueue<16> q;
     ASSERT_TRUE(q.empty());
 }
 
 TEST_F(BlockQueueTest, QueueNonEmptyAfterAppend) {
-    BlockQueue<16> q;
     std::shared_ptr<Block<16> > block;
     q.Append(block, 0, 0, 0);
     ASSERT_FALSE(q.empty());
 }
 
 TEST_F(BlockQueueTest, BlockWriterToQueue) {
-    BlockQueue<16> q;
     BlockWriter<Block<16>, BlockQueue<16> > bw(q);
     bw(int(42));
     bw(std::string("hello there BlockQueue"));
