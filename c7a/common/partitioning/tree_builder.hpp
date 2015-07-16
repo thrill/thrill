c5e0e60f23b6476c914e23cd8a8f19a71b4aec31
Alexander Noe <aleexnoe@gmail.com>
2015-07-16 10:21:47 +0200

    Apply our codestyle in c7a/common/partitioning/tree_builder.hpp

--- c7a/common/partitioning/tree_builder.hpp
+++ c7a/common/partitioning/tree_builder.hpp
@@ -1,47 +1,43 @@
 #pragma once
 
-template <typename value_type>
+namespace c7a {
+namespace sort {
+
+template <typename ValueType>
 struct TreeBuilder
 {
 
-    value_type* m_tree;
-    value_type* m_samples;
-    unsigned int m_index;
+    ValueType* tree_;
+    ValueType* samples_;
+    size_t index_;
     size_t ssplitter;
 
-    TreeBuilder(value_type* splitter_tree, // Target: tree. Size of 'number of splitter'
-                value_type* samples, // Source: sorted splitters. Size of 'number of splitter'
+    TreeBuilder(ValueType* splitter_tree, // Target: tree. Size of 'number of splitter'
+                ValueType* samples, // Source: sorted splitters. Size of 'number of splitter'
                 size_t ssplitter) // Number of splitter
-        : m_tree( splitter_tree ),
-          m_samples( samples ),
-          m_index( 0 ),
+        : tree_( splitter_tree ),
+          samples_( samples ),
+          index_( 0 ),
           ssplitter(ssplitter)
     {
         recurse(samples, samples + ssplitter, 1);
     }
 
-    ssize_t snum(value_type* s) const
+    ssize_t snum(ValueType* s) const
     {
-        return (ssize_t)(s - m_samples);
+        return (ssize_t)(s - samples_);
     }
 
-    value_type recurse(value_type* lo, value_type* hi, unsigned int treeidx)
+    ValueType recurse(ValueType* lo, ValueType* hi, unsigned int treeidx)
     {
 
         // pick middle element as splitter
-        value_type* mid = lo + (ssize_t)(hi - lo) / 2;
+        ValueType* mid = lo + (ssize_t)(hi - lo) / 2;
 
-        value_type mykey = m_tree[treeidx] = *mid;
-#if 0
-        value_type* midlo = mid;
-        while (lo < midlo && *(midlo-1) == mykey) midlo--;
-
-        value_type* midhi = mid;
-        while (midhi+1 < hi && *midhi == mykey) midhi++;
-
-#else
-        value_type *midlo = mid, *midhi = mid+1;
-#endif
+        ValueType mykey = tree_[treeidx] = *mid;
+        
+        ValueType *midlo = mid, *midhi = mid+1;
+        
         if (2 * treeidx < ssplitter)
         {
             recurse(lo, midlo, 2 * treeidx + 0);
@@ -54,3 +50,5 @@ struct TreeBuilder
         }
     }
 };
+} //sort
+} //c7a
