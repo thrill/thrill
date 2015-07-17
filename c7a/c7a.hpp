ff1a76814471fdafd975d4bc3c4afb4b0be6ac36
Timo Bingmann <tbgit@panthema.net>
2015-07-17 18:43:14 +0200

    Attempting to fix SizeNode, which DID NOT HAVE A TEST. Added it somewhere, but stage building does not work as expected.

--- c7a/c7a.hpp
+++ c7a/c7a.hpp
@@ -23,10 +23,10 @@
 #include <c7a/api/read.hpp>
 #include <c7a/api/reduce.hpp>
 #include <c7a/api/reduce_to_index.hpp>
+#include <c7a/api/size.hpp>
 #include <c7a/api/sum.hpp>
 #include <c7a/api/write.hpp>
 //-tb: #include <c7a/api/zip.hpp>
-#include <c7a/api/size.hpp>
 
 namespace c7a {
 
