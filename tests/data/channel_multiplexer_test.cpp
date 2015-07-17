71127e3598dbac170a15bc2aef302343ce99ec0c
Tobias Sturm <mail@tobiassturm.de>
2015-07-17 17:20:07 +0200

    add test for Channel::ReadCompleteChannel

--- tests/data/channel_multiplexer_test.cpp
+++ tests/data/channel_multiplexer_test.cpp
@@ -4,19 +4,86 @@
  * Part of Project c7a.
  *
  * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+ * Copyright (C) 2015 Tobias Sturm  <tobias.sturm@student.kit.edu>
  *
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
 
 #include <c7a/data/channel_multiplexer.hpp>
 #include <c7a/data/channel.hpp>
+#include <c7a/data/manager.hpp>
+#include <c7a/net/group.hpp>
+#include <c7a/net/dispatcher_thread.hpp>
+#include <c7a/common/cyclic_barrier.hpp>
 
 #include <gtest/gtest.h>
 #include <string>
 
 using namespace c7a;
+using namespace c7a::common;
+using namespace c7a::net;
+using Manager = c7a::data::Manager;
+using Group = c7a::net::Group;
 
-static const bool debug = false;
+static const bool debug = true;
+
+struct ChannelMultiplexerTest : public::testing::Test {
+    ChannelMultiplexerTest()
+        : dispatcher("dispatcher"), manager(dispatcher), single_group(0, 1) {
+        manager.Connect(&single_group);
+    }
+
+    using WorkerThread = std::function<void(Manager&)>;
+    void FunctionSelect(Group* group, WorkerThread f1, WorkerThread f2, WorkerThread f3 = [](Manager&) { }) {
+        Manager manager(dispatcher);
+        manager.Connect(group);
+        switch (group->MyRank()) {
+        case 0:
+            GetThreadDirectory().NameThisThread("t0");
+            f1(manager);
+            break;
+        case 1:
+            GetThreadDirectory().NameThisThread("t1");
+            f2(manager);
+            break;
+        case 2:
+            GetThreadDirectory().NameThisThread("t2");
+            f3(manager);
+            break;
+        }
+        barrier->Await();
+    }
+    void Execute(WorkerThread f1, WorkerThread f2, WorkerThread f3) {
+        barrier = new Barrier(3);
+        Group::ExecuteLocalMock(3,
+                                [ = ](Group* g) {
+                                    FunctionSelect(g, f1, f2, f3);
+                                });
+        free(barrier);
+    }
+
+    void Execute(WorkerThread f1, WorkerThread f2) {
+        barrier = new Barrier(2);
+        Group::ExecuteLocalMock(2,
+                                [ = ](Group* g) {
+                                    FunctionSelect(g, f1, f2);
+                                });
+        free(barrier);
+    }
+
+    void Execute(WorkerThread f1) {
+        barrier = new Barrier(1);
+        Group::ExecuteLocalMock(1,
+                                [ = ](Group* g) {
+                                    FunctionSelect(g, f1, [](Manager&) { });
+                                });
+        free(barrier);
+    }
+    DispatcherThread dispatcher;
+    Manager          manager;
+    Group            single_group;
+    Barrier          * barrier;
+};
 
 // open a Channel via data::Manager, and send a short message to all workers,
 // receive and check the message.
@@ -68,7 +135,7 @@ void TalkAllToAllViaChannel(net::Group* net) {
 
             sLOG << net->MyRank() << "got msg from" << src;
 
-            // write a few MiBs of oddly sized data
+            // read a few MiBs of oddly sized data
             unsigned char recv_buffer[sizeof(send_buffer)];
 
             for (size_t r = 0; r != iterations; ++r) {
@@ -87,6 +154,50 @@ TEST(ChannelMultiplexer, TalkAllToAllViaChannelForManyNetSizes) {
     for (size_t i = 1; i <= 8; ++i) {
         net::Group::ExecuteLocalMock(i, TalkAllToAllViaChannel);
     }
+}
+
+TEST_F(ChannelMultiplexerTest, ReadCompleteChannel) {
+    Barrier sync(3);
+    auto w0 = [&sync](Manager& manager) {
+                  auto c = manager.GetNewChannel();
+                  auto writers = c->OpenWriters();
+                  std::string msg1 = "I came from worker 0";
+                  std::string msg2 = "I am another message from worker 0";
+                  writers[2](msg1);
+                  //writers[2].Flush();
+                  writers[2](msg2);
+                  for (auto& w : writers) {
+                      sLOG << "close worker";
+                      w.Close();
+                  }
+              };
+    auto w1 = [&sync](Manager& manager) {
+                  auto c = manager.GetNewChannel();
+                  auto writers = c->OpenWriters();
+                  std::string msg1 = "I came from worker 1";
+                  writers[2](msg1);
+                  for (auto& w : writers) {
+                      sLOG << "close worker";
+                      w.Close();
+                  }
+              };
+    auto w2 = [&sync](Manager& manager) {
+                  auto c = manager.GetNewChannel();
+                  auto writers = c->OpenWriters();
+                  for (auto& w : writers) {
+                      sLOG << "close worker";
+                      w.Close();
+                  }
+                  auto file = c->ReadCompleteChannel();
+                  for (size_t i = 0; i < file.NumBlocks(); i++)
+                      sLOG << "block" << i << file.BlockAsString(i);
+
+                  auto reader = file.GetReader();
+                  ASSERT_EQ("I came from worker 0", reader.Next<std::string>());
+                  ASSERT_EQ("I am another message from worker 0", reader.Next<std::string>());
+                  ASSERT_EQ("I came from worker 1", reader.Next<std::string>());
+              };
+    Execute(w0, w1, w2);
 }
 
 /******************************************************************************/
