b04f5f8524461b97f9bd294ffc9bb176b6be7b81
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-20 18:34:46 +0200

    Added some debug output in case of error

--- scripts/slurmCheck.sh
+++ scripts/slurmCheck.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
+function printVars {
+  echo "SLURM_STEP_NUM_TASKS: $SLURM_STEP_NUM_TASKS"
+  echo "SLURM_NUM_NODES: $SLURM_NUM_NODES"
+  echo "SLURM_JOB_HOSTLIST: $SLURM_JOB_NODELIST"
+  echo "SLURM_PROCID: $SLURM_PROCID"
+}
+
+
 if [ -z $SLURM_STEP_NUM_TASKS ]; then
   echo "Error: SLURM environment not found."
+  printVars
   exit -1
 fi
 
 if [ $SLURM_STEP_NUM_TASKS -gt $SLURM_NUM_NODES ]; then 
   echo "Error: Multiple c7a instances running on a single node."
+  printVars
   exit -1
 fi
 
