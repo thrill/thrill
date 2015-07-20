2588f542bf744bd6f4dfc423485a48e3adffbff4
emiswelt <emi@eex-dev.net>
2015-07-20 19:09:47 +0200

    Fixed corner case (1 item in hostlist) II

--- scripts/getSlurmRank.sh
+++ scripts/getSlurmRank.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 HOSTS=$(./getSlurmHostlist.sh)
 HOST=$(hostname)
-echo $HOSTS | awk 'BEGIN { FS=" " } { for(i = 1; i < NF; i++) { if($i == "'$HOST'") { printf "%s", (i - 1) } } }'
+ID=$(echo $HOSTS | awk 'BEGIN { FS=" " } { for(i = 1; i < NF; i++) { if($i == "'$HOST'") { printf "%s", (i - 1) } } }')
+
+if [ -z $ID ]; then
+  echo "0"
+else 
+  echo "$ID"
+fi
 
