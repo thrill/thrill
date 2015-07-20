1d02095fbf9c2b59631b252af29c0419773d8f76
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-20 18:08:15 +0200

    Added a c7a invoke script

--- scripts/getSlurmHostlist.sh
+++ scripts/getSlurmHostlist.sh
@@ -2,4 +2,4 @@
 JOBS=$SLURM_JOB_NODELIST
 BASE=$(echo $JOBS | awk 'BEGIN { FS="[" } { print $1 }')
 REST=$(echo $JOBS | awk 'BEGIN { FS="(\[|\])" } { print $2 }')
-echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { print "'$BASE'"$i } }'
+echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { printf "%s%s ", "'$BASE'", $i } }'
