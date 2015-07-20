01b0cf60328d4c52a8641a7bcbbe7566f4e256cc
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-20 16:49:52 +0200

    Added hostname extraction script

---
+++ scripts/getBwHpcHostlist.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+JOBS=$SLURM_JOB_NODELIST
+BASE=$(echo $JOBS | awk 'BEGIN { FS="[" } { print $1 }')
+REST=$(echo $JOBS | awk 'BEGIN { FS="(\[|\])" } { print $2 }')
+echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { print "'$BASE'"$i } }'
