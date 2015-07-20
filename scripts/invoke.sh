add6fbdadb3cf79392abe9c9eb7e966d0a3f7f2f
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-20 18:52:06 +0200

    Added new method for getting host rank

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -12,4 +12,5 @@ COMMAND="../build/examples/word_count"
 HOSTLIST=$(./getSlurmHostlist.sh | ./formatPort.sh $PORT)
 MY_RANK=$(./getSlurmRank.sh)
 EX="$COMMAND -r $MY_RANK $HOSTLIST"
+echo "Invoking: $COMMAND"
 eval $EX
