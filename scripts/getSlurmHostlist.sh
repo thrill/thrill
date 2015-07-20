63a7ea5c2b142cd517ad98bc7f189003f03f82a9
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-20 19:05:55 +0200

    Fixed corner case (1 item in hostlist)

--- scripts/getSlurmHostlist.sh
+++ scripts/getSlurmHostlist.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 JOBS=$SLURM_JOB_NODELIST
 BASE=$(echo $JOBS | awk 'BEGIN { FS="[" } { print $1 }')
-REST=$(echo $JOBS | awk 'BEGIN { FS="(\[|\])" } { print $2 }')
-echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { printf "%s%s ", "'$BASE'", $i } }'
+REST=$(echo $JOBS | awk 'BEGIN { FS="([|])" } { print $2 }')
+
+if [ -z $REST ]; then
+  #1 item in list
+  echo $JOBS
+else
+  #n items in list
+  echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { printf "%s%s ", "'$BASE'", $i } }'
+fi
