9d16f49bf174ea2467a9f3474521fad8ac51d338
emiswelt <emi@eex-dev.net>
2015-07-22 01:14:36 +0200

    SLURM gives ranges inside hostlists. We obey.

--- scripts/getSlurmHostlist.sh
+++ scripts/getSlurmHostlist.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+
+#example hostlist:  uc1n[068,074,080-082,134,139,141,152,156]
 JOBS=$SLURM_JOB_NODELIST
+
+#gets the first part of a hostlist, for instance uc1n
 BASE=$(echo $JOBS | awk 'BEGIN { FS="[" } { print $1 }')
+
+#gets the enumeration of a hostlist, e.g. 068, 071, 080-082, ...
 REST=$(echo $JOBS | awk 'BEGIN { FS="(\\[|\\])" } { print $2 }')
 
 if [ -z "$REST" ]; then
@@ -8,5 +14,27 @@ if [ -z "$REST" ]; then
   echo $JOBS
 else
   #n items in list
-  echo $REST | awk 'BEGIN { FS=","} { for(i = 1; i <= NF; i++) { printf "%s%s ", "'$BASE'", $i } }'
+
+  #converts the parts of the hostlist to an enumeration
+  #of all hostnames, seperated by blanks. Also takes care 
+  # of concatenating the pre- and posfixes of hostnames and also 
+  #handles and expands ranges. 
+  echo $REST | awk '
+BEGIN { FS=","} 
+{ 
+  for(i = 1; i <= NF; i++) {
+    if($i ~ /-/) {
+      n = split($i, borders, "-")
+      if(n == 2) {
+        for(j = borders[1]; j <= borders[2]; j++) {
+          printf "%s%s ", "'$BASE'", j 
+        } 
+      } else {
+        exit -1
+      }
+    } else { 
+      printf "%s%s ", "'$BASE'", $i 
+    }
+  } 
+}'
 fi
