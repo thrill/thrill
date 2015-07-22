8003bf38726d92d9d48f6ce4729d53c4f75a124c
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-07-22 17:07:49 +0200

    adding an implementation and test for customized archive in cereal

--- tests/data/serializer_objects.hpp
+++ tests/data/serializer_objects.hpp
@@ -1,10 +1,9 @@
 #pragma once
+#include <cereal/archives/c7a.hpp>
 
 namespace c7a {
 namespace data {
 namespace serializers {
-
-struct TestSerializeObject;
 
 struct TestSerializeObject {
     TestSerializeObject(int bla, int blu) : bla_(bla), blu_(blu) { }
@@ -13,8 +12,6 @@ struct TestSerializeObject {
 };
 
 struct TestCerealObject {
-    // TestCerealObject();
-    // TestCerealObject(int x, int y, int z) : x_(x), y_(y), z_(z) { };
     int x_, y_, z_;
 
     // This method lets cereal know which data members to serialize
@@ -25,14 +22,20 @@ struct TestCerealObject {
 };
 
 struct TestCerealObject2 {
-    // TestCerealObject();
-    // TestCerealObject(int x, int y, int z) : x_(x), y_(y), z_(z) { };
-    int x_, y_, z_;
+    TestCerealObject2() { }
+    TestCerealObject2(int x, int y, int z) : x_(x), y_(y), z_(z) {
+        tco.x_ = x_;
+        tco.y_ = y_;
+        tco.z_ = z_;
+    }
+
+    int              x_, y_, z_;
+    TestCerealObject tco;
 
     // This method lets cereal know which data members to serialize
     template <class Archive>
     void serialize(Archive& archive) {
-        archive(x_, y_, z_);   // serialize things by passing them to the archive
+        archive(x_, y_, z_, tco);   // serialize things by passing them to the archive
     }
 };
 }
