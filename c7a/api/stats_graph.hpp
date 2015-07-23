bbf396f4d2cc3c6eb814df4d2cf0a2ac02d6d44a
Timo Bingmann <tbgit@panthema.net>
2015-07-23 16:38:13 +0200

    Uncrustify.

--- c7a/api/stats_graph.hpp
+++ c7a/api/stats_graph.hpp
@@ -1,32 +1,34 @@
 /*******************************************************************************
- * c7a/api/graph.hpp
+ * c7a/api/stats_graph.hpp
  *
  * Simple Graph to represent Execution Stages
  *
  * Part of Project c7a.
  *
+ *
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
 
 #pragma once
-#ifndef C7A_API_GRAPH_HEADER
-#define C7A_API_GRAPH_HEADER
+#ifndef C7A_API_STATS_GRAPH_HEADER
+#define C7A_API_STATS_GRAPH_HEADER
 
 #include <c7a/common/logger.hpp>
 
+#include <fstream>
 #include <string>
 #include <vector>
-#include <fstream>
 
 namespace c7a {
 namespace api {
 
-class StatsNode {
+class StatsNode
+{
 public:
-    StatsNode(const std::string& type) 
+    StatsNode(const std::string& type)
         : type_(type) { }
 
-    StatsNode(const StatsNode& other) = delete; 
+    StatsNode(const StatsNode& other) = delete;
 
     void AddNeighbor(StatsNode* const neighbor) {
         adjacent_nodes_.push_back(neighbor);
@@ -49,19 +51,19 @@ private:
     std::vector<StatsNode*> adjacent_nodes_;
 
     //! Type of node
-    std::string type_; 
+    std::string type_;
 };
 
 class StatsGraph
 {
 public:
-    StatsGraph(const std::string& path_out) 
+    StatsGraph(const std::string& path_out)
         : path_out_(path_out),
           file_(path_out_) { }
 
-    StatsGraph(const StatsGraph& other) = delete; 
+    StatsGraph(const StatsGraph& other) = delete;
 
-    virtual ~StatsGraph() { 
+    virtual ~StatsGraph() {
         for (auto node : nodes_) {
             delete node;
             node = NULL;
@@ -78,7 +80,7 @@ public:
         }
     }
 
-    void BuildLayout() { 
+    void BuildLayout() {
         file_ << "digraph {\n";
         for (const auto& node : nodes_) {
             for (const auto& neighbor : node->adjacent_nodes()) {
@@ -103,6 +105,6 @@ private:
 } // namespace api
 } // namespace c7a
 
-#endif // !C7A_API_GRAPH_HEADER
+#endif // !C7A_API_STATS_GRAPH_HEADER
 
 /******************************************************************************/
