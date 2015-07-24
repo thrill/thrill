6c0b577cd47449350da8ca9ffdd62680d324a8b6
Tobias Sturm <mail@tobiassturm.de>
2015-07-24 09:48:32 +0200

    fix copyright ö for Emi

--- c7a/net/flow_control_manager.hpp
+++ c7a/net/flow_control_manager.hpp
@@ -3,7 +3,7 @@
  *
  * Part of Project c7a.
  *
- * Copyright (C) 2015 Emanuel Jbstl <emanuel.joebstl@gmail.com>
+ * Copyright (C) 2015 Emanuel Jöbstl <emanuel.joebstl@gmail.com>
  *
  * This file has no license. Only Chunk Norris can compile it.
  ******************************************************************************/
