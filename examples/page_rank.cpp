f17ff5a58ff854ea502cc52c41a297afa8b738a1
Timo Bingmann <tbgit@panthema.net>
2015-07-27 11:12:05 +0200

    page rank example compiles now, but no idea if it works as expected.

--- examples/page_rank.cpp
+++ examples/page_rank.cpp
@@ -65,60 +65,59 @@ int page_rank(Context& ctx) {
                  return std::make_tuple(std::get<0>(input), 1.0);
              });
 
-    size_t i = 1;
-    //for (size_t i = 1; i <= 10; ++i) {
-    std::cout << "Iteration: " << i << std::endl;
+    for (size_t i = 1; i <= 10; ++i) {
+        std::cout << "Iteration: " << i << std::endl;
 
-#if 0
-    auto pages =
-        links
-        .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
-                 return std::make_tuple(std::get<0>(first),
-                                        std::get<1>(second),
-                                        std::get<1>(first));
-             });
-
-    auto contribs = pages.FlatMap<PageWithRank>(
-        [](Page page, auto emit) {
-            std::vector<int> urls = std::get<2>(page);
-            double rank = std::get<1>(page);
-            int num_urls = urls.size();
-            for (int url : urls) {
-                emit(std::make_tuple(url, rank / num_urls));
-            }
-            emit(std::make_tuple(std::get<0>(page), 0.0));
-        });
-
-    double dangling_sum = pages.Filter(
-        [](Page input) {
-            int num_urls = std::get<2>(input).size();
-            return (num_urls == 0);
-        }).Map([](Page page) {
-                   return std::get<1>(page);
-               }).Sum([](double first, double second) {
-                          return first + second;
-                      });
-
-    ranks = contribs
-            .ReduceToIndex(key_page_with_rank,
-                           [](PageWithRank rank1, PageWithRank rank2) {
-                               return std::make_tuple(std::get<0>(rank1),
-                                                      std::get<1>(rank1) + std::get<1>(rank2));
-                           },
-                           size - 1
-                           )
-            .Map([&s, &dangling_sum](PageWithRank input) {
-                     int url = std::get<0>(input);
-                     double rank = std::get<1>(input);
-                     return std::make_tuple(url, rank * s + dangling_sum * s + (1 - s));
+        auto pages =
+            links
+            .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
+                     return std::make_tuple(std::get<0>(first),
+                                            std::get<1>(second),
+                                            std::get<1>(first));
                  });
-}
 
-ranks.Map([](const PageWithRank& item) {
-              return std::to_string(std::get<0>(item)) + ": " + std::to_string(std::get<1>(item));
-          }).
-WriteToFileSystem("pagerank_" + std::to_string(ctx.rank()) + ".out");
-#endif
+        auto contribs = pages.FlatMap<PageWithRank>(
+            [](Page page, auto emit) {
+                std::vector<int> urls = std::get<2>(page);
+                double rank = std::get<1>(page);
+                int num_urls = urls.size();
+                for (int url : urls) {
+                    emit(std::make_tuple(url, rank / num_urls));
+                }
+                emit(std::make_tuple(std::get<0>(page), 0.0));
+            });
+
+        double dangling_sum = pages.Filter(
+            [](Page input) {
+                int num_urls = std::get<2>(input).size();
+                return (num_urls == 0);
+            }).Map([](Page page) {
+                       return std::get<1>(page);
+                   }).Sum([](double first, double second) {
+                              return first + second;
+                          });
+
+        ranks = contribs
+                .ReduceToIndex(
+            key_page_with_rank,
+            [](PageWithRank rank1, PageWithRank rank2) {
+                return std::make_tuple(
+                    std::get<0>(rank1),
+                    std::get<1>(rank1) + std::get<1>(rank2));
+            },
+            size - 1)
+                .Map([&dangling_sum](PageWithRank input) {
+                         int url = std::get<0>(input);
+                         double rank = std::get<1>(input);
+                         return std::make_tuple(url, rank * s + dangling_sum * s + (1 - s));
+                     });
+    }
+
+    ranks.Map([](const PageWithRank& item) {
+                  return std::to_string(std::get<0>(item))
+                  + ": " + std::to_string(std::get<1>(item));
+              }).
+    WriteToFileSystem("pagerank_" + std::to_string(ctx.rank()) + ".out");
 
     return 0;
 }
