0c9e4583a1c669c638063b257d0c7b01d5fccce6
Tobias Sturm <mail@tobiassturm.de>
2015-07-28 23:27:26 +0200

    move data generators to separate file

--- benchmarks/data/file_read_write.cpp
+++ benchmarks/data/file_read_write.cpp
@@ -14,84 +14,14 @@
 #include <c7a/api/context.hpp>
 #include <c7a/data/manager.hpp>
 
+#include "data_generators.hpp"
+
 #include <random>
 #include <iostream>
 #include <string>
 
 using namespace c7a; // NOLINT
 
-template <typename Type>
-std::vector<Type> generate(size_t bytes, size_t min_size, size_t max_size)
-{ }
-
-template <>
-std::vector<std::string> generate(size_t bytes, size_t min_size, size_t max_size) {
-    std::vector<std::string> result;
-    size_t remaining = bytes;
-
-    //init randomness
-    std::random_device rd;
-    std::default_random_engine randomness(rd());
-    std::uniform_int_distribution<size_t> uniform_dist(min_size, max_size);
-
-    while (remaining > 0) {
-        size_t next_size = std::min(uniform_dist(randomness), remaining);
-        remaining -= next_size;
-        result.emplace_back(std::string('f', next_size));
-    }
-    return result;
-}
-
-template <>
-std::vector<std::pair<std::string, int>> generate(size_t bytes, size_t min_size, size_t max_size) {
-    std::vector<std::pair<std::string, int>> result;
-    size_t remaining = bytes;
-
-    //init randomness
-    std::random_device rd;
-    std::default_random_engine randomness(rd());
-    std::uniform_int_distribution<size_t> uniform_dist(min_size, max_size);
-
-    while (remaining > 0) {
-        remaining -= sizeof(int);
-        size_t next_size = std::min(uniform_dist(randomness), remaining);
-        remaining -= next_size;
-        result.push_back(std::pair<std::string, int>(std::string('f', next_size), 42));
-    }
-    return result;
-}
-
-template <>
-std::vector<std::tuple<std::string, int, std::string>> generate(size_t bytes, size_t min_size, size_t max_size) {
-    std::vector<std::tuple<std::string, int, std::string>> result;
-    size_t remaining = bytes;
-
-    //init randomness
-    std::random_device rd;
-    std::default_random_engine randomness(rd());
-    std::uniform_int_distribution<size_t> uniform_dist(min_size, max_size);
-
-    while (remaining > 0) {
-        remaining -= sizeof(int);
-        size_t next_size1 = std::min(uniform_dist(randomness), remaining);
-        remaining -= next_size1;
-        size_t next_size2 = std::min(uniform_dist(randomness), remaining);
-        remaining -= next_size2;
-        result.push_back(std::tuple<std::string, int, std::string>(std::string('f', next_size1), 42, std::string('g', next_size2)));
-    }
-    return result;
-}
-
-template <>
-std::vector<int> generate(size_t bytes, size_t /*min_size*/, size_t /*max_size*/) {
-    assert(bytes % sizeof(int) == 0);
-    std::vector<int> result;
-
-    for (size_t current = 0; current < bytes; current += sizeof(int)) {
-        result.emplace_back(42);
-    }
-    return result;
-}
 
 template<typename Type>
 void ConductExperiment(uint64_t bytes, int iterations, api::Context& ctx) {
