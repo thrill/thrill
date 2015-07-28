19f38463e2067ee138f9b5ebd66c23775d1ae216
Tobias Sturm <mail@tobiassturm.de>
2015-07-28 21:05:06 +0200

    update generate random words

--- scripts/gen_random_words.sh
+++ scripts/gen_random_words.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
 # Generates random words, multiple words per line, space separated
 
-#!/bin/bash
 MAX_LEN=32 #of word
-LINES=10
+BYTES=1048576 #1MB
 MAX_WORDS_PER_LINE=10
-
 WORDS=1 #is filled later
-for (( c=1; c<$LINES; c++, WORDS=$(((RANDOM % MAX_WORDS_PER_LINE)+1)) ))
+
+for (( c=0; c<$BYTES; WORDS=$(((RANDOM % MAX_WORDS_PER_LINE)+1)) ))
 do
   for (( d=0; d<$WORDS; d++ ))
   do
-    echo -n "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $(((RANDOM % $MAX_LEN) +1)) | head -n 1) "
+    LEN=$(((RANDOM % $MAX_LEN) +1))
+    ((c=$c+$LEN+1))
+    echo -n "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $LEN | head -n 1) "
   done
-  echo ""
+  ((c=$c+1))
+  echo " "
 done
