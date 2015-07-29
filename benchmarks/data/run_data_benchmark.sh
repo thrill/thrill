268e7d2eb41b3cb8e90386bacf63d50c3ee8f3e2
Tobias Sturm <mail@tobiassturm.de>
2015-07-29 12:13:52 +0200

    run_data_benchmark.sh runs now all three benchmarks together

--- benchmarks/data/run_data_benchmark.sh
+++ benchmarks/data/run_data_benchmark.sh
@@ -2,10 +2,15 @@
 # different data types
 
 githash=$(eval "git rev-parse --short HEAD")
-for type in int string pair triple
+timestamp=$(date)
+echo ${timestamp}
+for benchmark in bench_data_file_read_write bench_data_channel_read_write bench_data_channel_a_to_b
 do
-  for size in 1K 100K 1M
+  for type in int string pair triple
   do
-    ./bench_data_file_read_write -b ${size} $1 int | grep 'single run; ti' | sed -e "s/^/${githash}; ${size}; ${type}; /"
- done
+    for size in 1K 100K 1M 100M 2G
+    do
+      eval ./${benchmark} -b ${size} $1 ${type}| grep 'single run; ti' | sed -e "s/^/${githash}; ${benchmark}; ${timestamp} ${size}; ${type}; /"
+    done
+  done
 done
