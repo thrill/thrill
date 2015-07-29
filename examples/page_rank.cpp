e3f5934bc2f564442a193d630effcece824030b9
Timo Bingmann <tbgit@panthema.net>
2015-07-29 19:11:33 +0200

    Adding two Collapse() to page_rank, less warnings, nicer formatting.

--- examples/page_rank.cpp
+++ examples/page_rank.cpp
@@ -63,7 +63,8 @@ int page_rank(Context& ctx) {
         links
         .Map([](PageWithLinks input) {
                  return std::make_tuple(std::get<0>(input), 1.0);
-             });
+             })
+        .Collapse();
 
     for (size_t i = 1; i <= 10; ++i) {
         std::cout << "Iteration: " << i << std::endl;
@@ -76,41 +77,48 @@ int page_rank(Context& ctx) {
                                             std::get<1>(first));
                  });
 
-        auto contribs = pages.FlatMap<PageWithRank>(
-            [](Page page, auto emit) {
-                std::vector<int> urls = std::get<2>(page);
-                double rank = std::get<1>(page);
-                int num_urls = urls.size();
-                for (int url : urls) {
-                    emit(std::make_tuple(url, rank / num_urls));
-                }
-                emit(std::make_tuple(std::get<0>(page), 0.0));
-            });
+        auto contribs =
+            pages
+            .FlatMap<PageWithRank>(
+                [](Page page, auto emit) {
+                    std::vector<int> urls = std::get<2>(page);
+                    double rank = std::get<1>(page);
+                    int num_urls = urls.size();
+                    for (int url : urls) {
+                        emit(std::make_tuple(url, rank / num_urls));
+                    }
+                    emit(std::make_tuple(std::get<0>(page), 0.0));
+                });
 
-        double dangling_sum = pages.Filter(
-            [](Page input) {
-                int num_urls = std::get<2>(input).size();
-                return (num_urls == 0);
-            }).Map([](Page page) {
-                       return std::get<1>(page);
-                   }).Sum([](double first, double second) {
-                              return first + second;
-                          });
+        double dangling_sum =
+            pages
+            .Filter([](Page input) {
+                        int num_urls = std::get<2>(input).size();
+                        return (num_urls == 0);
+                    })
+            .Map([](Page page) {
+                     return std::get<1>(page);
+                 })
+            .Sum([](double first, double second) {
+                     return first + second;
+                 });
 
-        ranks = contribs
-                .ReduceToIndex(
-            key_page_with_rank,
-            [](PageWithRank rank1, PageWithRank rank2) {
-                return std::make_tuple(
-                    std::get<0>(rank1),
-                    std::get<1>(rank1) + std::get<1>(rank2));
-            },
-            size - 1)
-                .Map([&dangling_sum](PageWithRank input) {
-                         int url = std::get<0>(input);
-                         double rank = std::get<1>(input);
-                         return std::make_tuple(url, rank * s + dangling_sum * s + (1 - s));
-                     });
+        ranks =
+            contribs
+            .ReduceToIndex(
+                key_page_with_rank,
+                [](PageWithRank rank1, PageWithRank rank2) {
+                    return std::make_tuple(
+                        std::get<0>(rank1),
+                        std::get<1>(rank1) + std::get<1>(rank2));
+                },
+                size - 1)
+            .Map([&dangling_sum](PageWithRank input) {
+                     int url = std::get<0>(input);
+                     double rank = std::get<1>(input);
+                     return std::make_tuple(url, rank * s + dangling_sum * s + (1 - s));
+                 })
+            .Collapse();
     }
 
     ranks.Map([](const PageWithRank& item) {
