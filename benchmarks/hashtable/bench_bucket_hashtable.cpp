896c0849af6402cd7ecab9d097ad83102e8ddba2
Matthias Stumpp <mstumpp@gmail.com>
2015-07-31 17:23:32 +0200

    refined bench default params

--- benchmarks/hashtable/bench_bucket_hashtable.cpp
+++ benchmarks/hashtable/bench_bucket_hashtable.cpp
@@ -13,6 +13,7 @@
 #include <c7a/core/reduce_pre_table.hpp>
 #include <c7a/data/block_writer.hpp>
 #include <c7a/data/discard_sink.hpp>
+#include <math.h>
 
 using IntPair = std::pair<int, int>;
 
@@ -34,15 +35,15 @@ int main(int argc, char* argv[]) {
 
     clp.SetVerboseProcess(false);
 
-    unsigned int size = 1;
+    unsigned int size = pow(2, 24);
     clp.AddUInt('s', "size", "S", size,
                 "Fill hashtable with S random integers");
 
-    unsigned int workers = 1;
+    unsigned int workers = 100;
     clp.AddUInt('w', "workers", "W", workers,
                 "Open hashtable with W workers, default = 1.");
 
-    unsigned int num_buckets_init_scale = 10;
+    unsigned int num_buckets_init_scale = 1000;
     clp.AddUInt('i', "num_buckets_init_scale", "I", num_buckets_init_scale,
                 "Open hashtable with num_buckets_init_scale, default = 10.");
 
@@ -50,7 +51,7 @@ int main(int argc, char* argv[]) {
     clp.AddUInt('r', "num_buckets_resize_scale", "R", num_buckets_resize_scale,
                 "Open hashtable with num_buckets_resize_scale, default = 2.");
 
-    unsigned int max_num_items_per_bucket = 256;
+    unsigned int max_num_items_per_bucket = 128;
     clp.AddUInt('b', "max_num_items_per_bucket", "B", max_num_items_per_bucket,
                 "Open hashtable with max_num_items_per_bucket, default = 256.");
 
@@ -58,7 +59,7 @@ int main(int argc, char* argv[]) {
     clp.AddUInt('t', "max_num_items_table", "T", max_num_items_table,
                 "Open hashtable with max_num_items_table, default = 1048576.");
 
-    unsigned int modulo = 1000;
+    unsigned int modulo = 10000;
     clp.AddUInt('m', "modulo", modulo,
                 "Open hashtable with keyspace size of M.");
 
