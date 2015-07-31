dc7108e8cbd0c7bc7a65eb92c87dcfd1814e242a
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-07-31 02:53:42 +0200

    add generators to serialization benchmark

--- benchmarks/serialization/bench_serialization.cpp
+++ benchmarks/serialization/bench_serialization.cpp
@@ -8,70 +8,110 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
+// #include <random>
+#include <vector>
+#include <string>
 
-int SerialString(int times) {
+
+template <typename T>
+int BenchmarkSerialization(T t, int iterrations) {
     c7a::common::StatsTimer<true> timer(false);
-    for (int i = 0; i < times; ++i) {
-        timer.Start();
-        //TODO: File creation in or outside benchmark?!
+    for (int i = 0; i < iterrations; ++i) {
         c7a::data::File f;
+        timer.Start();
         {
             auto w = f.GetWriter();
-            w(bench_string);
+            w(t);
         }
         auto r = f.GetReader();
-        auto deserial = r.Next<std::string>();
+        r.Next<T>();
         timer.Stop();
     }
-    return timer.Microseconds()/times;
+    return timer.Microseconds()/iterrations;
 }
 
-int SerialVector(int times) {
-    c7a::common::StatsTimer<true> timer(false);
-    for (int i = 0; i < times; ++i) {
+int SerialString(int iterrations) {
+    return BenchmarkSerialization(bench_string, iterrations);
+}
 
-        timer.Start();
-        c7a::data::File f;
-        {
-            auto w = f.GetWriter();
-            w(bv);
-        }
+int SerialVector(int iterrations) {
+    return BenchmarkSerialization(bv, iterrations);
+}
 
-        auto r = f.GetReader();
-        auto deserial = r.Next<BenchVector>();
-        timer.Stop();
+int SerialTuple(int iterrations) {
+    return BenchmarkSerialization(bench_tuple, iterrations);
+}
+
+void GetRandomIntVector(std::vector<int64_t> & res, int n) {
+    res.reserve(n);
+    for (int i = 0; i < n; ++i) {
+        res.push_back(rand());
     }
-    return timer.Microseconds()/times;
 }
 
-int SerialTuple(int times) {
-    c7a::common::StatsTimer<true> timer(false);
-    for (int i = 0; i < times; ++i) {
+void PrintSQLPlotTool(std::string datatype, size_t size, int iterations, int time) {
+    std::cout << "RESULT"
+              << " datatype=" << datatype
+              << " size=" << size
+              << " repeats=" << iterations
+              << " time=" << time
+              << std::endl;
+}
 
-        timer.Start();
-        c7a::data::File f;
-        {
-            auto w = f.GetWriter();
-            w(bench_tuple);
-        }
+// all glory to stackoverflow a/440240
+void gen_random(std::vector<char>& s, const int len) {
+    s.reserve(len);
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
 
-        auto r = f.GetReader();
-        auto deserial = r.Next<decltype(bench_tuple)>();
-        timer.Stop();
+    for (int i = 0; i < len; ++i) {
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
-    return timer.Microseconds()/times;
 }
-
 
 
 int main() {
-    std::cout << std::setfill(' ') << std::setw(30) << std::left <<
-                 "Serialization of a string:" << SerialString(50) << std::endl;
-    std::cout << std::setfill(' ') << std::setw(30) << std::left <<
-                 "Serialization of a vector:" << SerialVector(50) << std::endl;
-    std::cout << std::setfill(' ') << std::setw(30) << std::left <<
-                 "Serialization of a tuple:" << SerialTuple(50) << std::endl;
-    return 123;
+    int iterations = 50;
+    // string from cpp-serializer
+    PrintSQLPlotTool("std::string", bench_string.size(), iterations, SerialString(iterations));
+    // vector from cpp-serializer
+    PrintSQLPlotTool("std::vector<int64_t>", sizeof(int64_t) * bv.bench_vector.size(), iterations, SerialVector(iterations));
+    // tuple-pair-construct from cpp-serializer
+    PrintSQLPlotTool("???", 0, iterations, SerialTuple(iterations));
+
+    std::vector<int> size = {100, 8890, 121212, 999999};
+
+    // serialize some random strings
+    for (int s : size) {
+        std::vector<char> x;
+        gen_random(x, s);
+        std::string x_str(x.begin(), x.begin()+s);
+
+        PrintSQLPlotTool("std::string", s, iterations, BenchmarkSerialization(x_str, iterations));
+    }
+
+    // serialize some random ints
+    for (int s : size) {
+        int acc = 0;
+        for (int i = 0; i < s; ++i) {
+            int x = rand();
+            acc += BenchmarkSerialization(x, 1);
+        }
+        PrintSQLPlotTool("int", s, iterations, acc);
+    }
+
+    // serialize some random int vectors
+    for (int s : size) {
+        std::vector<int64_t> x;
+        GetRandomIntVector(x, s);
+        BenchVector x_struct = BenchVector(x);
+
+        PrintSQLPlotTool("std::vector<int64_t>", s, iterations, BenchmarkSerialization(x_struct, iterations));
+    }
+    return 1;
 }
 
 
