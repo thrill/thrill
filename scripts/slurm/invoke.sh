11a56d8cef332a4bbdea05eedc5a1093f83b330c
Emanuel Jöbstl <emiswelt@gmail.com>
2015-07-31 11:14:42 +0200

    Added debug option that invokes GDB and prints a backtrace of the first error that occurs.

--- scripts/slurm/invoke.sh
+++ scripts/slurm/invoke.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+DEBUG=1
+PORT=10090
+COMMAND="../../build/examples/word_count"
+
 ./slurmCheck.sh
 
 if [ $? -ne 0 ]; then
@@ -7,10 +11,11 @@ if [ $? -ne 0 ]; then
   exit -1
 fi 
 
-PORT=10090
-COMMAND="../../build/examples/word_count"
 HOSTLIST=$(./getSlurmHostlist.sh | ./formatPort.sh $PORT)
 MY_RANK=$(./getSlurmRank.sh)
 EX="$COMMAND -r $MY_RANK $HOSTLIST >> $MY_RANK.stdout 2>> $MY_RANK.stderr"
+if [ $DEBUG -eq 1 ]; then
+  EX="gdb -q -x ./backtrace --args $EX"
+fi
 echo "Invoking: $EX"
 eval $EX
