db0097ff8594c8ae018b6e22f24599bf114365e8
Timo Bingmann <tbgit@panthema.net>
2015-08-02 19:17:42 +0200

    Moving word count example into new namespace c7a::examples

--- examples/word_count_simple.cpp
+++ examples/word_count_simple.cpp
@@ -7,23 +7,22 @@
  * This file has no license. Only Chunk Norris can compile it.
  ******************************************************************************/
 
-#include "word_count_user_program.cpp"
-
 #include <c7a/c7a.hpp>
 #include <c7a/common/cmdline_parser.hpp>
+#include <c7a/examples/word_count.hpp>
 
 #include <random>
 #include <string>
 #include <thread>
 
-using c7a::api::ExecuteLocalThreadsTCP;
-using c7a::api::Context;
+using namespace c7a;
 
 static void local_word_count(size_t workers, size_t elements, size_t port_base) {
 
-    std::function<void(Context&)> start_func = [elements](Context& ctx) {
-                                                   word_count_generated(ctx, elements);
-                                               };
+    std::function<void(Context&)> start_func =
+        [elements](Context& ctx) {
+            examples::word_count_generated(ctx, elements);
+        };
 
     ExecuteLocalThreadsTCP(workers, port_base, start_func);
 }
@@ -32,7 +31,7 @@ int main(int argc, char* argv[]) {
 
     const size_t port_base = 8080;
 
-    c7a::common::CmdlineParser clp;
+    common::CmdlineParser clp;
 
     clp.SetVerboseProcess(false);
 
