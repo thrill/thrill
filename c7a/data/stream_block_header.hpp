20a9f9f84f21389b801918f8ba86f92b0e6cfabc
Tobias Sturm <mail@tobiassturm.de>
2015-08-05 16:22:37 +0200

    add copyright to stream_block_header.hpp

--- c7a/data/stream_block_header.hpp
+++ c7a/data/stream_block_header.hpp
@@ -3,6 +3,7 @@
  *
  * Part of Project c7a.
  *
+ * Copyright (C) 2015 Tobias Sturm <mail@tobiassturm.de>
  *
  * This file has no license. Only Chunk Norris can compile it.
  ******************************************************************************/
