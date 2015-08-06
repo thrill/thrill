thrill/thrill

commit d2bec24de8b602c52f734d477875aa72a8000d0c

Context embodies JobManager + old Context
- one host = one computing_node, hosts multiple workers
- create one context instance per worker
- context instances hold references to data::Manager / net::Manager --> share these components
- context instances hold independet objects like Stats & StatsGraph
- Execute* methods bootstrap the Context instances and call job_startpoints
- Added ExecuteSameThread method for execution of 1 worker within the master thread

!! Tests fail ATM. Will fix that

c7a/api/context.hpp
@@ -4,6 +4,7 @@
  * Part of Project c7a.
  *
  * Copyright (C) 2015 Alexander Noe <aleexnoe@gmail.com>
+ * Copyright (C) 2015 Tobias Sturm <mail@tobiassturm.de>
  *
  * This file has no license. Only Chunk Norris can compile it.
  ******************************************************************************/
@@ -14,8 +15,8 @@
 
 #include <c7a/api/stats_graph.hpp>
 #include <c7a/common/stats.hpp>
-#include <c7a/core/job_manager.hpp>
 #include <c7a/data/manager.hpp>
+#include <c7a/net/manager.hpp>
 #include <c7a/net/flow_control_channel.hpp>
 #include <c7a/net/flow_control_manager.hpp>
 
@@ -32,120 +33,155 @@ namespace api {
 //! \{
 
 /*!
- * The Context of a job is a unique structure inside a worker, which holds
+ * The Context of a job is a unique instance per worker which holds
  *  references to all underlying parts of c7a. The context is able to give
  *  references to the  \ref c7a::data::Manager "data manager", the
- * \ref c7a::net::Group  "net group" and to the
- * \ref c7a::core::JobManager "job manager". The context can also return the
- * total number of workers and the rank of this worker.
+ * \ref c7a::net::Group  "net group"
+ * \ref c7a::common::Stats "stats" and
+ * \ref c7a::common::StatsGraph "stats graph".
+ * Threads share the data manager and
+ * the net group via the context object.
  */
 class Context
 {
 public:
-    Context(core::JobManager& job_manager, int local_worker_id)
-        : job_manager_(job_manager),
-          local_worker_id_(local_worker_id)
+    Context(net::Manager& net_manager, net::FlowControlChannelManager& flow_manager, data::Manager& data_manager, size_t workers_per_computing_node, size_t worker_id)
+        : net_manager_(net_manager)
+        , flow_manager_(flow_manager)
+        , data_manager_(data_manager)
+        , worker_id_(worker_id)
+        , workers_per_computing_node_(workers_per_computing_node)
     { }
 
     //! Returns a reference to the data manager, which gives iterators and
     //! emitters for data.
-    data::Manager & data_manager() {
-        if (local_worker_id_ != 0)
-        {
-            //TODO (ts) --> CA-194
-            assert(false && "Data Manager does not support multi-threading at the moment.");
-        }
-        return job_manager_.data_manager();
+    data::Manager & data_manager() const {
+        return data_manager_;
     }
-
-    // This is forbidden now. Muha. >) (ej)
-    //net::Group & flow_net_group() {
-    //   return job_manager_.net_manager().GetFlowGroup();
-    //}
 
     /**
-     * @brief Gets the flow control channel for a certain thread.
+     * @brief Gets the flow control channel for the current worker.
      *
-     * @return The flow control channel associated with the given ID.
+     * @return The flow control channel instance for this worker.
      */
     net::FlowControlChannel & flow_control_channel() {
-        return job_manager_.flow_manager().GetFlowControlChannel(local_worker_id_);
+        return flow_manager_.GetFlowControlChannel(worker_id_);
     }
 
-    //! Returns the total number of workers.
-    size_t number_worker() {
-        return job_manager_.net_manager().Size();
+    //! Returns the total number of computing_nodes.
+    size_t number_computing_node() const {
+        return net_manager_.Size();
     }
 
-    //! Returns the rank of this worker. Between 0 and number_worker() - 1
-    size_t rank() {
-        return job_manager_.net_manager().MyRank();
+    //! Returns the number of nodes that is hosted on each computing_node
+    size_t workers_per_computing_node() const {
+        return workers_per_computing_node_;
     }
 
+    size_t rank() const {
+        return workers_per_computing_node() * computing_node_id() + worker_id();
+    }
+
+    size_t max_rank() {
+        return number_computing_node() * workers_per_computing_node() - 1;
+    }
+
+    //! Returns id of this computing_node in the cluser
+    //! A computing_node is a machine in the cluster that hosts multiple workers
+    size_t computing_node_id() const {
+        return net_manager_.MyRank();
+    }
+
+    //! Returns the local id ot this worker on the computing_node
+    //! A worker is _locally_ identified by this id
+    size_t worker_id() const {
+        return worker_id_;
+    }
+
+    //! Returns the stas object for this worker
     common::Stats<ENABLE_STATS> & stats() {
         return stats_;
     }
 
-    int local_worker_id() {
-        return local_worker_id_;
-    }
-
-    int local_worker_count() {
-        return job_manager_.local_worker_count();
-    }
-
+    //! Returns the stats graph object for this worker
     api::StatsGraph & stats_graph() {
         return stats_graph_;
     }
 
 private:
-    core::JobManager& job_manager_;
+    //! net::Manager instance that is shared among workers
+    net::Manager& net_manager_;
+
+    //! net::FlowControlChannelManager instance that is shared among workers
+    net::FlowControlChannelManager& flow_manager_;
+
+    //! data::Manager instance that is shared among workers
+    data::Manager& data_manager_;
+
+    //! Stats object that is uniquely held for this worker
     common::Stats<ENABLE_STATS> stats_;
+
+    //! StatsGrapg object that is uniquely held for this worker
     api::StatsGraph stats_graph_;
 
-    //! number of this worker context, 0..p-1, within this compute node.
-    int local_worker_id_;
+    //! number of this computing_node context, 0..p-1, within this compute node
+    size_t worker_id_;
+
+    //! number of workers hosted per computing_node
+    size_t workers_per_computing_node_;
 };
 
+//! Outputs the context as <computing node id>:<worker id> to an std::ostream
+static inline std::ostream& operator << (std::ostream& os, const Context& ctx) {
+    return os << ctx.computing_node_id() << ":" << ctx.worker_id();
+}
+
 //! Executes the given job startpoint with a context instance.
-//! Startpoint may be called multiple times with concurrent threads and
-//! different context instances.
+//! Startpoint may be called multiple times with concurrent workers (threads)
+//! and different context instances.
 //!
 //! \returns 0 if execution was fine on all threads. Otherwise, the first
 //! non-zero return value of any thread is returned.
 int Execute(
     int argc, char* const* argv,
     std::function<int(Context&)> job_startpoint,
-    size_t local_worker_count = 1, const std::string& log_prefix = "");
+    const std::string& log_prefix = "");
 
 /*!
- * Function to run a number of workers as locally independent threads, which
- * still communicate via TCP sockets.
+ * Function to run a number of computing_nodes as locally independent threads, which
+ * still communicate via TCP sockets (threads_per_computing_node = 1)
  */
 void
-ExecuteLocalThreadsTCP(const size_t& workers, const size_t& port_base,
+ExecuteLocalThreadsTCP(const size_t& computing_nodes, const size_t& port_base,
                        std::function<void(Context&)> job_startpoint);
+//TODO maybe this should be moved somewhere into test/helpers -ts
 
 /*!
  * Helper Function to ExecuteLocalThreads in test suite for many different
- * numbers of local workers as independent threads.
+ * numbers of local computing_nodes as independent threads.
  */
 void ExecuteLocalTestsTCP(std::function<void(Context&)> job_startpoint);
+//TODO maybe this should be moved somewhere into test/helpers -ts
 
 /*!
  * Function to run a number of mock compute nodes as locally independent
  * threads, which communicate via internal stream sockets.
  */
 void
-ExecuteLocalMock(size_t node_count, size_t local_worker_count,
-                 std::function<void(core::JobManager&, size_t)> job_startpoint);
+ExecuteLocalMock(size_t node_count, size_t local_computing_node_count,
+                 std::function<void(api::Context&, size_t)> job_startpoint);
 
 /*!
  * Helper Function to execute tests using mock networks in test suite for many
- * different numbers of node and workers as independent threads in one program.
+ * different numbers of node and computing_nodes as independent threads in one program.
  */
-void ExecuteLocalTests(std::function<void(Context&)> job_startpoint,
-                       const std::string& log_prefix = "");
+void ExecuteLocalTests(std::function<void(Context&)> job_startpoint);
+
+/*!
+ * Executes the given job_startpoint within the same thread -->
+ * one computing node with one thread
+ */
+void ExecuteSameThread(std::function<void(Context&)> job_startpoint);
 
 //! \}
 

c7a/api/input_line_iterator.hpp
@@ -25,10 +25,10 @@ class InputLineIterator
 public:
     //! Creates an instance of iterator that reads file line based
     InputLineIterator(std::ifstream& file,
-                      size_t my_id,
+                      size_t my_node_id,
                       size_t num_workers)
         : file_(file),
-          my_id_(my_id),
+          my_node_id_(my_node_id),
           num_workers_(num_workers) {
         //Find file size and save it
         file_.seekg(0, std::ios::end);
@@ -36,19 +36,19 @@ public:
 
         //Go to start of 'local part'.
         std::streampos per_worker = file_size_ / num_workers_;
-        std::streampos my_start = per_worker * my_id_;
-        if (my_id_ == (num_workers - 1)) {
+        std::streampos my_start = per_worker * my_node_id_;
+        if (my_node_id_ == (num_workers)) {
             my_end_ = file_size_ - 1;
         }
         else {
-            my_end_ = per_worker * (my_id_ + 1) - 1;
+            my_end_ = per_worker * (my_node_id_ + 1) - 1;
         }
 
         file_.seekg(my_start, std::ios::beg);
 
         //Go to next new line if the stream-pointer is not at the beginning of a line
-        if (my_id != 0) {
-            std::streampos previous = (per_worker * my_id_) - 1;
+        if (my_node_id != 0) {
+            std::streampos previous = (per_worker * my_node_id_) - 1;
             file_.seekg(previous, std::ios::beg);
             //file_.unget();
             if (file_.get() != '\n') {
@@ -78,7 +78,7 @@ private:
     //!File size in bytes
     size_t file_size_;
     //!Worker ID
-    size_t my_id_;
+    size_t my_node_id_;
     //!Total number of workers
     size_t num_workers_;
     //!End of local block

c7a/api/reduce_to_index.hpp
@@ -96,7 +96,7 @@ public:
           reduce_function_(reduce_function),
           channel_(parent.ctx().data_manager().GetNewChannel()),
           emitters_(channel_->OpenWriters()),
-          reduce_pre_table_(parent.ctx().number_worker(), key_extractor,
+          reduce_pre_table_(parent.ctx().max_rank() + 1, key_extractor,
                             reduce_function_, emitters_,
                             core::HashByIndex(max_index)),
           max_index_(max_index),
@@ -136,13 +136,13 @@ public:
         size_t min_local_index =
             std::ceil(static_cast<double>(max_index_ + 1)
                       * static_cast<double>(context_.rank())
-                      / static_cast<double>(context_.number_worker()));
+                      / static_cast<double>(context_.max_rank()));
         size_t max_local_index =
             std::ceil(static_cast<double>(max_index_ + 1)
-                      * static_cast<double>(context_.rank() + 1)
-                      / static_cast<double>(context_.number_worker())) - 1;
+                      * static_cast<double>(context_.rank())
+                      / static_cast<double>(context_.max_rank())) - 1;
 
-        if (context_.rank() == context_.number_worker() - 1) {
+        if (context_.rank() == context_.max_rank()) {
             max_local_index = max_index_;
         }
         if (context_.rank() == 0) {

c7a/api/context.cpp
@@ -21,12 +21,13 @@
 #include <thread>
 #include <tuple>
 #include <vector>
+#include <memory>
 
 namespace c7a {
 namespace api {
 
 static inline
-std::tuple<int, size_t, std::vector<std::string> >
+std::tuple<int, size_t, size_t, std::vector<std::string> >
 ParseArgs(int argc, char* const* argv) {
     //replace with arbitrary complex implementation
     size_t my_rank;
@@ -36,37 +37,40 @@ ParseArgs(int argc, char* const* argv) {
     clp.SetVerboseProcess(false);
 
     unsigned int rank = 1;
+    unsigned int workers_per_node = 1;
     clp.AddUInt('r', "rank", "R", rank,
-                "Rank of this worker");
+                "Rank of this computing node");
+    clp.AddUInt('w', "workers", "W", rank,
+                "Workers per computing node");
 
     std::vector<std::string> addr;
     clp.AddParamStringlist("addresses", addr,
                            "List of all worker addresses.");
 
     if (!clp.Process(argc, argv)) {
-        return std::make_tuple(-1, my_rank, endpoints);
+        return std::make_tuple(-1, my_rank, workers_per_node, endpoints);
     }
 
     for (auto address : addr) {
         if (address.find(":") == std::string::npos) {
             std::cerr << "Invalid address. No Portnumber detectable";
-            return std::make_tuple(-1, my_rank, endpoints);
+            return std::make_tuple(-1, my_rank, workers_per_node, endpoints);
         }
     }
 
-    if (argc > 2) {
+    if (argc > 3) {
         my_rank = rank;
         endpoints.assign(addr.begin(), addr.end());
     }
-    else if (argc == 2) {
+    else if (argc == 3) {
         std::cerr << "Wrong number of arguments. Must be 0 or > 1";
-        return std::make_tuple(-1, my_rank, endpoints);
+        return std::make_tuple(-1, my_rank, workers_per_node, endpoints);
     }
     else {
         my_rank = 0;
         endpoints.push_back("127.0.0.1:1234");
     }
-    return std::make_tuple(0, my_rank, endpoints);
+    return std::make_tuple(0, my_rank, workers_per_node, endpoints);
 }
 
 //! Executes the given job startpoint with a context instance.
@@ -77,15 +81,16 @@ ParseArgs(int argc, char* const* argv) {
 int Execute(
     int argc, char* const* argv,
     std::function<int(Context&)> job_startpoint,
-    size_t local_worker_count, const std::string& log_prefix) {
+    const std::string& log_prefix) {
 
     // true if program time should be taken and printed
     static const bool debug = false;
 
     size_t my_rank;
+    size_t workers_per_node;
     std::vector<std::string> endpoints;
     int global_result = 0;
-    std::tie(global_result, my_rank, endpoints) = ParseArgs(argc, argv);
+    std::tie(global_result, my_rank, workers_per_node, endpoints) = ParseArgs(argc, argv);
     if (global_result != 0)
         return -1;
 
@@ -100,40 +105,43 @@ int Execute(
     for (const std::string& ep : endpoints)
         LOG << ep << " ";
 
-    // construct node global objects: net::Manager, data::Manager, etc.
+    //connect TCP streams
+    net::Manager net_manager;
+    net_manager.Initialize(my_rank, net::Endpoint::ParseEndpointList(endpoints));
 
-    core::JobManager jobMan(log_prefix);
-    jobMan.Connect(my_rank, net::Endpoint::ParseEndpointList(endpoints),
-                   local_worker_count);
+    //connect data subsystem to network
+    net::DispatcherThread net_dispatcher("dispatcher " + std::to_string(my_rank));
+    data::Manager data_manager(net_dispatcher);
+    data_manager.Connect(&net_manager.GetDataGroup());
 
-    // launch initial thread for each of the workers on this node.
+    //create flow control subsystem
+    net::FlowControlChannelManager flow_manager(net_manager.GetFlowGroup(), workers_per_node);
 
-    std::vector<std::thread> threads(local_worker_count);
-    std::vector<std::atomic<int> > results(local_worker_count);
+    // launch thread for each of the workers on this node.
+    std::vector<std::thread> threads(workers_per_node);
+    std::vector<int > results(workers_per_node);
 
-    for (size_t i = 0; i < local_worker_count; i++) {
+    for (size_t i = 0; i < workers_per_node; i++) {
         threads[i] = std::thread(
-            [&jobMan, &results, &job_startpoint, i, log_prefix] {
-                Context ctx(jobMan, i);
+            [&net_manager, &data_manager, &flow_manager, &results, &job_startpoint, i, log_prefix, workers_per_node] {
+                Context ctx(net_manager, flow_manager, data_manager, workers_per_node, i);
                 common::NameThisThread(
                     log_prefix + " worker " + std::to_string(i));
 
-                LOG << "Starting job on worker " << ctx.rank();
+                LOG << "Starting job on node " << ctx.computing_node_id();
                 auto overall_timer = ctx.stats().CreateTimer("job::overall", "", true);
-                // TODO: this cannot be correct, the job needs to know which
-                // worker number it is on the node.
                 int job_result = job_startpoint(ctx);
                 STOP_TIMER(overall_timer)
-                LOG << "Worker " << ctx.rank() << " done!";
+                LOG << "Worker " << i << " done!";
 
                 results[i] = job_result;
-                jobMan.flow_manager().GetFlowControlChannel(0).Await();
+                ctx.flow_control_channel().Await();
             });
     }
 
     // join worker threads
 
-    for (size_t i = 0; i < local_worker_count; i++) {
+    for (size_t i = 0; i < workers_per_node; i++) {
         threads[i].join();
         if (results[i] != 0 && global_result == 0)
             global_result = results[i];
@@ -143,8 +151,9 @@ int Execute(
 }
 
 /*!
- * Function to run a number of workers as locally independent threads, which
- * still communicate via TCP sockets.
+ * Starts n computing nodes with one worker each, all running on this machine.
+ * Thus the workers communicate via TCP sockets and do not share a
+ * data::Manager or net::FlowControlChannel.
  */
 void
 ExecuteLocalThreadsTCP(const size_t& workers, const size_t& port_base,
@@ -158,7 +167,7 @@ ExecuteLocalThreadsTCP(const size_t& workers, const size_t& port_base,
 
         // construct command line for independent worker thread
 
-        strargs[i] = { "local_c7a", "-r", std::to_string(i) };
+        strargs[i] = { "local_c7a", "-r", std::to_string(i), "-w", "1" };
 
         for (size_t j = 0; j < workers; j++)
             strargs[i].push_back("127.0.0.1:" + std::to_string(port_base + j));
@@ -180,7 +189,7 @@ ExecuteLocalThreadsTCP(const size_t& workers, const size_t& port_base,
         threads[i] = std::thread(
             [=]() {
                 Execute(strargs[i].size(), args[i].data(),
-                        intReturningFunction, 1, "worker " + std::to_string(i));
+                        intReturningFunction, "worker " + std::to_string(i));
             });
     }
 
@@ -207,64 +216,97 @@ void ExecuteLocalTestsTCP(std::function<void(Context&)> job_startpoint) {
 }
 
 /*!
- * Function to run a number of mock compute nodes as locally independent
- * threads, which communicate via internal stream sockets.
+ * Starts n computing nodes with multiple workers each, all running on this machine.
+ * The nodes communicate via Sockets created by the socketpair call and do
+ * not share a data::Manager or net::FlowControlChannel. The workers withing
+ * the same node do share these components though.
  */
 void
-ExecuteLocalMock(size_t node_count, size_t local_worker_count,
-                 std::function<void(core::JobManager&, size_t)> job_startpoint) {
+ExecuteLocalMock(size_t node_count, size_t workers_per_node,
+                 std::function<void(api::Context&)> job_startpoint) {
+    static const bool debug = false;
+    //connect TCP streams
+    std::vector<net::Manager> net_managers = net::Manager::ConstructLocalMesh(node_count);
 
-    // construct a mock mesh network of JobManagers
-    std::vector<core::JobManager> jm_mesh
-        = core::JobManager::ConstructLocalMesh(node_count, local_worker_count);
+    //cannot be constructed inside loop because we pass only references down
+    //thus the objects must live longer than the loop.
+    net::DispatcherThread** net_dispatchers = (net::DispatcherThread**) malloc(node_count * sizeof(net::DispatcherThread));
+    data::Manager** data_managers = (data::Manager**) malloc(node_count * sizeof(data::Manager));
+    net::FlowControlChannelManager** flow_managers = (net::FlowControlChannelManager**)malloc(node_count * sizeof(net::FlowControlChannelManager));
 
-    // launch initial thread for each compute node.
+    for (size_t node = 0; node < node_count; node++) {
+        //connect data subsystem to network
+        net_dispatchers[node] = new net::DispatcherThread("dispatcher " + std::to_string(node));
+        net::DispatcherThread& dispatcher = *(net_dispatchers[node]);
+        data_managers[node] = new data::Manager(dispatcher);
+        data_managers[node]->Connect(&net_managers[node].GetDataGroup());
 
-    std::vector<std::thread> threads(node_count);
-    for (size_t n = 0; n < node_count; n++) {
-        threads[n] = std::thread(
-            [&jm_mesh, n, job_startpoint] {
-                job_startpoint(jm_mesh[n], n);
-            });
+        //create flow control subsystem
+        auto & group = net_managers[node].GetFlowGroup();
+        flow_managers[node] = new net::FlowControlChannelManager(group, workers_per_node);
     }
 
-    // join compute node threads
-    for (size_t i = 0; i < node_count; i++) {
+    // launch thread for each of the workers on this node.
+    std::vector<std::thread> threads(node_count * workers_per_node);
+
+    for (size_t node = 0; node < node_count; node++) {
+        std::string log_prefix = "node " + std::to_string(node);
+        for (size_t i = 0; i < workers_per_node; i++) {
+            threads[i] = std::thread(
+                [&net_managers, &data_managers, &flow_managers, &job_startpoint, node, i, log_prefix, workers_per_node ] {
+                    Context ctx(net_managers[node], *(flow_managers[node]), *(data_managers[node]), workers_per_node, i);
+                    common::NameThisThread(
+                        log_prefix + " worker " + std::to_string(i));
+
+                    LOG << "Starting job on node " << ctx.computing_node_id();
+                    auto overall_timer = ctx.stats().CreateTimer("job::overall", "", true);
+                    job_startpoint(ctx);
+                    STOP_TIMER(overall_timer)
+                    LOG << "Worker " << i << " done!";
+                    ctx.flow_control_channel().Await();
+                });
+        }
+    }
+
+    // join worker threads
+
+    for (size_t i = 0; i < node_count * workers_per_node; i++) {
         threads[i].join();
+        free(flow_managers[i]);
     }
-
-    // tear down mock mesh of JobManagers
-    // TODO(tb): ???
 }
 
 /*!
  * Helper Function to execute tests using mock networks in test suite for many
  * different numbers of node and workers as independent threads in one program.
  */
-void ExecuteLocalTests(std::function<void(Context&)> job_startpoint,
-                       const std::string& log_prefix) {
+void ExecuteLocalTests(std::function<void(Context&)> job_startpoint) {
+    int num_nodes[] = { 1, 2, 5, 8};
+    int num_workers[] = {1, 2, 3};
 
-    static const bool debug = false;
-
-    for (size_t nodes = 1; nodes <= 8; ++nodes) {
-
-        ExecuteLocalMock(
-            nodes, 1,
-            [job_startpoint, log_prefix](core::JobManager& jm, size_t node_id) {
-
-                Context ctx(jm, 0);
-                common::NameThisThread(
-                    log_prefix + " node " + std::to_string(node_id));
-
-                LOG << "Starting node " << node_id;
-                auto overall_timer = ctx.stats().CreateTimer("job::overall", "", true);
-                job_startpoint(ctx);
-                STOP_TIMER(overall_timer)
-                LOG << "Worker " << node_id << " done!";
-
-                jm.flow_manager().GetFlowControlChannel(0).Await();
-            });
+    for (auto& nodes : num_nodes) {
+        for (auto& workers : num_workers) {
+            ExecuteLocalMock(nodes, workers, job_startpoint);
+        }
     }
+}
+
+void ExecuteSameThread(std::function<void(Context&)> job_startpoint) {
+    net::Manager net_manager;
+    net_manager.Initialize(0, net::Endpoint::ParseEndpointList("127.0.0.1:12345"));
+
+    //connect data subsystem to network
+    net::DispatcherThread net_dispatcher("dispatcher");
+    data::Manager data_manager(net_dispatcher);
+    data_manager.Connect(&net_manager.GetDataGroup());
+
+    //create flow control subsystem
+    net::FlowControlChannelManager flow_manager(net_manager.GetFlowGroup(), 1);
+
+    Context ctx(net_manager, flow_manager, data_manager, 1, 0);
+    common::NameThisThread("worker " + std::to_string(0));
+
+    job_startpoint(ctx);
 }
 
 } // namespace api

c7a/api/generate.hpp
@@ -80,17 +80,14 @@ public:
         static_assert(std::is_convertible<size_t, InputArgument>::value,
                       "The GeneratorFunction needs a size_t as input parameter");
 
-        size_t offset = (size_ / context_.number_worker()) * context_.rank();
-        size_t local_elements;
+        size_t elements_per_worker = (size_ / (context_.max_rank() + 1));
+        size_t offset = elements_per_worker * context_.rank();
+        size_t local_elements = elements_per_worker;
 
-        if (context_.number_worker() == context_.rank() + 1) {
-            //last worker gets leftovers
-            local_elements = size_ -
-                             ((context_.number_worker() - 1) *
-                              (size_ / context_.number_worker()));
-        }
-        else {
-            local_elements = (size_ / context_.number_worker());
+        //last worker might has less elements than others
+        if (context_.max_rank() == context_.rank() && context_.max_rank() > 0) {
+            size_t elements_of_workers_before = elements_per_worker * context_.max_rank();
+            local_elements = size_ - elements_of_workers_before;
         }
 
         for (size_t i = 0; i < local_elements; i++) {

c7a/api/generate_from_file.hpp
@@ -95,14 +95,14 @@ public:
         }
 
         size_t local_elements;
-        if (context_.number_worker() == context_.rank() + 1) {
+        if (context_.max_rank() == context_.rank()) {
             //last worker gets leftovers
             local_elements = size_ -
-                             ((context_.number_worker() - 1) *
-                              (size_ / context_.number_worker()));
+                             ((context_.max_rank()) *
+                              (size_ / context_.max_rank() + 1));
         }
         else {
-            local_elements = (size_ / context_.number_worker());
+            local_elements = (size_ / context_.max_rank() + 1);
         }
 
         std::random_device random_device;

c7a/api/read.hpp
@@ -88,7 +88,7 @@ public:
         assert(file.good());
 
         InputLineIterator it = GetInputLineIterator(
-            file, context_.rank(), context_.number_worker());
+            file, context_.rank(), context_.max_rank());
 
         // Hook Read
         while (it.HasNext()) {

commit 98e89e26a169741b1001873e5cdc114d31dd0599

fix another off-by-one error in reduce_to_index - introduced by d2bec24de8b6

c7a/api/context.hpp
@@ -86,6 +86,10 @@ public:
         return number_computing_node() * workers_per_computing_node() - 1;
     }
 
+    size_t num_workers() {
+        return max_rank() + 1;
+    }
+
     //! Returns id of this computing_node in the cluser
     //! A computing_node is a machine in the cluster that hosts multiple workers
     size_t computing_node_id() const {

c7a/api/input_line_iterator.hpp
@@ -23,6 +23,8 @@ namespace c7a {
 class InputLineIterator
 {
 public:
+    static const bool debug = false;
+
     //! Creates an instance of iterator that reads file line based
     InputLineIterator(std::ifstream& file,
                       size_t my_node_id,

c7a/api/reduce_to_index.hpp
@@ -96,7 +96,7 @@ public:
           reduce_function_(reduce_function),
           channel_(parent.ctx().data_manager().GetNewChannel()),
           emitters_(channel_->OpenWriters()),
-          reduce_pre_table_(parent.ctx().max_rank() + 1, key_extractor,
+          reduce_pre_table_(parent.ctx().num_workers(), key_extractor,
                             reduce_function_, emitters_,
                             core::HashByIndex(max_index)),
           max_index_(max_index),
@@ -136,11 +136,11 @@ public:
         size_t min_local_index =
             std::ceil(static_cast<double>(max_index_ + 1)
                       * static_cast<double>(context_.rank())
-                      / static_cast<double>(context_.max_rank()));
+                      / static_cast<double>(context_.num_workers()));
         size_t max_local_index =
             std::ceil(static_cast<double>(max_index_ + 1)
-                      * static_cast<double>(context_.rank())
-                      / static_cast<double>(context_.max_rank())) - 1;
+                      * static_cast<double>(context_.rank() + 1)
+                      / static_cast<double>(context_.num_workers())) - 1;
 
         if (context_.rank() == context_.max_rank()) {
             max_local_index = max_index_;