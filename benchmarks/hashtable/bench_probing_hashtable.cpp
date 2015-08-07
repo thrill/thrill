529807ca14d6611eab5c06dc3106ae9a699701ea
Matthias Stumpp <mstumpp@gmail.com>
2015-08-07 14:23:02 +0200

    make explicit functions inlined

--- benchmarks/hashtable/bench_probing_hashtable.cpp
+++ benchmarks/hashtable/bench_probing_hashtable.cpp
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) {
         writers.emplace_back(sinks[i].GetWriter());
     }
 
-    core::ReducePreProbingTable<decltype(key_ex), decltype(red_fn), true>
+    core::ReducePreProbingTable<int, int, decltype(key_ex), decltype(red_fn), true>
     table(workers, num_buckets_init_scale, num_buckets_resize_scale,
           max_partition_fill_ratio, max_num_items_table, key_ex, red_fn, writers, -1);
 
