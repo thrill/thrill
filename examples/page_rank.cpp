9f9d194ce53e70dcafacfdd05b2672a4c6fb5df0
Alexander Noe <aleexnoe@gmail.com>
2015-08-07 17:11:04 +0200

    page rank - works only when an action is in every iteration

--- examples/page_rank.cpp
+++ examples/page_rank.cpp
@@ -13,7 +13,7 @@
 #include <c7a/api/reduce_to_index.hpp>
 #include <c7a/api/size.hpp>
 #include <c7a/api/sum.hpp>
-#include <c7a/api/write_lines_many.hpp>
+#include <c7a/api/write_lines.hpp>
 #include <c7a/api/zip.hpp>
 #include <c7a/common/cmdline_parser.hpp>
 #include <c7a/common/string.hpp>
@@ -32,34 +32,29 @@ void page_rank(Context& ctx) {
 
     static const double s = 0.85;
 
-    using PageWithLinks = std::tuple<int, std::vector<int> >;
-    using PageWithRank = std::tuple<int, double>;
-    using Page = std::tuple<int, double, std::vector<int> >;
+	using PageWithLinks = std::pair<size_t, std::vector<int> >;
+    using PageWithRank = std::pair<size_t, double>;
+    using Page = std::tuple<size_t, double, std::vector<int> >;
 
-    auto key_page_with_rank = [](PageWithRank in) {
-                                  return (size_t)std::get<0>(in);
-                              };
+    DIARef<PageWithRank> ranks =
+        ReadLines(ctx, "pagerank.in")
+        .Map([](const std::string& input) {				
+				auto splitted = c7a::common::split(input, " ");
+				return std::make_pair((size_t) std::stoi(splitted[0]), 1.0);
+			}).Cache();
+
+	size_t size = ranks.Size();
 
     auto links = ReadLines(ctx, "pagerank.in")
                  .Map([](const std::string& line) {
                           auto splitted = c7a::common::split(line, " ");
-
                           std::vector<int> links;
                           links.reserve(splitted.size() - 1);
                           for (size_t i = 1; i < splitted.size(); i++) {
                               links.push_back(std::stoi(splitted[i]));
                           }
-                          return std::make_tuple(std::stoi(splitted[0]), links);
-                      });
-
-    auto size = links.Size();
-
-    DIARef<PageWithRank> ranks =
-        links
-        .Map([](PageWithLinks input) {
-                 return std::make_tuple(std::get<0>(input), 1.0);
-             })
-        .Collapse();
+                          return std::make_pair(std::stoi(splitted[0]), links);
+					 });
 
     for (size_t i = 1; i <= 10; ++i) {
         std::cout << "Iteration: " << i << std::endl;
@@ -67,9 +62,7 @@ void page_rank(Context& ctx) {
         auto pages =
             links
             .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
-                     return std::make_tuple(std::get<0>(first),
-                                            std::get<1>(second),
-                                            std::get<1>(first));
+					return std::make_tuple(first.first, second.second, first.second);
                  });
 
         auto contribs =
@@ -78,52 +71,35 @@ void page_rank(Context& ctx) {
                 [](Page page, auto emit) {
                     std::vector<int> urls = std::get<2>(page);
                     double rank = std::get<1>(page);
-                    int num_urls = urls.size();
+                    double num_urls = urls.size();
                     for (int url : urls) {
-                        emit(std::make_tuple(url, rank / num_urls));
+                        emit(std::make_pair(url, rank / num_urls));
                     }
-                    emit(std::make_tuple(std::get<0>(page), 0.0));
                 });
-
-        double dangling_sum =
-            pages
-            .Filter([](Page input) {
-                        int num_urls = std::get<2>(input).size();
-                        return (num_urls == 0);
-                    })
-            .Map([](Page page) {
-                     return std::get<1>(page);
-                 })
-            .Sum([](double first, double second) {
-                     return first + second;
-                 });
 
         ranks =
             contribs
-            .ReduceToIndex(
-                key_page_with_rank,
-                [](PageWithRank rank1, PageWithRank rank2) {
-                    return std::make_tuple(
-                        std::get<0>(rank1),
-                        std::get<1>(rank1) + std::get<1>(rank2));
+            .ReducePairToIndex(
+                [](double rank1, double rank2) {
+                    return rank1 + rank2;
                 },
-                size - 1)
-            .Map([&dangling_sum](PageWithRank input) {
-                     int url = std::get<0>(input);
-                     double rank = std::get<1>(input);
-                     return std::make_tuple(url, rank * s + dangling_sum * s + (1 - s));
-                 })
-            .Collapse();
+                size)
+			.Map([](PageWithRank page) {
+			 		return std::make_pair(page.first, (1 - s) + page.second * s);
+			 	})
+			.Cache();
+
+		pages.Size();
     }
 
-    ranks.Map([](const PageWithRank& item) {
-                  return std::to_string(std::get<0>(item))
-                  + ": " + std::to_string(std::get<1>(item));
+    ranks.Map([](PageWithRank item) {
+                  return std::to_string(item.first)
+                  + ": " + std::to_string(item.second);
               }).
-    WriteLinesMany("pagerank_" + std::to_string(ctx.my_rank()) + ".out");
+    WriteLines("pagerank.out");
 }
 
-int main(int argc, char* argv[]) {
+int main(int, char**) {
     return c7a::api::Run(page_rank);
 }
 
