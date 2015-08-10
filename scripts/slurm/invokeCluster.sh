8f1a23835d7196fe6b8ef075eb9821c860e5d4b3
Timo Bingmann <tbgit@panthema.net>
2015-08-10 09:39:17 +0200

    Fixing up slurm bootstrap scripts for uc2. Mapping hosts -> infiniband IPs

--- scripts/slurm/invokeCluster.sh
+++ scripts/slurm/invokeCluster.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 
 NODES=${1-10}
 
-msub -V -N test -l nodes=$NODES:ppn=1,walltime=0:00:01 ./invokeWrapper.sh
+msub -V -N test -l nodes=$NODES:ppn=1,walltime=0:05:00 ./invokeWrapper.sh
--- scripts/slurm/invokeInteractive.sh
+++ scripts/slurm/invokeInteractive.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
-msub -I -V -N test -l nodes=1:ppn=1,walltime=0:00:10 ./invoke.sh
+msub -I -V -N test -l nodes=1:ppn=1,walltime=0:01:00 ./invoke.sh
