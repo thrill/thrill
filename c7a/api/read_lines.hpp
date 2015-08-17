c0525f8b0d3dc22bea537ffe6557c88bf2c89342
Alexander Noe <aleexnoe@gmail.com>
2015-08-17 14:23:57 +0200

    read now works with lowlevel file io

--- c7a/api/read_lines.hpp
+++ c7a/api/read_lines.hpp
@@ -15,6 +15,7 @@
 
 #include <c7a/api/dia.hpp>
 #include <c7a/api/dop_node.hpp>
+#include <c7a/net/buffer_builder.hpp>
 #include <c7a/common/logger.hpp>
 
 #include <fstream>
@@ -55,11 +56,10 @@ public:
         : Super(ctx, { }, "Read", stats_node),
           path_(path)
     { 
-
 		glob_t glob_result;
 		struct stat filestat;
 		glob(path_.c_str(),GLOB_TILDE,NULL,&glob_result);
-		std::streampos directory_size = 0;
+		size_t directory_size = 0;
 
 		for(unsigned int i=0;i<glob_result.gl_pathc;++i){
 			std::string filepath = std::string(glob_result.gl_pathv[i]);
@@ -83,9 +83,6 @@ public:
     void Execute() final { }
 
     void PushData() final {
-        static const bool debug = false;
-        LOG << "READING data " << result_file_.ToString();
-
         InputLineIterator it = GetInputLineIterator(
             filesize_prefix, context_.my_rank(), context_.num_workers());
 
@@ -122,14 +119,16 @@ private:
     //! Path of the input file.
     std::string path_;
 
-	std::vector<std::pair<std::string, std::streampos> > filesize_prefix;
+	std::vector<std::pair<std::string, size_t> > filesize_prefix;
 
     //! InputLineIterator gives you access to lines of a file
     class InputLineIterator
     {
     public:
+		
+		const size_t read_size = 2 * 1024 * 1024;
         //! Creates an instance of iterator that reads file line based
-        InputLineIterator(std::vector<std::pair<std::string, std::streampos>> files,
+        InputLineIterator(std::vector<std::pair<std::string, size_t>> files,
                           size_t my_id,
                           size_t num_workers)
             : files_(files),
@@ -139,8 +138,8 @@ private:
             file_size_ = files[files.size() - 1].second;
 
             // Go to start of 'local part'.
-            std::streampos per_worker = file_size_ / num_workers_;
-            std::streampos my_start = per_worker * my_id_;
+            size_t per_worker = file_size_ / num_workers_;
+            size_t my_start = per_worker * my_id_;
             if (my_id_ == (num_workers - 1)) {
                 my_end_ = file_size_ - 1;
             }
@@ -153,78 +152,115 @@ private:
 			}
 
 			c_file_ = open(files_[current_file_].first.c_str(), O_RDONLY);
-			
-			file_.open(files_[current_file_].first, std::ifstream::in);
 
+			//find offset in current file:
+			//offset = start - sum of previous file sizes 
 			if (current_file_) {
 				offset_ = lseek(c_file_, my_start - files_[current_file_ - 1].second, SEEK_CUR);
-				file_.seekg(my_start - files_[current_file_ - 1].second, std::ios::beg);
 				current_size_ = files_[current_file_].second - files_[current_file_ - 1].second;
 			} else {		
 				offset_ = lseek(c_file_, my_start, SEEK_CUR);
-				file_.seekg(my_start, std::ios::beg);
 				current_size_ = files_[0].second;
 			}
 
-            // Go to next new line if the stream-pointer is not at the beginning
-            // of a line
-            if (file_.tellg() != 0) {
-                std::streampos previous = file_.tellg() - (std::streampos) 1;
-                file_.seekg(previous, std::ios::beg);
-                if (file_.get() != '\n') {
-                    std::string str;
-                    std::getline(file_, str);
-                }				
-            }
 
 			if (offset_ != 0) {
-                std::streampos previous = file_.tellg() - (std::streampos) 1;
-                file_.seekg(previous, std::ios::beg);
-                if (file_.get() != '\n') {
-                    std::string str;
-                    std::getline(file_, str);
-                }				
-            }
+				offset_ = lseek(c_file_, -1, SEEK_CUR);	
+				bb_.Reserve(read_size);
+				ssize_t buffer_size = read(c_file_, bb_.data(), read_size);
+				bb_.set_size(buffer_size);
+				buffer_ = bb_.ToBuffer();
+				current_ = 1;
+				if (buffer_[0] != '\n') {
+					bool found_n = false;
+					//find next newline, discard all previous data as previous worker already covers it
+					while (!found_n) {
+						for (auto it = buffer_.begin() + current_; it != buffer_.end(); it++) { 
+							if (*it == '\n') {
+								current_ = it - buffer_.begin() + 1;
+								found_n = true;
+								break;
+							}
+						}
+						//no newline found: read new data into buffer_builder
+						if (!found_n) {
+							current_ = 0;							
+							offset_ += buffer_.size();
+							bb_.Reserve(read_size);
+							buffer_size = read(c_file_, bb_.data(), read_size);
+							bb_.set_size(buffer_size);
+							buffer_ = bb_.ToBuffer();
+						}
+					}					
+					assert(buffer_[current_ - 1] == '\n');
+				}
+            } else {
+				bb_.Reserve(read_size);
+				ssize_t buffer_size = read(c_file_, bb_.data(), read_size);
+				bb_.set_size(buffer_size);
+				buffer_ = bb_.ToBuffer();
+			}
         }
 
         //! returns the next element if one exists
         //!
         //! does no checks whether a next element exists!
         std::string Next() {
-			//go to next file
-			if (file_.tellg() >= current_size_ - (std::streampos) 1) {
-				file_.close();
-				current_file_++;
-				file_.open(files_[current_file_].first, std::ifstream::in);
-				assert(file_.good());
-				current_size_ = files_[current_file_].second - files_[current_file_ - 1].second;
+			while (true) {
+				std::string ret;
+				for (auto it = buffer_.begin() + current_; it != buffer_.end(); it++) { 
+					if (*it == '\n') {
+						size_t strlen = it - buffer_.begin() - current_;
+						current_ = it - buffer_.begin() + 1;
+						return ret.append(buffer_.PartialToString(current_ - strlen - 1, strlen));
+					}
+				}
+				ret.append(buffer_.PartialToString(current_, buffer_.size() - current_));
+				current_ = 0;
+				bb_.Reserve(read_size);
+				ssize_t buffer_size = read(c_file_, bb_.data(), read_size);
+				offset_ += buffer_.size();
+				if (buffer_size) {
+					bb_.set_size(buffer_size);
+					buffer_ = bb_.ToBuffer();
+				} else {
+					close(c_file_);
+					current_file_++;
+					offset_ = 0;
+					c_file_ = open(files_[current_file_].first.c_str(), O_RDONLY);					
+					bb_.Reserve(read_size);
+					ssize_t buffer_size = read(c_file_, bb_.data(), read_size);
+					bb_.set_size(buffer_size);
+					buffer_ = bb_.ToBuffer();
+					
+					if (ret.length()) {
+						return ret;
+					}
+				}
 			}
-            std::string line;
-            std::getline(file_, line);
-            return line;
         }
 
         //! returns true, if an element is available in local part
         bool HasNext() {
             if (current_file_) {
-				return (file_.tellg() + files_[current_file_ - 1].second <= my_end_);
+				return (offset_ + current_ + files_[current_file_ - 1].second <= my_end_);
 			} else {
-				return file_.tellg() <= my_end_;
+				return offset_ + current_ <= my_end_;
 			}
         }
 
     private:
         //! Input file stream
-        std::vector<std::pair<std::string, std::streampos> > files_;
+        std::vector<std::pair<std::string, size_t> > files_;
 		//! Index of current file in files_
 		size_t current_file_ = 0;
 		//!Size of current file in bytes
-		std::streampos current_size_;
+		size_t current_size_;
 		//! Current stream, from files_[current_file_]
 		std::ifstream file_;
 
 		int c_file_;
-		off_t offset_;
+		size_t offset_;
         //! File size in bytes
         size_t file_size_;
         //! Worker ID
@@ -232,7 +268,11 @@ private:
         //! total number of workers
         size_t num_workers_;
         //! end of local block
-        std::streampos my_end_;
+        size_t my_end_;
+
+		net::BufferBuilder bb_;
+		net::Buffer buffer_;
+		size_t current_ = 0;
     };
 
     //! Returns an InputLineIterator with a given input file stream.
@@ -243,7 +283,7 @@ private:
     //!
     //! \return An InputLineIterator for a given file stream
     InputLineIterator GetInputLineIterator(
-        std::vector<std::pair<std::string, std::streampos> > files, size_t my_id, size_t num_work) {
+        std::vector<std::pair<std::string, size_t> > files, size_t my_id, size_t num_work) {
         return InputLineIterator(files, my_id, num_work);
     }
 };
