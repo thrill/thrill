1cf733faa7c30f730fc9920c562412191e4af086
Timo Bingmann <tbgit@panthema.net>
2015-08-20 08:09:40 +0200

    Renaming libc7a -> libthrill and more in CMake scripts.

--- tests/core/stage_builder_test.cpp
+++ tests/core/stage_builder_test.cpp
@@ -7,7 +7,7 @@
  * This file has no license. Only Chuck Norris can compile it.
  ******************************************************************************/
 
-#include <thrill/c7a.hpp>
+#include <thrill/thrill.hpp>
 #include <gtest/gtest.h>
 
 #include <algorithm>
