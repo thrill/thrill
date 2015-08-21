f99c8d91194e35e77d584c0220c276d76a851269
Timo Bingmann <tbgit@panthema.net>
2015-08-21 20:21:20 +0200

    Adding C++11 things and Deleter to CountingPtr.

--- tests/common/counting_ptr_test.cpp
+++ tests/common/counting_ptr_test.cpp
@@ -36,14 +36,14 @@ using IntegerPtr = common::CountingPtr<MyInteger>;
 using IntegerCPtr = common::CountingPtr<const MyInteger>;
 
 IntegerPtr MakeIntegerPtr() {
-    return new MyInteger(24);
+    return IntegerPtr(new MyInteger(24));
 }
 
 TEST(CountingPtr, RunTest) {
     {
         {
             // create object and pointer to it
-            IntegerPtr i1 = new MyInteger(42);
+            IntegerPtr i1 = IntegerPtr(new MyInteger(42));
 
             ASSERT_EQ(42, i1->i);
             ASSERT_EQ(42, (*i1).i);
@@ -73,7 +73,7 @@ TEST(CountingPtr, RunTest) {
         // check number of objects destructed
         ASSERT_EQ(2, count_deletes);
 
-        // create a const pointer
+        // create a const pointer from a normal ptr.
         IntegerCPtr i4 = MakeIntegerPtr();
 
         // quitting the block will release the ptr
