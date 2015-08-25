36ff31cb227c631316d21cdf218a37d5aaa72318
Alexander Noe <aleexnoe@gmail.com>
2015-08-25 16:41:53 +0200

    Uncrustify.

--- thrill/core/read_file_list.hpp
+++ thrill/core/read_file_list.hpp
@@ -22,49 +22,49 @@ namespace core {
 
 using FileSizePair = std::pair<std::string, size_t>;
 
-//Returns true, if file at filepath is compressed (e.g, ends with '.[gz/bz2,xz,lzo]')
+// Returns true, if file at filepath is compressed (e.g, ends with '.[gz/bz2,xz,lzo]')
 bool IsCompressed(const std::string& path) {
-	return common::ends_with(path, ".gz") ||
-		common::ends_with(path, ".bz2") ||
-		common::ends_with(path, ".xz") ||
-		common::ends_with(path, ".lzo");
+    return common::ends_with(path, ".gz") ||
+           common::ends_with(path, ".bz2") ||
+           common::ends_with(path, ".xz") ||
+           common::ends_with(path, ".lzo");
 }
-	
-std::pair<std::vector<FileSizePair>,bool> ReadFileList(const std::string& path) {
 
-	bool contains_compressed_file = false;
+std::pair<std::vector<FileSizePair>, bool> ReadFileList(const std::string& path) {
 
-	std::vector<FileSizePair> filesize_prefix;
-	glob_t glob_result;
-	struct stat filestat;
-	glob(path.c_str(), GLOB_TILDE, nullptr, &glob_result);
-	size_t directory_size = 0;
+    bool contains_compressed_file = false;
 
-	for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
-		const char* filepath = glob_result.gl_pathv[i];
+    std::vector<FileSizePair> filesize_prefix;
+    glob_t glob_result;
+    struct stat filestat;
+    glob(path.c_str(), GLOB_TILDE, nullptr, &glob_result);
+    size_t directory_size = 0;
 
-		if (stat(filepath, &filestat)) {
-			throw std::runtime_error(
-				"ERROR: Invalid file " + std::string(filepath));
-		}
-		if (!S_ISREG(filestat.st_mode)) continue;
+    for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
+        const char* filepath = glob_result.gl_pathv[i];
 
-		if (IsCompressed(filepath)) {
-			contains_compressed_file = true;
-		}
+        if (stat(filepath, &filestat)) {
+            throw std::runtime_error(
+                      "ERROR: Invalid file " + std::string(filepath));
+        }
+        if (!S_ISREG(filestat.st_mode)) continue;
 
-		filesize_prefix.emplace_back(std::move(filepath), directory_size);
-		directory_size += filestat.st_size;
-	}
-	filesize_prefix.emplace_back("", directory_size);
-	globfree(&glob_result);
+        if (IsCompressed(filepath)) {
+            contains_compressed_file = true;
+        }
 
-	return std::make_pair(filesize_prefix, contains_compressed_file);
+        filesize_prefix.emplace_back(std::move(filepath), directory_size);
+        directory_size += filestat.st_size;
+    }
+    filesize_prefix.emplace_back("", directory_size);
+    globfree(&glob_result);
+
+    return std::make_pair(filesize_prefix, contains_compressed_file);
 }
 
 } // namespace core
 } // namespace thrill
 
-#endif // !THRILL_CORE_REDUCE_PRE_PROBING_TABLE_HEADER
+#endif // !THRILL_CORE_READ_FILE_LIST_HEADER
 
 /******************************************************************************/
