9ae312d4401069f42bac9bdc3b90f6c87a48a71d
Alexander Noe <aleexnoe@gmail.com>
2015-08-26 14:35:32 +0200

    improve io_test

--- tests/api/io_test.cpp
+++ tests/api/io_test.cpp
@@ -21,11 +21,68 @@
 #include <string>
 #include <vector>
 
+#include <glob.h>
+#include <sys/stat.h>
+
 using namespace thrill;
 using thrill::api::Context;
 using thrill::api::DIARef;
 
-TEST(IO, GenerateFromFileCorrectAmountOfCorrectIntegers) {
+TEST(IO, ReadSingleFile) {
+    std::function<void(Context&)> start_func =
+        [](Context& ctx) {
+            auto integers = ReadLines(ctx, "test1")
+                            .Map([](const std::string& line) {
+                                     return std::stoi(line);
+                                 });
+
+            std::vector<int> out_vec = integers.AllGather();
+
+            int i = 1;
+            for (int element : out_vec) {
+                ASSERT_EQ(element, i++);
+            }
+
+            ASSERT_EQ((size_t)16, out_vec.size());
+        };
+
+    api::RunLocalTests(start_func);
+}
+
+TEST(IO, ReadFolder) {
+    std::function<void(Context&)> start_func =
+        [](Context& ctx) {
+            ASSERT_EQ(ReadLines(ctx, "read_folder/*").Size(), 20);
+        };
+
+    api::RunLocalTests(start_func);
+}
+
+TEST(IO, ReadPartOfFolderCompressed) {
+    std::function<void(Context&)> start_func =
+        [](Context& ctx) {
+		//folder read_ints contains compressed and non-compressed files with integers
+		//from 25 to 1 and a file 'donotread', which contains non int-castable
+		//strings
+            auto integers = ReadLines(ctx, "read_ints/read*")
+                            .Map([](const std::string& line) {
+                                     return std::stoi(line);
+                                 });
+
+            std::vector<int> out_vec = integers.AllGather();
+
+            int i = 25;
+            for (int element : out_vec) {
+                ASSERT_EQ(element, i--);
+            }
+
+            ASSERT_EQ((size_t)25, out_vec.size());
+        };
+
+    api::RunLocalTests(start_func);
+}
+
+TEST(IO, GenerateFromFileRandomIntegers) {
     api::RunSameThread(
         [](api::Context& ctx) {
             std::default_random_engine generator({ std::random_device()() });
@@ -43,7 +100,7 @@ TEST(IO, GenerateFromFileCorrectAmountOfCorrectIntegers) {
 
             size_t writer_size = 0;
 
-            input.Map(
+			input.Map(
                 [&writer_size](const int& item) {
                     // file contains ints between 1  and 16
                     // fails if wrong integer is generated
@@ -52,8 +109,9 @@ TEST(IO, GenerateFromFileCorrectAmountOfCorrectIntegers) {
                     writer_size++;
                     return std::to_string(item) + "\n";
                 })
-            .WriteBinary("test1.out");
+            .WriteLinesMany("out1_");
 
+			//DIA contains as many elements as we wanted to generate
             ASSERT_EQ(generate_size, writer_size);
         });
 }
@@ -92,106 +150,80 @@ TEST(IO, GenerateStringWriteBinary) {
         });
 }
 
-TEST(IO, ReadFolder) {
-    std::function<void(Context&)> start_func =
-        [](Context& ctx) {
-            ASSERT_EQ(ReadLines(ctx, "read_folder/*").Size(), 20);
-        };
-
-    api::RunLocalTests(start_func);
-}
-
-TEST(IO, WriteToSingleFile) {
+TEST(IO, WriteBinaryCorrectSize) {
 
     std::function<void(Context&)> start_func =
         [](Context& ctx) {
 
-            std::string path = "testsf.out";
+		auto integers = ReadLines(ctx, "test1")
+		.Map([](const std::string& line) {
+				return std::stoi(line);
+			});
 
-            auto integers = ReadLines(ctx, "test1")
-                            .Map([](const std::string& line) {
-                                     return std::stoi(line);
-                                 });
+		integers.WriteBinary("binary/output_");		
 
-            integers.WriteBinary("binary/output_");
+		ctx.Barrier();
 
-            ctx.Barrier();
+		if (ctx.my_rank() == 0) {
+			glob_t glob_result;
+			struct stat filestat;
+			glob("binary/*", GLOB_TILDE, nullptr, &glob_result);
+			size_t directory_size = 0;
 
-            int bla = 5;
+			for (unsigned int i = 0; i < glob_result.gl_pathc; ++i) {
+				const char* filepath = glob_result.gl_pathv[i];
 
-            auto integers2 = ReadBinary(ctx, "binary/*", bla);
+				if (stat(filepath, &filestat)) {
+					throw std::runtime_error(
+						"ERROR: Invalid file " + std::string(filepath));
+				}
+				if (!S_ISREG(filestat.st_mode)) continue;
 
-            integers2.Map(
-                [](const int& item) {
-                    return std::to_string(item);
-                })
-            .WriteLines(path);
+				directory_size += filestat.st_size;
 
-            // Race condition as one worker might be finished while others are
-            // still writing to output file.	
-            ctx.Barrier();			
-			if (ctx.my_rank() == 0) {
-				std::system("rm -r ./binary/*");
+				remove(filepath);
 			}
+			globfree(&glob_result);
 
-            std::ifstream file(path);
-            size_t begin = file.tellg();
-            file.seekg(0, std::ios::end);
-            size_t end = file.tellg();
-            ASSERT_EQ(end - begin, 39);
-            file.seekg(0);
-            for (int i = 1; i <= 16; i++) {
-                std::string line;
-                std::getline(file, line);
-                ASSERT_EQ(std::stoi(line), i);
-            }
-        };
+			ASSERT_EQ(16 * sizeof(int), directory_size);
+		}
+	};
 
     api::RunLocalTests(start_func);
 }
 
-TEST(IO, ReadAndAllGatherElementsCorrect) {
+TEST(IO, ReadBinary) {
 
     std::function<void(Context&)> start_func =
         [](Context& ctx) {
 
-            auto integers = ReadLines(ctx, "test1")
-                            .Map([](const std::string& line) {
-                                     return std::stoi(line);
-                                 });
+		std::string path = "testsf.out";
 
-            std::vector<int> out_vec = integers.AllGather();
+		int bla = 5;
+		auto integers2 = ReadBinary(ctx, "./binary" + std::to_string(ctx.num_workers()) + "/*", bla);
 
-            int i = 1;
-            for (int element : out_vec) {
-                ASSERT_EQ(element, i++);
-            }
+		integers2.Map(
+			[](const int& item) {
+				return std::to_string(item);
+			})
+		.WriteLines(path);
 
-            ASSERT_EQ((size_t)16, out_vec.size());
-        };
+		// Race condition as one worker might be finished while others are
+		// still writing to output file.	
+		ctx.Barrier();
 
-    api::RunLocalTests(start_func);
-}
-
-TEST(IO, ReadPartOfFolderAndAllGatherElementsCorrect) {
-
-    std::function<void(Context&)> start_func =
-        [](Context& ctx) {
-
-            auto integers = ReadLines(ctx, "read_ints/read*")
-                            .Map([](const std::string& line) {
-                                     return std::stoi(line);
-                                 });
-
-            std::vector<int> out_vec = integers.AllGather();
-
-            int i = 25;
-            for (int element : out_vec) {
-                ASSERT_EQ(element, i--);
-            }
-
-            ASSERT_EQ((size_t)25, out_vec.size());
-        };
+		std::ifstream file(path);
+		size_t begin = file.tellg();
+		file.seekg(0, std::ios::end);
+		size_t end = file.tellg();
+		ASSERT_EQ(end - begin, 39);
+		file.seekg(0);
+		for (int i = 1; i <= 16; i++) {
+			std::string line;
+			std::getline(file, line);
+			ASSERT_EQ(std::stoi(line), i);
+		}
+	};
 
     api::RunLocalTests(start_func);
 }
