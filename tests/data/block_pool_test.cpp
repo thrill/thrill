838c1e3ac72d6da8d292fd34f17ee52cf4a16ae8
Tobias Sturm <mail@tobiassturm.de>
2015-09-06 10:55:23 +0200

    add tests for block pool

---
+++ tests/data/block_pool_test.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * tests/data/block_pool_test.cpp
+ *
+ * Part of Project Thrill.
+ *
+ * Copyright (C) 2015 Tobias Sturm <mail@tobiassturm.de>
+ *
+ * This file has no license. Only Chunk Norris can compile it.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <thrill/data/block_pool.hpp>
+
+#include <string>
+
+using namespace thrill;
+
+struct BlockPoolTest : public::testing::Test {
+    BlockPoolTest() : mem_manager_(nullptr), block_pool_(&mem_manager_) { }
+
+    mem::Manager mem_manager_;
+    data::BlockPool block_pool_;
+};
+
+TEST_F(BlockPoolTest, AllocateAccountsSizeInManager) {
+    auto block = block_pool_.AllocateBlock(8);
+    ASSERT_EQ(8u, mem_manager_.total());
+    auto block2 = block_pool_.AllocateBlock(2);
+    ASSERT_EQ(10u,  mem_manager_.total());
+}
+
+TEST_F(BlockPoolTest, AllocateIncreasesBlockCountByOne) {
+    auto block = block_pool_.AllocateBlock(8);
+    ASSERT_EQ(1u, block_pool_.block_count());
+}
+
+TEST_F(BlockPoolTest, BlocksOutOfScopeReduceBlockCount) {
+    {
+        auto block = block_pool_.AllocateBlock(8);
+    }
+    ASSERT_EQ(0u, block_pool_.block_count());
+}
+
+TEST_F(BlockPoolTest, BlocksOutOfScopeAreAccountetInMemManager) {
+    {
+        auto block = block_pool_.AllocateBlock(8);
+    }
+    ASSERT_EQ(0u, mem_manager_.total());
+}
+
+TEST_F(BlockPoolTest, AllocatedBlocksHaveRefCountOne) {
+    auto block = block_pool_.AllocateBlock(8);
+    ASSERT_EQ(1u, block->reference_count());
+}
+
+
+
+/******************************************************************************/
