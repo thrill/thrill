0dd0a56559bd32eec3ee6131d2bc1433446394dd
Tobias Sturm <mail@tobiassturm.de>
2015-09-09 19:55:31 +0200

    update PageMapper
    - use file backing
    - use SHARED instead of PRIVATE flag. Otherwise the memory reagion is not actually written to disk (docu was not so clear about that. Thank you unit tests)
    - Extended unit tests that check memory content after re-mapping
    - Use swap tokens as identifier to re-map memory regions

--- tests/mem/page_mapper_test.cpp
+++ tests/mem/page_mapper_test.cpp
@@ -17,51 +17,87 @@ using namespace thrill;
 TEST(PageMapper, AllocateReturnsAccessibleMemoryArea) {
     mem::PageMapper mapper;
 
-    int* array = reinterpret_cast<int*>(mapper.Allocate());
+    size_t token;
+    int* array = reinterpret_cast<int*>(mapper.Allocate(token));
 
     //write into memory area to see if we get a segfault
     for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
         array[i] = i;
     }
 
-    mapper.Free(reinterpret_cast<char*>(array));
+    mapper.Free(token, reinterpret_cast<char*>(array));
 }
 
 TEST(PageMapper, FreeMakesMemoryAreaInaccessible) {
     mem::PageMapper mapper;
 
-    int* array = reinterpret_cast<int*>(mapper.Allocate());
-    mapper.Free(reinterpret_cast<char*>(array));
+    size_t token;
+    int* array = reinterpret_cast<int*>(mapper.Allocate(token));
+    mapper.Free(token, reinterpret_cast<char*>(array));
 
     ASSERT_DEATH({ array[1] = 42; }, "");
 }
 
-TEST(PageMapper, SwapOutLeavesAreaAccessible) {
+TEST(PageMapper, SwapOutLeavesAreaInaccessible) {
     mem::PageMapper mapper;
 
-    int* array = reinterpret_cast<int*>(mapper.Allocate());
+    size_t token;
+    int* array = reinterpret_cast<int*>(mapper.Allocate(token));
     mapper.SwapOut(reinterpret_cast<char*>(array));
 
-    //write into memory area to see if we get a segfault
-    for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
-        array[i] = i;
-    }
-
-    mapper.Free(reinterpret_cast<char*>(array));
+    ASSERT_DEATH({ array[1] = 42; }, "");
+    mapper.Free(token);
 }
 
-TEST(PageMapper, PrefetchLeavesAreaAccessible) {
+TEST(PageMapper, SwapInLeavesMakesAreaAccessible) {
     mem::PageMapper mapper;
 
-    int* array = reinterpret_cast<int*>(mapper.Allocate());
-    mapper.Prefetch(reinterpret_cast<char*>(array));
+    size_t token;
+    int* array = reinterpret_cast<int*>(mapper.Allocate(token));
+    mapper.SwapOut(reinterpret_cast<char*>(array));
+    array = reinterpret_cast<int*>(mapper.SwapIn(token));
 
     //write into memory area to see if we get a segfault
     for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
         array[i] = i;
     }
 
-    mapper.Free(reinterpret_cast<char*>(array));
+    mapper.Free(token, reinterpret_cast<char*>(array));
+}
+
+TEST(PageMapper, SwappingMultiplePagesDoesNotAlterContent) {
+    mem::PageMapper mapper;
+
+    //write ascending numbers into array 1 & swap out
+    size_t token1;
+    int* array1 = reinterpret_cast<int*>(mapper.Allocate(token1));
+    for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
+        array1[i] = i;
+    }
+    mapper.SwapOut(reinterpret_cast<char*>(array1));
+
+    //write descending numbers into array 2 & swap out
+    size_t token2;
+    int* array2 = reinterpret_cast<int*>(mapper.Allocate(token2));
+    for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
+        array2[i] = mapper.object_size() - i;
+    }
+    mapper.SwapOut(reinterpret_cast<char*>(array2));
+
+    //read array1
+    array1 = reinterpret_cast<int*>(mapper.SwapIn(token1));
+    for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
+        ASSERT_EQ(i, array1[i]);
+    }
+
+    //read array2
+    array2 = reinterpret_cast<int*>(mapper.SwapIn(token2));
+    for(unsigned int i = 0; i * sizeof(int) < mapper.object_size() ; i++) {
+        ASSERT_EQ(mapper.object_size() - i, array2[i]);
+    }
+
+    mapper.Free(token1, reinterpret_cast<char*>(array1));
+    mapper.Free(token2, reinterpret_cast<char*>(array2));
 }
 
 namespace thrill {
--- thrill/mem/page_mapper.hpp
+++ thrill/mem/page_mapper.hpp
@@ -26,10 +26,14 @@
 #include <queue>
 
 #include <thrill/common/logger.hpp>
+#include <thrill/common/concurrent_queue.hpp>
 #include <thrill/data/byte_block.hpp>
 
 namespace thrill {
 namespace mem {
+
+#define SWAP_FILE_PATH "/tmp/thrill.swap"
+#define ADDITIONAL_GROWTH 7
 
 /*! The PageMapper maps objects onto disk using mmap and madvise and acts as
  * a mapper for the c syscalls.
@@ -47,45 +51,111 @@ public:
         //runtime check if object_size_ is correct
         long page_size = sysconf(_SC_PAGESIZE);
         die_unless(object_size % page_size == 0);
+
+        //create our swapfile
+        //- read + write access
+        //- create the file
+        //- delete content if file exists
+        //- this is gonna be a large file-> use 64bit ptrs
+        //- don't update access time
+        static const int flags = O_RDWR | O_CREAT | O_TRUNC | O_LARGEFILE | O_NOATIME;
+
+        // user can read+write, group may read
+        static const int permission = S_IRUSR | S_IWUSR | S_IRGRP;
+
+        std::remove(SWAP_FILE_PATH);
+        fd_ = open64(SWAP_FILE_PATH, flags, permission);
+        die_unless(fd_ != -1);
     }
 
-    char* Allocate() {
+    char* Allocate(size_t& result_token) {
+        sLOG << "allocate memory w/ disk backing";
+        result_token = next_free_token();
+        return SwapIn(result_token, false /*prefetch*/);
+    }
+
+    //! Returns the next free token and eventually streches the swapfile if
+    //! required
+    size_t next_free_token() {
+        size_t result = 0;
+        if (free_tokens_.try_pop(result)) {
+            sLOG << "reuse swap token" << result;
+            return result;
+        }
+
+        //remember result
+        result = next_token_;
+
+        //we allocate ADDITIONAL_GROWTH new ids,
+        //+1 since result is 0-based
+        size_t file_size = (1 + ADDITIONAL_GROWTH + result) * object_size_;
+        sLOG << "streching swap file to" << file_size;
+
+        //seek to end - 1 of file and write one zero-byte to 'strech' file
+        die_unless(lseek(fd_, file_size - 1, SEEK_SET) != -1);
+        die_unless(write(fd_, "\0", 1) == 1); //expect 1byte written
+
+        //push remaining allocated ids into free-queue
+        for(size_t token = result + 1; token <= next_token_ + ADDITIONAL_GROWTH; token++) {
+            sLOG << "create new swap token" << token;
+            free_tokens_.push(token);
+        }
+
+        next_token_++;
+        sLOG << "use swap token" << result;
+        return result;
+    }
+
+    //! Releases a memory region and it's token.
+    //! Does NOT write memory content back to disk
+    //!\param token that was returned from Allocate before
+    //!\param addr  of memory region. Use nullptr if area is swapped out.
+    void Free(size_t token, char* addr = nullptr) {
+        sLOG << "free swap token" << token << "(" << addr << ")";
+        if (addr != nullptr)
+            SwapOut(addr, false);
+        free_tokens_.push(token);
+    }
+
+    //! Swaps out a given memory region. The memory region will be invalidated and
+    //! is not accessible afterwards.
+    //! \param addr that was returned by Alloc before
+    //! \param write_back set to false if content must not be written to disk
+    void SwapOut(char* addr, bool write_back = true) {
+        //we might sometimes not write back, if we want to unmap a block but
+        //don't care about the content (block for net send operation)
+        if(write_back) {
+            sLOG << "writing back" << static_cast<void*>(addr);
+            die_unless(msync(static_cast<void*>(addr), object_size_, MS_SYNC) == 0);
+        }
+        sLOG << "unmapping " << static_cast<void*>(addr);
+        die_unless(munmap(static_cast<void*>(addr), object_size_) == 0);
+    }
+
+    //! Swaps in a given memory region.
+    //! \param token that was returned from Allocate before
+    //! \returns pointer to memory region
+    char* SwapIn(size_t token, bool prefetch = true) {
         //Flags exaplained:
         //- readable
         //- writeable
-        //- not shared with other processes -> no automatic writebacks
-        //- use huge pages (2MB on my machine) - not sure about this
-        //- don't zero out pages
-        //- anonymous = not backed by a file :)
         static const int protection_flags = PROT_READ | PROT_WRITE;
-        static const int flags = MAP_PRIVATE | MAP_ANONYMOUS;
+        //- not shared with other processes -> no automatic writebacks
+        //- no swap space reserved (we like to live dangerously)
+        //TODO we want MAP_HUGE_2MB
+        int flags = MAP_SHARED | MAP_NORESERVE;
+
+        //we don't want to prefetch when we allocate a fresh mapping
+        if (prefetch)
+            flags |= MAP_POPULATE;
+
         static void* addr_hint = nullptr; //we give no hint - kernel decides alone
-        static const int fd = -1; //for portability
-        static const size_t offset = 0; //for portability
-        void* result = mmap(addr_hint, object_size_, protection_flags, flags, fd, offset);
+        off64_t offset = token * object_size_;
+
+        void* result = mmap64(addr_hint, object_size_, protection_flags, flags, fd_, offset);
+        sLOG << "swapping in token" << token << "to address" << result << "into offset" << offset << "prefetch?" << prefetch;
         die_unless(result != MAP_FAILED);
         return static_cast<char*>(result);
-    }
-
-    void Free(char* addr) {
-        die_unless(munmap(addr, object_size_) == 0);
-    }
-
-    void SwapOut(char* addr) {
-        // flags explained in order
-        // - don't need pages in near future
-        static const int flags = MADV_DONTNEED;
-        int result = madvise(addr, object_size_, flags);
-        die_unless(result == EAGAIN || result == 0);
-    }
-
-    void Prefetch(char* addr) {
-        // flags explained in order
-        // - aggressig prefetching for sequential read access
-        // - will need pages in the memmory region -> plz prefetch Mr Kernel.
-        static const int flags = MADV_SEQUENTIAL | MADV_WILLNEED;
-        int result = madvise(addr, object_size_, flags);
-        die_unless(result == EAGAIN || result == 0);
     }
 
     //! Returns the object_size that is used for this mapper
@@ -94,7 +164,11 @@ public:
     }
 
 private:
+    static const bool debug = true;
     size_t object_size_;
+    int fd_;
+    size_t next_token_ = { 0 };
+    common::ConcurrentQueue<size_t, std::allocator<size_t>> free_tokens_;
 };
 
 } // namespace mem
