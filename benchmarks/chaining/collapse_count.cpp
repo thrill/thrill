3b704f5fe71cd536dd5362615d2630ee6451e810
Sebastian Lamm <seba.lamm@googlemail.com>
2015-09-11 13:56:13 +0200

    Possible node benchmark.

---
+++ benchmarks/chaining/collapse_count.cpp
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * benchmarks/word_count/word_count.cpp
+ *
+ * Runner program for WordCount example. See thrill/examples/word_count.hpp for
+ * the source to the example.
+ *
+ * Part of Project Thrill.
+ *
+ * Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+ *
+ * This file has no license. Only Chuck Norris can compile it.
+ ******************************************************************************/
+
+#include <thrill/api/dia.hpp>
+#include <thrill/common/cmdline_parser.hpp>
+#include <thrill/common/logger.hpp>
+#include <thrill/thrill.hpp>
+
+using WordCountPair = std::pair<std::string, size_t>;
+using namespace thrill; // NOLINT
+
+int main(int argc, char* argv[]) {
+
+    common::CmdlineParser clp;
+
+    clp.SetVerboseProcess(false);
+
+    std::string input;
+    clp.AddParamString("input", input,
+                       "input file pattern");
+
+    std::string output;
+    clp.AddParamString("output", output,
+                       "output file pattern");
+
+    if (!clp.Process(argc, argv)) {
+        return -1;
+    }
+
+    clp.PrintResult();
+
+    auto start_func =
+        [&input, &output](api::Context& ctx) {
+            auto input_dia = ReadLines(ctx, input);
+
+            std::string word;
+            // word.reserve(1024);
+            auto word_pairs = input_dia;
+            for (size_t i = 0; i < 1000; ++i) {
+                word_pairs = word_pairs.Map([](const std::string& line) {
+                        return line;
+                    }).Collapse();
+            }
+            word_pairs.WriteLinesMany(output);
+        };
+    LOG1 << "Done";
+
+    return api::Run(start_func);
+}
+
+/******************************************************************************/
---
+++ benchmarks/chaining/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+this="${BASH_SOURCE-$0}"
+build=$(cd -P -- "$(dirname -- "$this")" && pwd -P)/../../build/benchmarks/chaining
+
+set -e
+
+time ${build}/cache_count ${build}/headwords ${build}/output
+time ${build}/collapse_count ${build}/headwords ${build}/output
+# time ${build}/chain_count ${build}/headwords ${build}/output
