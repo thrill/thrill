83b5744d165970e3eb5df595d1c836ef7cc0ede7
Tobias Sturm <mail@tobiassturm.de>
2015-09-15 15:21:21 +0200

    more fair alloc benchmark

--- benchmarks/data/allocate.cpp
+++ benchmarks/data/allocate.cpp
@@ -21,9 +21,11 @@
 
 using namespace thrill; // NOLINT
 using common::StatsTimer;
+using data::default_block_size;
 
 unsigned g_iterations = 1;
 uint64_t g_allocations;
+unsigned g_swapfile_growth = 1;
 
 int main(int argc, const char** argv) {
     common::NameThisThread("benchmark");
@@ -33,6 +35,7 @@ int main(int argc, const char** argv) {
     clp.SetAuthor("Tobias Sturm <mail@tobiassturm.de>");
 
     clp.AddBytes('a', "allocs", g_allocations, "number of allocations");
+    clp.AddUInt('g', "growth", g_swapfile_growth, "growth of swap file (default: 1)");
     clp.AddUInt('n', "iterations", g_iterations, "Iterations (default: 1)");
 
     std::string experiment;
@@ -41,31 +44,42 @@ int main(int argc, const char** argv) {
 
     if (!clp.Process(argc, argv)) return -1;
 
+    std::vector<void*> allocations;
+    allocations.reserve(g_allocations);
     for (unsigned n = 0; n < g_iterations; n++) {
         StatsTimer<true> wall_time;
         if (experiment == "malloc") {
-            std::vector<void*> malloc_allocations;
-            malloc_allocations.reserve(g_allocations);
             wall_time.Start();
             for (unsigned int i = 0; i < g_allocations; i++) {
-                malloc_allocations[i] = malloc(thrill::data::default_block_size);
+                allocations[i] = malloc(default_block_size);
             }
             wall_time.Stop();
-            for (auto a : malloc_allocations)
+            for (auto a : allocations)
                 free(a);
 
         } else {
-            std::vector<uint8_t*> mmap_allocations;
-            thrill::mem::PageMapper<thrill::data::default_block_size> pm("/tmp/swapfile");
-            uint32_t token;
-            mmap_allocations.reserve(g_allocations);
+            static const int permission = S_IRUSR | S_IWUSR | S_IRGRP;
+            static int file_flags = O_RDWR | O_CREAT | O_TRUNC | O_LARGEFILE | O_NOATIME;
+            int fd_ = open("/tmp/swapfile", file_flags, permission);
+            die_unless(fd_ != -1);
+            static void* addr_hint = nullptr; //we give no hint - kernel decides alone
+            static const int protection_flags = PROT_READ | PROT_WRITE;
+            int flags = MAP_SHARED | MAP_NORESERVE;
+            size_t allocated_swapspace = 0;
             wall_time.Start();
             for (unsigned int i = 0; i < g_allocations; i++) {
-                mmap_allocations[i] = pm.Allocate(token);
+                if (i >= allocated_swapspace) {
+                    allocated_swapspace += g_swapfile_growth;
+                    die_unless(lseek(fd_, (allocated_swapspace * default_block_size)- 1, SEEK_SET) != -1);
+                    die_unless(write(fd_, "\0", 1) == 1); //expect 1byte written
+                }
+                off_t offset = i * default_block_size;
+                allocations[i] = mmap(addr_hint, default_block_size, protection_flags, flags, fd_, offset);
             }
             wall_time.Stop();
-            for (auto a : mmap_allocations)
-                pm.SwapOut(a);
+            for (auto a : allocations)
+                die_unless(munmap(a, default_block_size) == 0);
+            remove("/tmp/swapfile");
         }
         std::cout
             << "RESULT"
