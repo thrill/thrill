2174f394b41d04143ac8f007dbf74656743c84c9
Alexander Noe <aleexnoe@gmail.com>
2015-09-17 17:48:34 +0200

    change wordcount to use faststrings

--- benchmarks/word_count/word_count.cpp
+++ benchmarks/word_count/word_count.cpp
@@ -12,15 +12,19 @@
  ******************************************************************************/
 
 #include <thrill/api/dia.hpp>
+#include <thrill/api/read_lines.hpp>
+#include <thrill/api/reduce.hpp>
+#include <thrill/api/write_lines_many.hpp>
 #include <thrill/common/cmdline_parser.hpp>
+#include <thrill/common/fast_string.hpp>
 #include <thrill/common/logger.hpp>
-#include <thrill/thrill.hpp>
+
 
 #include <string>
 #include <utility>
 
-using WordCountPair = std::pair<std::string, size_t>;
 using namespace thrill; // NOLINT
+using WordCountPair = std::pair<common::FastString, size_t>;
 
 int main(int argc, char* argv[]) {
 
@@ -44,37 +48,33 @@ int main(int argc, char* argv[]) {
 
     auto start_func =
         [&input, &output](api::Context& ctx) {
-            auto input_dia = ReadLines(ctx, input);
+		auto input_dia = ReadLines(ctx, input);
 
-            std::string word;
-            word.reserve(24);
-            auto word_pairs = input_dia.template FlatMap<WordCountPair>(
-                [&word](const std::string& line, auto emit) -> void {
-                    /* map lambda: emit each word */
-                    word.clear();
-                    for (auto it = line.begin(); it != line.end(); it++) {
-                        if (*it == ' ') {
-                            emit(WordCountPair(word, 1));
-                            word.clear();
-                            word.reserve(40);
-                        }
-                        else {
-                            word.push_back(*it);
-                        }
-                    }
-                    emit(WordCountPair(word, 1));
-                    word.reserve(40);
-                }).ReducePair(
+		auto word_pairs = input_dia.template FlatMap<WordCountPair>(
+			[](const std::string& line, auto emit) -> void {
+				/* map lambda: emit each word */
+				auto last = line.begin();
+				for (auto it = line.begin(); it != line.end(); it++) {
+					if (*it == ' ') {
+						if (it > last) {
+							emit(WordCountPair(common::FastString::Ref(&(*last), it - last), 1));
+						}
+						last = it + 1;
+					}
+				}
+					if (line.end() > last) {
+						emit(WordCountPair(common::FastString::Ref(&(*last), line.end() - last), 1));
+					}
+			}).ReducePair(
                 [](const size_t& a, const size_t& b) {
-                    /* associative reduction operator: add counters */
-                    return a + b;
+				    return a + b;
                 });
 
-            word_pairs.Map(
-                [](const WordCountPair& wc) {
-                    return wc.first + ": " + std::to_string(wc.second);
-                }).WriteLinesMany(output);
-        };
+		word_pairs.Map(
+			[](const WordCountPair& wc) {
+				return wc.first.ToString() + ": " + std::to_string(wc.second);
+			}).WriteLinesMany(output);
+	};
 
     return api::Run(start_func);
 }
