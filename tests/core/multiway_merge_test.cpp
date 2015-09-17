37f700afe22525fc7ecf10f3c6a2b3847c7d1889
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-09-18 01:09:59 +0200

    add a puller to get the next element in multiway merge from the loser tree

--- tests/core/multiway_merge_test.cpp
+++ tests/core/multiway_merge_test.cpp
@@ -28,9 +28,13 @@
 
 using namespace thrill; // NOLINT
 
-static const bool debug = false;
 
-TEST(MultiwayMerge, Basic) {
+struct MultiwayMerge : public::testing::Test {
+    data::BlockPool block_pool_ { nullptr };
+};
+
+TEST_F(MultiwayMerge, Basic) {
+    // static const bool debug = false;
     std::mt19937 gen(0);
     std::size_t a = 2;
     std::size_t b = 5;
@@ -75,6 +79,7 @@ TEST(MultiwayMerge, Basic) {
 }
 
 TEST_F(MultiwayMerge, VectorWrapper) {
+    // static const bool debug = false;
     std::mt19937 gen(0);
     std::size_t a = 200;
     std::size_t b = 50;
@@ -119,7 +124,8 @@ TEST_F(MultiwayMerge, VectorWrapper) {
     }
 }
 
-TEST(MultiwayMerge, File_Wrapper_with_many_Runs) {
+TEST_F(MultiwayMerge, File_Wrapper_with_many_Runs) {
+    static const bool debug = false;
     std::mt19937 gen(0);
     // get _a_ different runs with _a_ number of elements
     std::size_t a = 400;
@@ -199,7 +205,8 @@ TEST(MultiwayMerge, File_Wrapper_with_many_Runs) {
     }
 }
 
-TEST(MultiwayMerge, File_Wrapper_with_1_Runs) {
+TEST_F(MultiwayMerge, File_Wrapper_with_1_Runs) {
+    static const bool debug = false;
     std::mt19937 gen(0);
     std::size_t a = 1;
     std::size_t b = 100;
@@ -263,6 +270,70 @@ TEST(MultiwayMerge, File_Wrapper_with_1_Runs) {
     auto r = output_file.GetReader(true);
     for (std::size_t i = 0; i < total; ++i) {
         auto e = r.Next<int>();
+        sLOG << std::setw(3) << ref[i] << std::setw(3) << e;
+        ASSERT_EQ(ref[i], e);
+    }
+}
+
+TEST_F(MultiwayMerge, GetMultiwayMergePuller) {
+    static const bool debug = false;
+    std::mt19937 gen(0);
+    std::size_t a = 4;
+    std::size_t b = 3;
+    std::size_t total = a * b;
+
+    using Iterator = thrill::core::FileIteratorWrapper<int>;
+    using File = data::File;
+    using Reader = File::Reader;
+    // using Writer = File::Writer;
+    std::vector<File> in;
+    std::vector<int> ref;
+    std::vector<int> output;
+    std::vector<std::pair<Iterator, Iterator> > seq;
+
+    in.reserve(a);
+    ref.reserve(total);
+    seq.reserve(a);
+    output.resize(total);
+
+    for (std::size_t i = 0; i < a; ++i) {
+        std::vector<int> tmp;
+        tmp.reserve(b);
+        for (std::size_t j = 0; j < b; ++j) {
+            auto elem = gen() % 100;
+            sLOG << "FILE" << i << "with elem" << elem;
+            tmp.push_back(elem);
+            ref.push_back(elem);
+        }
+        std::sort(std::begin(tmp), std::end(tmp));
+
+        data::File f(block_pool_);
+        {
+            auto w = f.GetWriter();
+            for (auto& t : tmp) {
+                w(t);
+            }
+        }
+        in.push_back(f);
+    }
+
+    for (std::size_t t = 0; t < in.size(); ++t) {
+        auto reader = std::make_shared<Reader>(in[t].GetReader(true));
+        Iterator s(&in[t], reader, 0, true);
+        Iterator e(&in[t], reader, in[t].num_items(), false);
+        seq.push_back(std::make_pair(s, e));
+    }
+
+    auto puller = core::get_sequential_file_multiway_merge_tree<true, false>(
+        std::begin(seq),
+        std::end(seq),
+        total,
+        std::less<int>());
+
+    std::sort(std::begin(ref), std::end(ref));
+
+    for (std::size_t i = 0; i < total; ++i) {
+        auto e = puller.Next();
         sLOG << std::setw(3) << ref[i] << std::setw(3) << e;
         ASSERT_EQ(ref[i], e);
     }
