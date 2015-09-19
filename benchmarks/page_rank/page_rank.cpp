c5af4ea4c76e8743ba39dda7602de869c01a9159
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-09-19 17:58:38 +0200

    add input and output param to pagerank

--- benchmarks/page_rank/page_rank.cpp
+++ benchmarks/page_rank/page_rank.cpp
@@ -35,6 +35,8 @@
 using thrill::DIARef;
 using thrill::Context;
 
+using namespace thrill; // NOLINT
+
 //! The PageRank user program
 void page_rank(Context& ctx) {
 
@@ -199,128 +201,145 @@ void page_rank_with_reduce_sort(Context& ctx) {
     WriteLines("pagerank.out");
 }
 
+int main(int argc, char* argv[]) {
+    common::CmdlineParser clp;
 
+    clp.SetVerboseProcess(false);
 
-//! The PageRank user program with group by
-void page_rank_with_groupbyindex(Context& ctx) {
-    thrill::common::StatsTimer<true> timer(false);
-    static const bool debug = true;
-    static const double s = 0.85;
-    static const double f = 0.15;
-    static const std::size_t iters = 10;
+    std::string input;
+    clp.AddParamString("input", input,
+                       "input file pattern");
 
-    using Key = std::size_t;
-    using Node = std::size_t;
-    using PageWithLinks = std::pair<std::size_t, std::vector<Node> >;
-    using PageWithRank = std::pair<std::size_t, double>;
-    using Link = std::pair<std::size_t, std::size_t>;
+    std::string output;
+    clp.AddParamString("output", output,
+                       "output file pattern");
 
-    ////////////////////////////////////////////////////////////////////////////
-    ////////////////////////// ALL KINDS OF LAMBDAS ////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-
-    auto create_links_fn = [](const std::string& input) {
-        auto split = thrill::common::split(input, " ");
-        // set node ids base to zero
-        return std::make_pair((std::size_t)(std::stoi(split[0]) - 1),
-            (std::size_t)(std::stoi(split[1]) - 1));
-    };
-
-    auto max_fn = [](const Link &in1, const Link &in2) {
-        return std::make_pair(((in1.first > in2.first) ? in1.first : in2.first), in2.second);
-    };
-
-    auto key_link_fn = [](Link p) { return p.first; };
-    auto key_page_with_ranks_fn = [](const PageWithRank& p) { return p.first; };
-
-    auto group_fn = [](auto& r, Key k) {
-        std::vector<Node> all;
-        while (r.HasNext()) {
-            all.push_back(r.Next().second);
-        }
-        return std::make_pair(k, all);
-    };
-
-    auto set_rank_fn = [](PageWithLinks p) {
-        return std::make_pair(p.first, 1.0);
-    };
-
-    auto compute_rank_contrib_fn = [](const PageWithLinks& l, const PageWithRank& r){
-        return std::make_pair(l.first, r.second/(l.second.size()));
-    };
-
-    //spark computation is:
-    // ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
-    auto update_rank_fn = [](const PageWithRank& p1, const PageWithRank& p2) {
-        return std::make_pair(p1.first, f + s * (p1.second + p2.second));
-    };
-
-    PageWithLinks neutral_page = std::make_pair(0, std::vector<Node>());
-
-
-    ////////////////////////////////////////////////////////////////////////////
-    //////////////////////// START OF COMPUTATION HERE /////////////////////////
-    ////////////////////////////////////////////////////////////////////////////
-
-    timer.Start();
-    // url linked_url
-    // url linked_url
-    // url linked_url
-    // ...
-    auto in = ReadLines(ctx, "../../../bench_in/eu-2005.in");
-    auto input = in.Map(create_links_fn);
-
-    //  URL   OUTGOING
-    // (url, [linked_url, linked_url, ...])
-    // (url, [linked_url, linked_url, ...])
-    // (url, [linked_url, linked_url, ...])
-    // ...
-    auto number_nodes = input.Sum(max_fn).first + 1;
-    auto links = input.GroupByIndex<PageWithLinks>(key_link_fn, group_fn, number_nodes).Keep();
-
-    // (url, rank)
-    // (url, rank)
-    // (url, rank)
-    // ...
-    auto ranks = links.Map(set_rank_fn).Cache();
-
-    // do iterations
-    for (size_t i = 1; i <= iters; ++i) {
-        // (linked_url, rank / OUTGOING.size)
-        // (linked_url, rank / OUTGOING.size)
-        // (linked_url, rank / OUTGOING.size)
-        // ...
-        auto contribs = links.Zip(ranks, compute_rank_contrib_fn);
-
-        // (url, rank)
-        // (url, rank)
-        // (url, rank)
-        // ...
-        ranks = contribs.ReduceToIndex(key_page_with_ranks_fn, update_rank_fn, number_nodes);
+    if (!clp.Process(argc, argv)) {
+        return -1;
     }
 
-    ranks.Map([](PageWithRank item) {
-        return std::to_string(item.first + 1)
-        + ": " + std::to_string(item.second);
-    }).
-    WriteLines("pagerank.out");
-    timer.Stop();
+    clp.PrintResult();
 
-    auto number_edges = in.Size();
-    LOG << "FINISHED PAGERANK COMPUTATION"
-        << "\n"
-        << std::left << std::setfill(' ')
-        << std::setw(10) << "#nodes: " << number_nodes
-        << "\n"
-        << std::setw(10) << "#edges: " << number_edges
-        << "\n"
-        << std::setw(10) << "#iter: " << iters
-        << "\n"
-        << std::setw(10) << "time: " << timer.Seconds();
-}
+    auto start_func =
+        [&input, &output](api::Context& ctx) {
+            thrill::common::StatsTimer<true> timer(false);
+            static const bool debug = true;
+            static const double s = 0.85;
+            static const double f = 0.15;
+            static const std::size_t iters = 10;
 
-int main(int, char**) {
-    return thrill::api::Run(page_rank_with_groupbyindex);
+            using Key = std::size_t;
+            using Node = std::size_t;
+            using PageWithLinks = std::pair<std::size_t, std::vector<Node> >;
+            using PageWithRank = std::pair<std::size_t, double>;
+            using Link = std::pair<std::size_t, std::size_t>;
+
+            ////////////////////////////////////////////////////////////////////////////
+            ////////////////////////// ALL KINDS OF LAMBDAS ////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+
+            auto create_links_fn = [](const std::string& input) {
+                auto split = thrill::common::split(input, " ");
+                // set node ids base to zero
+                return std::make_pair((std::size_t)(std::stoi(split[0]) - 1),
+                    (std::size_t)(std::stoi(split[1]) - 1));
+            };
+
+            auto max_fn = [](const Link &in1, const Link &in2) {
+                return std::make_pair(((in1.first > in2.first) ? in1.first : in2.first), in2.second);
+            };
+
+            auto key_link_fn = [](Link p) { return p.first; };
+            auto key_page_with_ranks_fn = [](const PageWithRank& p) { return p.first; };
+
+            auto group_fn = [](auto& r, Key k) {
+                std::vector<Node> all;
+                while (r.HasNext()) {
+                    all.push_back(r.Next().second);
+                }
+                return std::make_pair(k, all);
+            };
+
+            auto set_rank_fn = [](PageWithLinks p) {
+                return std::make_pair(p.first, 1.0);
+            };
+
+            auto compute_rank_contrib_fn = [](const PageWithLinks& l, const PageWithRank& r){
+                return std::make_pair(l.first, r.second/(l.second.size()));
+            };
+
+            //spark computation is:
+            // ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
+            auto update_rank_fn = [](const PageWithRank& p1, const PageWithRank& p2) {
+                return std::make_pair(p1.first, f + s * (p1.second + p2.second));
+            };
+
+            PageWithLinks neutral_page = std::make_pair(0, std::vector<Node>());
+
+
+            ////////////////////////////////////////////////////////////////////////////
+            //////////////////////// START OF COMPUTATION HERE /////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+
+            timer.Start();
+            // url linked_url
+            // url linked_url
+            // url linked_url
+            // ...
+            auto in = ReadLines(ctx, input);
+            auto input = in.Map(create_links_fn);
+
+            //  URL   OUTGOING
+            // (url, [linked_url, linked_url, ...])
+            // (url, [linked_url, linked_url, ...])
+            // (url, [linked_url, linked_url, ...])
+            // ...
+            auto number_nodes = input.Sum(max_fn).first + 1;
+            auto links = input.GroupByIndex<PageWithLinks>(key_link_fn, group_fn, number_nodes).Keep();
+
+            // (url, rank)
+            // (url, rank)
+            // (url, rank)
+            // ...
+            auto ranks = links.Map(set_rank_fn).Cache();
+
+            // do iterations
+            for (size_t i = 1; i <= iters; ++i) {
+                // (linked_url, rank / OUTGOING.size)
+                // (linked_url, rank / OUTGOING.size)
+                // (linked_url, rank / OUTGOING.size)
+                // ...
+                auto contribs = links.Zip(ranks, compute_rank_contrib_fn);
+
+                // (url, rank)
+                // (url, rank)
+                // (url, rank)
+                // ...
+                ranks = contribs.ReduceToIndex(key_page_with_ranks_fn, update_rank_fn, number_nodes);
+            }
+
+            ranks.Map([](PageWithRank item) {
+                return std::to_string(item.first + 1)
+                + ": " + std::to_string(item.second);
+            }).
+            WriteLines("pagerank.out");
+            timer.Stop();
+
+            auto number_edges = in.Size();
+            LOG << "\n"
+                << "FINISHED PAGERANK COMPUTATION"
+                << "\n"
+                << std::left << std::setfill(' ')
+                << std::setw(10) << "#nodes: " << number_nodes
+                << "\n"
+                << std::setw(10) << "#edges: " << number_edges
+                << "\n"
+                << std::setw(10) << "#iter: " << iters
+                << "\n"
+                << std::setw(10) << "time: " << timer.Milliseconds() << "ms";
+        };
+
+    return api::Run(start_func);
 }
 
 /******************************************************************************/
