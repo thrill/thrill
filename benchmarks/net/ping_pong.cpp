fb17cb9605556c7b7dadd0f99bbbe6c4ee8cf4ee
Timo Bingmann <tbgit@panthema.net>
2015-09-19 13:21:53 +0200

    Adding network bandwidth test to ping_pong.

--- benchmarks/net/ping_pong.cpp
+++ benchmarks/net/ping_pong.cpp
@@ -1,7 +1,9 @@
 /*******************************************************************************
  * benchmarks/net/ping_pong.cpp
  *
- * 1-factor ping pong latency benchmark
+ * Network backend benchmarks:
+ * - 1-factor ping pong latency benchmark
+ * - 1-factor full bandwidth test
  *
  * Part of Project Thrill.
  *
@@ -22,8 +24,8 @@
 
 std::string benchmark;
 
-unsigned int iterations = 1000;
-unsigned int repeats = 1;
+unsigned int outer_repeats = 1;
+unsigned int inner_repeats = 1;
 
 using namespace thrill;
 
@@ -35,49 +37,49 @@ void PingPongLatencyTest(api::Context& ctx) {
 
     net::Group& group = ctx.flow_control_channel().group();
 
-    //! a counter to count and match ping pongs.
-    size_t it = 0;
+    // a counter to count and match ping pongs.
+    size_t counter = 0;
 
-    for (size_t repeat = 0; repeat < repeats; ++repeat) {
+    for (size_t outer_repeat = 0;
+         outer_repeat < outer_repeats; ++outer_repeat) {
 
         common::StatsTimer<true> timer;
 
-        size_t p = ctx.num_hosts();
-
         timer.Start();
-        for (size_t i = 0; i < iterations; i++) {
+        for (size_t inner_repeat = 0;
+             inner_repeat < inner_repeats; inner_repeat++) {
             // perform 1-factor ping pongs (without barriers)
-            size_t me = ctx.host_rank();
-            for (size_t i = 0; i < p; ++i) {
+            for (size_t iter = 0; iter < ctx.num_hosts(); ++iter) {
 
-                size_t peer = group.OneFactorPeer(i);
+                size_t peer = group.OneFactorPeer(iter);
 
-                sLOG0 << "round i" << i << "me" << me << "peer" << peer;
+                sLOG0 << "round iter" << iter
+                      << "me" << ctx.host_rank() << "peer" << peer;
 
-                if (me < peer) {
+                if (ctx.host_rank() < peer) {
                     // send ping to peer
-                    size_t value = it++;
+                    size_t value = counter++;
                     group.SendTo(peer, value);
 
                     // wait for ping
                     group.ReceiveFrom(peer, &value);
-                    assert(value == it);
+                    assert(value == counter);
                 }
-                else if (me > peer) {
+                else if (ctx.host_rank() > peer) {
                     // wait for ping
                     size_t value;
                     group.ReceiveFrom(peer, &value);
-                    assert(value == it);
+                    assert(value == counter);
 
                     // increment counters
-                    it++, value++;
+                    counter++, value++;
 
                     // send pong
                     group.SendTo(peer, value);
                 }
                 else {
                     // not participating in this round
-                    ++it;
+                    ++counter;
                 }
             }
         }
@@ -89,12 +91,95 @@ void PingPongLatencyTest(api::Context& ctx) {
 
         if (ctx.my_rank() == 0) {
             LOG1 << "RESULT"
+                 << " benchmark=" << benchmark
                  << " hosts=" << ctx.num_hosts()
-                 << " repeat=" << repeat
-                 << " iterations=" << iterations
-                 << " ping_pongs=" << it
+                 << " outer_repeat=" << outer_repeat
+                 << " inner_repeats=" << inner_repeats
+                 << " ping_pongs=" << counter
                  << " time[us]=" << time
-                 << " time_per_ping_pong[us]=" << static_cast<double>(time) / it;
+                 << " time_per_ping_pong[us]=" << static_cast<double>(time) / counter;
+        }
+    }
+}
+
+//! perform a 1-factor bandwidth test
+void BandwidthTest(api::Context& ctx) {
+
+    // only work with first thread on this host.
+    if (ctx.local_worker_id() != 0) return;
+
+    net::Group& group = ctx.flow_control_channel().group();
+
+    // a counter to count and match messages
+    size_t counter = 0;
+
+    // data block to send or receive
+    static const size_t data_size = 1024 * 1024;
+    std::array<size_t, data_size / sizeof(size_t)> data_block;
+    static const size_t block_count = 64;
+
+    for (size_t outer_repeat = 0;
+         outer_repeat < outer_repeats; ++outer_repeat) {
+
+        common::StatsTimer<true> timer;
+
+        timer.Start();
+        for (size_t inner_repeat = 0;
+             inner_repeat < inner_repeats; inner_repeat++) {
+            // perform 1-factor ping pongs (without barriers)
+            for (size_t iter = 0; iter < ctx.num_hosts(); ++iter) {
+
+                size_t peer = group.OneFactorPeer(iter);
+
+                sLOG0 << "round iter" << iter
+                      << "me" << ctx.host_rank() << "peer" << peer;
+
+                if (ctx.host_rank() < peer) {
+                    // send blocks to peer
+                    for (size_t i = 0; i != block_count; ++i) {
+                        data_block[0] = counter++;
+                        group.SendTo(peer, data_block);
+                    }
+
+                    // wait for response pong
+                    size_t value;
+                    group.ReceiveFrom(peer, &value);
+                    assert(value == counter);
+                }
+                else if (ctx.host_rank() > peer) {
+                    // receive blocks from peer
+                    for (size_t i = 0; i != block_count; ++i) {
+                        group.ReceiveFrom(peer, &data_block);
+                        assert(data_block[0] == counter);
+
+                        // increment counters
+                        counter++;
+                    }
+
+                    // send ping
+                    group.SendTo(peer, counter);
+                }
+                else {
+                    // not participating in this round
+                    counter += block_count;
+                }
+            }
+        }
+        timer.Stop();
+
+        size_t time = timer.Microseconds();
+        // calculate maximum time.
+        group.AllReduce(time, common::maximum<size_t>());
+
+        if (ctx.my_rank() == 0) {
+            LOG1 << "RESULT"
+                 << " benchmark=" << benchmark
+                 << " hosts=" << ctx.num_hosts()
+                 << " outer_repeat=" << outer_repeat
+                 << " inner_repeats=" << inner_repeats
+                 << " ping_pongs=" << counter
+                 << " time[us]=" << time
+                 << " time_per_ping_pong[us]=" << static_cast<double>(time) / counter;
         }
     }
 }
@@ -103,6 +188,9 @@ void PingPongLatencyTest(api::Context& ctx) {
 void net_test(api::Context& ctx) {
     if (benchmark == "pingpong") {
         PingPongLatencyTest(ctx);
+    }
+    else if (benchmark == "bandwidth") {
+        BandwidthTest(ctx);
     }
     else {
         die("Unknown benchmark " + benchmark);
@@ -115,15 +203,16 @@ int main(int argc, char** argv) {
 
     clp.SetVerboseProcess(false);
 
-    clp.AddUInt('i', "iterations", iterations,
-                "Count of iterations");
+    clp.AddUInt('r', "inner_repeats", inner_repeats,
+                "Repeat inner experiment a number of times.");
 
-    clp.AddUInt('r', "repeats", repeats,
-                "Repeat experiment a number of times.");
+    clp.AddUInt('R', "outer_repeats", outer_repeats,
+                "Repeat whole experiment a number of times.");
 
     clp.AddParamString("benchmark", benchmark,
                        "name of benchmark to run:\n"
-                       "  pingpong - latency test");
+                       "  pingpong - 1-factor latency\n"
+                       "  bandwidth - 1-factor full bandwidth");
 
     if (!clp.Process(argc, argv)) {
         return -1;
