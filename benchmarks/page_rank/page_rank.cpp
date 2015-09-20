a4ea673c4400a1de5534e1f1309d738ef1517ff0
Huyen Chau Nguyen <hello@chau-nguyen.de>
2015-09-20 18:10:19 +0200

    compute new rank correctly

--- benchmarks/page_rank/page_rank.cpp
+++ benchmarks/page_rank/page_rank.cpp
@@ -37,169 +37,169 @@ using thrill::Context;
 
 using namespace thrill; // NOLINT
 
-//! The PageRank user program
-void page_rank(Context& ctx) {
+// //! The PageRank user program
+// void page_rank(Context& ctx) {
 
-    static const double s = 0.85;
+//     static const double s = 0.85;
 
-    using PageWithLinks = std::pair<size_t, std::vector<int> >;
-    using PageWithRank = std::pair<size_t, double>;
-    using Page = std::tuple<size_t, double, std::vector<int> >;
+//     using PageWithLinks = std::pair<size_t, std::vector<int> >;
+//     using PageWithRank = std::pair<size_t, double>;
+//     using Page = std::tuple<size_t, double, std::vector<int> >;
 
-    DIARef<PageWithRank> ranks =
-        ReadLines(ctx, "pagerank.in")
-        .Map([](const std::string& input) {
-                 auto splitted = thrill::common::split(input, " ");
-                 return std::make_pair((size_t)std::stoi(splitted[0]), 1.0);
-             }).Cache();
+//     DIARef<PageWithRank> ranks =
+//         ReadLines(ctx, "pagerank.in")
+//         .Map([](const std::string& input) {
+//                  auto splitted = thrill::common::split(input, " ");
+//                  return std::make_pair((size_t)std::stoi(splitted[0]), 1.0);
+//              }).Cache();
 
-    size_t size = ranks.Size();
+//     size_t size = ranks.Size();
 
-    auto links = ReadLines(ctx, "pagerank.in")
-                 .Map([](const std::string& line) {
-                          auto splitted = thrill::common::split(line, " ");
-                          std::vector<int> links;
-                          links.reserve(splitted.size() - 1);
-                          for (size_t i = 1; i < splitted.size(); i++) {
-                              links.push_back(std::stoi(splitted[i]));
-                          }
-                          return std::make_pair(std::stoi(splitted[0]), links);
-                      });
+//     auto links = ReadLines(ctx, "pagerank.in")
+//                  .Map([](const std::string& line) {
+//                           auto splitted = thrill::common::split(line, " ");
+//                           std::vector<int> links;
+//                           links.reserve(splitted.size() - 1);
+//                           for (size_t i = 1; i < splitted.size(); i++) {
+//                               links.push_back(std::stoi(splitted[i]));
+//                           }
+//                           return std::make_pair(std::stoi(splitted[0]), links);
+//                       });
 
-    for (size_t i = 1; i <= 10; ++i) {
-        std::cout << "Iteration: " << i << std::endl;
+//     for (size_t i = 1; i <= 10; ++i) {
+//         std::cout << "Iteration: " << i << std::endl;
 
-        auto pages =
-            links
-            .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
-                     return std::make_tuple(first.first, second.second, first.second);
-                 });
+//         auto pages =
+//             links
+//             .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
+//                      return std::make_tuple(first.first, second.second, first.second);
+//                  });
 
-        auto contribs =
-            pages
-            .FlatMap<PageWithRank>(
-                [](Page page, auto emit) {
-                    std::vector<int> urls = std::get<2>(page);
-                    double rank = std::get<1>(page);
-                    double num_urls = static_cast<double>(urls.size());
-                    for (int url : urls) {
-                        emit(std::make_pair(url, rank / num_urls));
-                    }
-                });
+//         auto contribs =
+//             pages
+//             .FlatMap<PageWithRank>(
+//                 [](Page page, auto emit) {
+//                     std::vector<int> urls = std::get<2>(page);
+//                     double rank = std::get<1>(page);
+//                     double num_urls = static_cast<double>(urls.size());
+//                     for (int url : urls) {
+//                         emit(std::make_pair(url, rank / num_urls));
+//                     }
+//                 });
 
-        ranks =
-            contribs
-            .ReducePairToIndex(
-                [](double rank1, double rank2) {
-                    return rank1 + rank2;
-                },
-                size)
-            .Map([](PageWithRank page) {
-                     return std::make_pair(page.first, (1 - s) + page.second * s);
-                 })
-            .Cache();
-    }
+//         ranks =
+//             contribs
+//             .ReducePairToIndex(
+//                 [](double rank1, double rank2) {
+//                     return rank1 + rank2;
+//                 },
+//                 size)
+//             .Map([](PageWithRank page) {
+//                      return std::make_pair(page.first, (1 - s) + page.second * s);
+//                  })
+//             .Cache();
+//     }
 
-    ranks.Map([](PageWithRank item) {
-                  return std::to_string(item.first)
-                  + ": " + std::to_string(item.second);
-              }).
-    WriteLines("pagerank.out");
-}
+//     ranks.Map([](PageWithRank item) {
+//                   return std::to_string(item.first)
+//                   + ": " + std::to_string(item.second);
+//               }).
+//     WriteLines("pagerank.out");
+// }
 
-//! The PageRank user program with group by
-void page_rank_with_reduce_sort(Context& ctx) {
+// //! The PageRank user program with group by
+// void page_rank_with_reduce_sort(Context& ctx) {
 
-    static const double s = 0.85;
+//     static const double s = 0.85;
 
-    using PageWithLinks = std::pair<size_t, std::vector<int> >;
-    using PageWithRank = std::pair<size_t, double>;
-    using Page = std::tuple<size_t, double, std::vector<int> >;
+//     using PageWithLinks = std::pair<size_t, std::vector<int> >;
+//     using PageWithRank = std::pair<size_t, double>;
+//     using Page = std::tuple<size_t, double, std::vector<int> >;
 
-    // url linked_url
-    // url linked_url
-    // url linked_url
-    // ...
-    auto in = ReadLines(ctx, "pagerank_2.in");
+//     // url linked_url
+//     // url linked_url
+//     // url linked_url
+//     // ...
+//     auto in = ReadLines(ctx, "pagerank_2.in");
 
-    auto key_fn = [](const std::pair<int, std::vector<int> >& p) { return p.first; };
+//     auto key_fn = [](const std::pair<int, std::vector<int> >& p) { return p.first; };
 
-    auto red_fn = [](const std::pair<int, std::vector<int> >& in1, const std::pair<int, std::vector<int> >& in2) {
-                      std::vector<int> v;
-                      v.reserve(in1.second.size() + in2.second.size());
-                      v.insert(v.end(), in1.second.begin(), in1.second.end());
-                      v.insert(v.end(), in2.second.begin(), in2.second.end());
-                      return std::make_pair(in1.first, v);
-                  };
+//     auto red_fn = [](const std::pair<int, std::vector<int> >& in1, const std::pair<int, std::vector<int> >& in2) {
+//                       std::vector<int> v;
+//                       v.reserve(in1.second.size() + in2.second.size());
+//                       v.insert(v.end(), in1.second.begin(), in1.second.end());
+//                       v.insert(v.end(), in2.second.begin(), in2.second.end());
+//                       return std::make_pair(in1.first, v);
+//                   };
 
-    //  URL   OUTGOING
-    // (url, [linked_url, linked_url, ...])
-    // (url, [linked_url, linked_url, ...])
-    // (url, [linked_url, linked_url, ...])
-    // ...
-    auto links = in.Map(
-        [](const std::string& input) {
-            auto split = thrill::common::split(input, " ");
-            return std::make_pair<int, std::vector<int> >(std::stoi(split[0]), std::vector<int>(1, std::stoi(split[1])));
-        }).ReduceByKey(key_fn, red_fn);
+//     //  URL   OUTGOING
+//     // (url, [linked_url, linked_url, ...])
+//     // (url, [linked_url, linked_url, ...])
+//     // (url, [linked_url, linked_url, ...])
+//     // ...
+//     auto links = in.Map(
+//         [](const std::string& input) {
+//             auto split = thrill::common::split(input, " ");
+//             return std::make_pair<int, std::vector<int> >(std::stoi(split[0]), std::vector<int>(1, std::stoi(split[1])));
+//         }).ReduceByKey(key_fn, red_fn);
 
-    auto compare_fn = [](const std::pair<int, std::vector<int> >& in1, const std::pair<int, std::vector<int> >& in2) {
-                          return in1.first < in2.first;
-                      };
+//     auto compare_fn = [](const std::pair<int, std::vector<int> >& in1, const std::pair<int, std::vector<int> >& in2) {
+//                           return in1.first < in2.first;
+//                       };
 
-    auto links_sorted = links.Sort(compare_fn).Keep();
+//     auto links_sorted = links.Sort(compare_fn).Keep();
 
-    // (url, rank)
-    // (url, rank)
-    // (url, rank)
-    // ...
-    DIARef<PageWithRank> ranks = links_sorted.Map(
-        [](const std::pair<int, std::vector<int> >& l) {
-            return std::make_pair((size_t)l.first, 1.0);
-        }).Cache();
+//     // (url, rank)
+//     // (url, rank)
+//     // (url, rank)
+//     // ...
+//     DIARef<PageWithRank> ranks = links_sorted.Map(
+//         [](const std::pair<int, std::vector<int> >& l) {
+//             return std::make_pair((size_t)l.first, 1.0);
+//         }).Cache();
 
-    size_t size = ranks.Size();
+//     size_t size = ranks.Size();
 
-    for (size_t i = 1; i <= 10; ++i) {
-        std::cout << "Iteration: " << i << std::endl;
+//     for (size_t i = 1; i <= 10; ++i) {
+//         std::cout << "Iteration: " << i << std::endl;
 
-        auto pages =
-            links_sorted
-            .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
-                     return std::make_tuple(first.first, second.second, first.second);
-                 });
+//         auto pages =
+//             links_sorted
+//             .Zip(ranks, [](PageWithLinks first, PageWithRank second) {
+//                      return std::make_tuple(first.first, second.second, first.second);
+//                  });
 
-        auto contribs =
-            pages
-            .FlatMap<PageWithRank>(
-                [](Page page, auto emit) {
-                    std::vector<int> urls = std::get<2>(page);
-                    double rank = std::get<1>(page);
-                    double num_urls = static_cast<double>(urls.size());
-                    for (int url : urls) {
-                        emit(std::make_pair(url, rank / num_urls));
-                    }
-                });
+//         auto contribs =
+//             pages
+//             .FlatMap<PageWithRank>(
+//                 [](Page page, auto emit) {
+//                     std::vector<int> urls = std::get<2>(page);
+//                     double rank = std::get<1>(page);
+//                     double num_urls = static_cast<double>(urls.size());
+//                     for (int url : urls) {
+//                         emit(std::make_pair(url, rank / num_urls));
+//                     }
+//                 });
 
-        ranks =
-            contribs
-            .ReducePairToIndex(
-                [](double rank1, double rank2) {
-                    return rank1 + rank2;
-                },
-                size)
-            .Map([](PageWithRank page) {
-                     return std::make_pair(page.first, (1 - s) + page.second * s);
-                 })
-            .Cache();
-    }
+//         ranks =
+//             contribs
+//             .ReducePairToIndex(
+//                 [](double rank1, double rank2) {
+//                     return rank1 + rank2;
+//                 },
+//                 size)
+//             .Map([](PageWithRank page) {
+//                      return std::make_pair(page.first, (1 - s) + page.second * s);
+//                  })
+//             .Cache();
+//     }
 
-    ranks.Map([](PageWithRank item) {
-                  return std::to_string(item.first)
-                  + ": " + std::to_string(item.second);
-              }).
-    WriteLines("pagerank.out");
-}
+//     ranks.Map([](PageWithRank item) {
+//                   return std::to_string(item.first)
+//                   + ": " + std::to_string(item.second);
+//               }).
+//     WriteLines("pagerank.out");
+// }
 
 int main(int argc, char* argv[]) {
     common::CmdlineParser clp;
@@ -237,75 +237,81 @@ int main(int argc, char* argv[]) {
             using Page_Link = std::pair<std::size_t, std::size_t>;
             using Outgoings_Rank = std::pair<std::vector<Node>, double>;
 
-            ////////////////////////////////////////////////////////////////////////////
-            ////////////////////////// ALL KINDS OF LAMBDAS ////////////////////////////
-            ////////////////////////////////////////////////////////////////////////////
-
-            auto create_links_fn = [](const std::string& input) {
-                auto split = thrill::common::split(input, " ");
-                // set node ids base to zero
-                // LOG << (std::size_t)(std::stoi(split[0]) - 1);
-                // LOG << (std::size_t)(std::stoi(split[1]) - 1);
-                return std::make_pair((std::size_t)(std::stoi(split[0]) - 1),
-                    (std::size_t)(std::stoi(split[1]) - 1));
-            };
-
-            auto max_fn = [](const Page_Link &in1, const Page_Link &in2) {
-                Node first = std::max(in1.first, in2.first);
-                Node second = std::max(in1.second, in2.second);
-                return std::make_pair(std::max(first, second), first);
-            };
-
-            auto key_link_fn = [](Page_Link p) { return p.first; };
-            auto key_page_with_ranks_fn = [](const Page_Rank& p) { return p.first; };
-
-            auto group_fn = [](auto& r, Key k) {
-                // LOG << k << " has outgoings to";
-                std::vector<Node> all;
-                while (r.HasNext()) {
-                    // auto out = r.Next().second;
-                    // LOG << out;
-                    all.push_back(r.Next().second);
-                }
-                return std::make_pair(k, all);
-            };
-
-            auto set_rank_fn = [](Page_Outgoings p) {
-                return std::make_pair(p.first, 1.0);
-            };
-
-            Page_Outgoings neutral_page = std::make_pair(0, std::vector<Node>());
-
 
             ////////////////////////////////////////////////////////////////////////////
             //////////////////////// START OF COMPUTATION HERE /////////////////////////
             ////////////////////////////////////////////////////////////////////////////
 
             timer.Start();
+            // read input file and create links in this format:
+            //
             // url linked_url
             // url linked_url
             // url linked_url
             // ...
+            auto create_links_fn = [](const std::string& input) {
+                auto split = thrill::common::split(input, " ");
+                // set base of page_id to 0
+                return std::make_pair((std::size_t)(std::stoi(split[0]) - 1),
+                    (std::size_t)(std::stoi(split[1]) - 1));
+            };
+
             auto in = ReadLines(ctx, input);
             auto input = in.Map(create_links_fn);
 
+            // aggregate all outgoing links of a page in this format:
+            //
             //  URL   OUTGOING
             // (url, [linked_url, linked_url, ...])
             // (url, [linked_url, linked_url, ...])
             // (url, [linked_url, linked_url, ...])
             // ...
+
+            // get number of nodes by finding max page_id
+            auto max_fn = [](const Page_Link &in1, const Page_Link &in2) {
+                Node first = std::max(in1.first, in2.first);
+                Node second = std::max(in1.second, in2.second);
+                return std::make_pair(std::max(first, second), first);
+            };
+            // group outgoing links
+            auto group_fn = [](auto& r, Key k) {
+                std::vector<Node> all;
+                while (r.HasNext()) {
+                    all.push_back(r.Next().second);
+                }
+                return std::make_pair(k, all);
+            };
+            auto key_link_fn = [](Page_Link p) { return p.first; };
+            // add 1 to max node_id to get number of nodes because of node_id 0
             const auto number_nodes = input.Sum(max_fn).first + 1;
             auto links = input.GroupByIndex<Page_Outgoings>(key_link_fn, group_fn, number_nodes).Keep();
 
+            // initialize all ranks to 1.0
+            //
             // (url, rank)
             // (url, rank)
             // (url, rank)
             // ...
+            auto set_rank_fn = [](Page_Outgoings p) {
+                return std::make_pair(p.first, 1.0);
+            };
+
             auto ranks = links.Map(set_rank_fn).Cache();
             // do iterations
             for (int i = 1; i <= iter; ++i) {
                 LOG << "iteration " << i;
 
+                // for all outgoing link, get their rank contribution from all
+                // links by doing:
+                //
+                // 1) group all outgoing links with rank of its parent page: (Zip)
+                //
+                // ([linked_url, linked_url, ...], rank_parent)
+                // ([linked_url, linked_url, ...], rank_parent)
+                // ([linked_url, linked_url, ...], rank_parent)
+                //
+                // 2) compute rank contribution for each linked_url: (FlatMap)
+                //
                 // (linked_url, rank / OUTGOING.size)
                 // (linked_url, rank / OUTGOING.size)
                 // (linked_url, rank / OUTGOING.size)
@@ -326,21 +332,26 @@ int main(int argc, char* argv[]) {
 
                 auto contribs = links.Zip(ranks, merge_outgoings_w_rank_fn).FlatMap<Page_Rank>(compute_rank_contrib_fn);
 
+                // reduce all rank contributions by adding all rank contributions
+                // and compute the new rank with 0.15 * 0.85 * sum_rank_contribs
+                //
                 // (url, rank)
                 // (url, rank)
                 // (url, rank)
                 // ...
 
-                //spark computation is:
-                // ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
-                auto update_rank_fn = [](const Page_Rank& p1, const Page_Rank& p2) {
+                auto sum_rank_contrib_fn = [](const Page_Rank& p1, const Page_Rank& p2) {
                     assert(p1.first == p2.first);
-                    // assert(f + s * (p1.second + p2.second) <= 1);
-                    return std::make_pair(p1.first, f + s * (p1.second + p2.second));
+                    return std::make_pair(p1.first, p1.second + p2.second);
                 };
-                ranks = contribs.ReduceToIndex(key_page_with_ranks_fn, update_rank_fn, number_nodes).Cache();
+                auto update_rank_fn = [](const Page_Rank& p) {
+                    return std::make_pair(p.first, f + s * p.second);
+                };
+                auto key_page_with_ranks_fn = [](const Page_Rank& p) { return p.first; };
+                ranks = contribs.ReduceToIndex(key_page_with_ranks_fn, sum_rank_contrib_fn, number_nodes).Map(update_rank_fn).Cache();
             }
 
+            // write result to line. add 1 to node_ids to revert back to normal
             auto res = ranks.Map([](Page_Rank item) {
                 if (item.first == 0 && item.second == 0.0) {
                     return std::string("");
