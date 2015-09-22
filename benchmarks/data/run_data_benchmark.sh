c3b0f519bf39ab3c5efda4c356ff9ceada87ebda
Tobias Sturm <mail@tobiassturm.de>
2015-09-22 17:48:36 +0200

    multiple threads for block_queue experiment

--- benchmarks/data/run_data_benchmark.sh
+++ benchmarks/data/run_data_benchmark.sh
@@ -4,19 +4,38 @@
 githash=$(eval "git rev-parse --short HEAD")
 timestamp=$(date)
 echo ${timestamp}
-for benchmark in file block_queue
+for benchmark in block_queue
 do
-  for type in size_t string pair triple
+for type in size_t string pair triple
+do
+  for reader in consume non-consume
   do
-    for reader in consume non-consume
+    for size in 100K 1M 100M 2G
     do
-      for size in 100K 1M 10M 100M
+      for block_size in 1K 1024K 2048K 4096K
       do
-        for block_size in 64K 265K 512K 2048K 4096K
+        for threads in 1 2 4
         do
-          eval ./data_io_benches -l 1 -u 100 -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
-          eval ./data_io_benches -l 1K -u 10K -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
+          eval ./data_io_benches -t ${threads} -l 1 -u 100 -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
+          eval ./data_io_benches -t ${threads} -l 1K -u 10K -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
         done
+      done
+    done
+  done
+done
+done
+
+for benchmark in file
+for type in size_t string pair triple
+do
+  for reader in consume non-consume
+  do
+    for size in 100K 1M 100M 2G
+    do
+      for block_size in 1K 1024K 2048K 4096K
+      do
+        eval ./data_io_benches -l 1 -u 100 -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
+        eval ./data_io_benches -l 1K -u 10K -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
       done
     done
   done
