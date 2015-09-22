8013efbc8e7bea0b603e5451e6cd05fdbd603bf6
Tobias Sturm <mail@tobiassturm.de>
2015-09-22 08:41:28 +0200

    convert lower & upper bound for variable-length elements to parameter

--- benchmarks/data/io_benches.cpp
+++ benchmarks/data/io_benches.cpp
@@ -28,9 +28,9 @@ using common::StatsTimer;
 //! Number of elements depends on the number of bytes.
 //! one RESULT line will be printed for each iteration
 //! All iterations use the same generated data.
-//! Variable-length elements range between 1 and 100 bytes
+//! Variable-length elements range between 1 and 100 bytes per default
 template <typename Type>
-void ConductExperiment(uint64_t bytes, unsigned iterations, api::Context& ctx, const std::string& type_as_string, const std::string& reader_type) {
+void ConductExperiment(uint64_t bytes, size_t min_size, size_t max_size, unsigned iterations, api::Context& ctx, const std::string& type_as_string, const std::string& reader_type) {
 
     if (reader_type != "consume" && reader_type != "non-consume")
         abort();
@@ -38,7 +38,7 @@ void ConductExperiment(uint64_t bytes, unsigned iterations, api::Context& ctx, c
     for (unsigned i = 0; i < iterations; i++) {
         auto file = ctx.GetFile();
         auto writer = file.GetWriter();
-        auto data = Generator<Type>(bytes);
+        auto data = Generator<Type>(bytes, min_size, max_size);
 
         std::cout << "writing " << bytes << " bytes" << std::endl;
         StatsTimer<true> write_timer(true);
@@ -58,6 +58,7 @@ void ConductExperiment(uint64_t bytes, unsigned iterations, api::Context& ctx, c
         std::cout << "RESULT"
                   << " datatype=" << type_as_string
                   << " size=" << bytes
+                  << " avg_element_size=" << (min_size + max_size) / 2.0
                   << " reader=" << reader_type
                   << " write_time=" << write_timer.Microseconds()
                   << " read_time=" << read_timer.Microseconds()
@@ -73,9 +74,13 @@ int main(int argc, const char** argv) {
     clp.SetAuthor("Tobias Sturm <mail@tobiassturm.de>");
     unsigned iterations = 1;
     uint64_t bytes = 1024;
+    size_t min_variable_length = 1;
+    size_t max_variable_length = 100;
     std::string type;
     std::string reader_type;
     clp.AddBytes('b', "bytes", bytes, "number of bytes to process (default 1024)");
+    clp.AddBytes('l', "lower", min_variable_length, "lower bound for variable element length (default 1)");
+    clp.AddBytes('u', "upper", max_variable_length, "upper bound for variable element length (default 100)");
     clp.AddUInt('n', "iterations", iterations, "Iterations (default: 1)");
     clp.AddParamString("type", type,
                        "data type (size_t, string, pair, triple)");
@@ -87,13 +92,13 @@ int main(int argc, const char** argv) {
     using triple = std::tuple<std::string, size_t, std::string>;
 
     if (type == "size_t")
-        api::RunLocalSameThread(std::bind(ConductExperiment<size_t>, bytes, iterations, std::placeholders::_1, type, reader_type));
+        api::RunLocalSameThread(std::bind(ConductExperiment<size_t>, bytes, min_variable_length, max_variable_length, iterations, std::placeholders::_1, type, reader_type));
     else if (type == "string")
-        api::RunLocalSameThread(std::bind(ConductExperiment<std::string>, bytes, iterations, std::placeholders::_1, type, reader_type));
+        api::RunLocalSameThread(std::bind(ConductExperiment<std::string>, bytes, min_variable_length, max_variable_length, iterations, std::placeholders::_1, type, reader_type));
     else if (type == "pair")
-        api::RunLocalSameThread(std::bind(ConductExperiment<pair>, bytes, iterations, std::placeholders::_1, type, reader_type));
+        api::RunLocalSameThread(std::bind(ConductExperiment<pair>, bytes, min_variable_length, max_variable_length, iterations, std::placeholders::_1, type, reader_type));
     else if (type == "triple")
-        api::RunLocalSameThread(std::bind(ConductExperiment<triple>, bytes, iterations, std::placeholders::_1, type, reader_type));
+        api::RunLocalSameThread(std::bind(ConductExperiment<triple>, bytes, min_variable_length, max_variable_length, iterations, std::placeholders::_1, type, reader_type));
     else
         abort();
 }
