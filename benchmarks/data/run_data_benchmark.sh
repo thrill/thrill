91a9d9f4280995c7ebee58df616704da17f8496e
Tobias Sturm <mail@tobiassturm.de>
2015-09-22 17:25:48 +0200

    update benchmark run script for io_benches

--- benchmarks/data/run_data_benchmark.sh
+++ benchmarks/data/run_data_benchmark.sh
@@ -10,9 +10,9 @@ do
   do
     for reader in consume non-consume
     do
-      for size in 100K 1M 100M 2G
+      for size in 100K 1M 10M 100M
       do
-        for block_size in 1K 1024K 2048K 4096K
+        for block_size in 64K 265K 512K 2048K 4096K
         do
           eval ./data_io_benches -l 1 -u 100 -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
           eval ./data_io_benches -l 1K -u 10K -s ${block_size} -b ${size} ${benchmark} ${type} ${reader} | grep "RESULT" |  sed -e "s/$/ version=${githash} timestamp=${timestamp} /"
