62a415ad0433396cfc03943a6fb124be8c7425ac
Timo Bingmann <tbgit@panthema.net>
2015-09-26 12:32:57 +0200

    Including .pm files in analyze-source scan.

--- tests/net/test_gen.pm
+++ tests/net/test_gen.pm
@@ -1,7 +1,11 @@
 ################################################################################
-# misc/analyze-source.pl
+# tests/net/test_gen.pm
+#
+# Part of Project Thrill.
 #
 # Copyright (C) 2015 Timo Bingmann <tb@panthema.net>
+#
+# All rights reserved. Published under the BSD-2 license in the LICENSE file.
 ################################################################################
 
 sub generate_group_tests {
