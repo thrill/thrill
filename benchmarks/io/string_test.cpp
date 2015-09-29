dc2ab9647e20a32ef6dedb8e85627070f2d58406
Alexander Noe <aleexnoe@gmail.com>
2015-09-29 09:56:02 +0200

    Uncrustify

--- benchmarks/io/string_test.cpp
+++ benchmarks/io/string_test.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
     clp.AddParamString("input", input,
                        "input file pattern");
 
-	int iterations;
+    int iterations;
     clp.AddParamInt("n", iterations, "Iterations");
 
     if (!clp.Process(argc, argv)) {
@@ -42,36 +42,35 @@ int main(int argc, char* argv[]) {
 
     clp.PrintResult();
 
-	for (int i = 0; i < iterations; i++) {
+    for (int i = 0; i < iterations; i++) {
 
-		api::Run([&input](api::Context& ctx) {
-				auto input_dia = ReadLines(ctx, input);
+        api::Run([&input](api::Context& ctx) {
+                     auto input_dia = ReadLines(ctx, input);
 
-				common::StatsTimer<true> timer(true);
-				std::string str;
+                     common::StatsTimer<true> timer(true);
+                     std::string str;
 
-				LOG1 << input_dia.template FlatMap<std::string>(
-					[&str](const std::string& line, auto emit) -> void {
-						/* map lambda: emit each word */
-						auto last = line.begin();
-						for (auto it = line.begin(); it != line.end(); it++) {
-							if (*it == ' ') {
-								if (it > last) {
-									emit(str.assign(last, it));
-								}
-								last = it + 1;
-							}
-						}
-						if (line.end() > last) {
-							emit(str.assign(last, line.end()));
-						}
-					}).Size();
-			
-				timer.Stop();
-				std::cout << "RESULT" << " time=" << timer.Milliseconds() << std::endl;
-			
-			});
-	}
+                     LOG1 << input_dia.template FlatMap<std::string>(
+                         [&str](const std::string& line, auto emit) -> void {
+                             /* map lambda: emit each word */
+                             auto last = line.begin();
+                             for (auto it = line.begin(); it != line.end(); it++) {
+                                 if (*it == ' ') {
+                                     if (it > last) {
+                                         emit(str.assign(last, it));
+                                     }
+                                     last = it + 1;
+                                 }
+                             }
+                             if (line.end() > last) {
+                                 emit(str.assign(last, line.end()));
+                             }
+                         }).Size();
+
+                     timer.Stop();
+                     std::cout << "RESULT" << " time=" << timer.Milliseconds() << std::endl;
+                 });
+    }
 }
 
 /******************************************************************************/
