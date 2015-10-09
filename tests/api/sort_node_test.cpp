95a3f3f8e2eb5f552e74b8e2aef026859c8e5236
Alexander Noe <aleexnoe@gmail.com>
2015-10-09 11:06:35 +0200

    fix worst case mitigation in sort

--- tests/api/sort_node_test.cpp
+++ tests/api/sort_node_test.cpp
@@ -160,4 +160,36 @@ TEST(Sort, SortRandomIntIntStructs) {
     api::RunLocalTests(start_func);
 }
 
+TEST(Sort, SortZeros) {
+
+    std::function<void(Context&)> start_func =
+        [](Context& ctx) {
+
+
+            std::default_random_engine generator(std::random_device { } ());
+            std::uniform_int_distribution<int> distribution(1, 10);
+
+            auto integers = Generate(
+                ctx,
+                [](const size_t&) -> size_t {
+                    return 0;
+                },
+                10000);
+
+            auto sorted = integers.Sort();
+
+            std::vector<size_t> out_vec;
+
+            sorted.AllGather(&out_vec);
+
+			/* for (size_t i = 0; i < out_vec.size() - 1; i++) {
+                ASSERT_FALSE(out_vec[i + 1].first < out_vec[i].first);
+				}*/
+
+            ASSERT_EQ(10000u, out_vec.size());
+        };
+
+    api::RunLocalTests(start_func);
+}
+
 /******************************************************************************/
