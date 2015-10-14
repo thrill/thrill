59fc497d80f600e678a9c852581133ac9e6ee11c
Timo Bingmann <tbgit@panthema.net>
2015-10-14 18:07:22 +0200

    Fixing ZipPadding: use std::tuple<> with remove_cv and remove_reference applied.

--- thrill/common/function_traits.hpp
+++ thrill/common/function_traits.hpp
@@ -41,6 +41,11 @@ struct FunctionTraits<ReturnType (ClassType::*)(Args ...) const>{
     //! the tuple of arguments
     using args = std::tuple<Args ...>;
 
+    //! the tuple of arguments: with remove_cv and remove_reference applied.
+    using args_plain = std::tuple<
+              typename std::remove_cv<
+                  typename std::remove_reference<Args>::type>::type ...>;
+
     //! the i-th argument is equivalent to the i-th tuple element of a tuple
     //! composed of those arguments.
     template <size_t i>
@@ -52,9 +57,7 @@ struct FunctionTraits<ReturnType (ClassType::*)(Args ...) const>{
     using arg_plain =
               typename std::remove_cv<
                   typename std::remove_reference<
-                      arg<i>
-                      >::type
-                  >::type;
+                      arg<i> >::type>::type;
 };
 
 //! specialize for pointers to mutable member function
@@ -78,6 +81,11 @@ struct FunctionTraits<ReturnType (*)(Args ...)>{
     //! the tuple of arguments
     using args = std::tuple<Args ...>;
 
+    //! the tuple of arguments: with remove_cv and remove_reference applied.
+    using args_plain = std::tuple<
+              typename std::remove_cv<
+                  typename std::remove_reference<Args>::type>::type ...>;
+
     //! the i-th argument is equivalent to the i-th tuple element of a tuple
     //! composed of those arguments.
     template <size_t i>
@@ -89,9 +97,7 @@ struct FunctionTraits<ReturnType (*)(Args ...)>{
     using arg_plain =
               typename std::remove_cv<
                   typename std::remove_reference<
-                      arg<i>
-                      >::type
-                  >::type;
+                      arg<i> >::type>::type;
 };
 
 } // namespace common
