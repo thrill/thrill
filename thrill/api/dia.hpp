8fc9d33c087b9a7e95f8db2f250429e5d28886e4
Emanuel Jöbstl <emiswelt@gmail.com>
2015-10-18 00:42:20 +0100

    Added comments. Code cleanup.

--- thrill/api/dia.hpp
+++ thrill/api/dia.hpp
@@ -603,10 +603,21 @@ public:
     // duplicate.
 
     /*!
-     * TODO
+     * Merge is a DOp, which merges two sorted DIAs to a single sorted DIA.
+     * Both input DIAs must be used sorted conforming to the given comparator. 
+     * The type of the output DIA will be the type of this DIA. 
+     *
+     * The merge operation balances all input data, so that each worker will 
+     * have an equal number of elements when the merge completes. 
+     *
+     * \tparam Comparator Comparator to specify the order of input and output. 
+     *
+     * \param comparator Comparator to specify the order of input and output. 
+     *
+     * \param second_dia DIA, which is merged with this DIA. 
      */
-    template <typename SecondDIA, typename Comperator = std::less<ValueType> >
-    auto Merge(SecondDIA second_dia, const Comperator& comperator = Comperator()) const;
+    template <typename SecondDIA, typename Comparator = std::less<ValueType> >
+    auto Merge(SecondDIA second_dia, const Comparator& comparator = Comparator()) const;
 
     /*!
      * PrefixSum is a DOp, which computes the prefix sum of all elements. The sum
