726040ae6ef32b3b89f6a78ece8701908aaa3d76
Timo Bingmann <tbgit@panthema.net>
2016-01-19 22:33:30 +0100

    switching cdash to panthema.net

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,12 +10,12 @@
 # All rights reserved. Published under the BSD-2 license in the LICENSE file.
 ################################################################################
 
-set(CTEST_PROJECT_NAME "thrill")
+set(CTEST_PROJECT_NAME "Thrill")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=thrill")
+set(CTEST_DROP_SITE "panthema.net")
+set(CTEST_DROP_LOCATION "/cdash-12/submit.php?project=Thrill")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 ################################################################################
