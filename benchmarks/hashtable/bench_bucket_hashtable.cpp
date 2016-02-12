31394bab03c4a27feee9f8179a938cb54cde700e
Timo Bingmann <tbgit@panthema.net>
2016-02-12 13:36:42 +0100

    api: using new ReduceToIndexPostStage in ReduceToIndex node

--- benchmarks/hashtable/bench_bucket_hashtable.cpp
+++ benchmarks/hashtable/bench_bucket_hashtable.cpp
@@ -109,7 +109,7 @@ int main(int argc, char* argv[]) {
                  table(ctx,
                        workers, key_ex, red_fn, writers,
                        core::ReduceByHashKey<size_t>(),
-                       0, 0, byte_size,
+                       0, byte_size,
                        bucket_rate, max_partition_fill_rate, std::equal_to<size_t>());
 
                  common::StatsTimer<true> timer(true);
