19cc4f8534f87eeddb24ccbfde9f8a9e4b22753a
Timo Bingmann <tbgit@panthema.net>
2016-02-14 23:30:42 +0100

    examples: moving all page_rank things into examples/page_rank

--- examples/page_rank/page_rank_run.cpp
+++ examples/page_rank/page_rank_run.cpp
@@ -1,41 +1,22 @@
 /*******************************************************************************
- * examples/run/page_rank.cpp
+ * examples/page_rank/page_rank_run.cpp
  *
  * Part of Project Thrill - http://project-thrill.org
  *
+ * Copyright (C) 2016 Timo Bingmann <tb@panthema.net>
  *
  * All rights reserved. Published under the BSD-2 license in the LICENSE file.
  ******************************************************************************/
 
-#include <examples/page_rank.hpp>
-#include <thrill/api/cache.hpp>
-#include <thrill/api/collapse.hpp>
-#include <thrill/api/generate.hpp>
-#include <thrill/api/groupby_index.hpp>
+#include <examples/page_rank/page_rank.hpp>
+
 #include <thrill/api/read_lines.hpp>
-#include <thrill/api/reduce.hpp>
-#include <thrill/api/reduce_to_index.hpp>
-#include <thrill/api/size.hpp>
-#include <thrill/api/sort.hpp>
-#include <thrill/api/sum.hpp>
 #include <thrill/api/write_lines.hpp>
-#include <thrill/api/zip.hpp>
 #include <thrill/common/cmdline_parser.hpp>
 #include <thrill/common/logger.hpp>
 #include <thrill/common/stats_timer.hpp>
 
-#include <iostream>
-#include <iterator>
-#include <random>
-#include <sstream>
 #include <string>
-#include <thread>
-#include <tuple>
-#include <utility>
-#include <vector>
-
-using thrill::DIA;
-using thrill::Context;
 
 using namespace thrill; // NOLINT
 
@@ -66,6 +47,8 @@ int main(int argc, char* argv[]) {
         [&input, &output, &iter](api::Context& ctx) {
             ctx.enable_consume(false);
 
+            thrill::common::StatsTimer<true> timer;
+
             // read input file and create links in this format:
             //
             // url linked_url
@@ -78,16 +61,18 @@ int main(int argc, char* argv[]) {
 
             res.WriteLines(output);
 
+            timer.Stop();
+
             auto number_edges = in.Size();
             LOG1 << "\n"
                  << "FINISHED PAGERANK COMPUTATION"
                  << "\n"
                  << std::left << std::setfill(' ')
-                //                 << std::setw(10) << "#nodes: " << number_nodes
-                //                 << "\n"
                  << std::setw(10) << "#edges: " << number_edges
                  << "\n"
-                 << std::setw(10) << "#iter: " << iter;
+                 << std::setw(10) << "#iter: " << iter
+                 << "\n"
+                 << std::setw(10) << "time: " << timer.Milliseconds() << "ms";
 
             ctx.stats_graph().BuildLayout("pagerank.out");
         };
