20637e1710519de9d800071077de3729f0fc20e0
Timo Bingmann <tbgit@panthema.net>
2016-02-15 15:51:32 +0100

    page_rank: adding Zipf graph generator

---
+++ examples/page_rank/zipf_graph_gen.cpp
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * examples/page_rank/zipf_graph_gen.cpp
+ *
+ * A simple graph generator for the PageRank benchmark inspired by HiBench's
+ * generator. The number of outgoing links of each page is Gaussian distributed,
+ * by default with mean 50 and variance 10, and the link targets themselves
+ * follow a Zipf-Mandelbrot distribution with very small scale parameter, such
+ * that the pages with low id numbers have a slightly higher probability than
+ * the rest.
+ *
+ * Part of Project Thrill - http://project-thrill.org
+ *
+ * Copyright (C) 2016 Timo Bingmann <tb@panthema.net>
+ *
+ * All rights reserved. Published under the BSD-2 license in the LICENSE file.
+ ******************************************************************************/
+
+#include <examples/page_rank/zipf_graph_gen.hpp>
+
+#include <thrill/common/cmdline_parser.hpp>
+#include <thrill/common/logger.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace thrill; // NOLINT
+
+int main(int argc, char* argv[]) {
+
+    common::CmdlineParser clp;
+
+    clp.SetVerboseProcess(false);
+
+    // Graph Generator
+    ZipfGraphGen gg(1);
+
+    uint64_t pages;
+    clp.AddParamBytes("pages", pages, "number of pages");
+
+    bool group = false;
+    clp.AddFlag('g', "group", group, "group outgoing links");
+
+    clp.AddSizeT('m', "size_mean", gg.size_mean,
+                 "mean of number of outgoing links, default: "
+                 + std::to_string(gg.size_mean));
+
+    clp.AddDouble(0, "size_var", gg.size_var,
+                  "variance of number of outgoing links, default: "
+                  + std::to_string(gg.size_var));
+
+    clp.AddDouble(0, "link_scale", gg.link_zipf_scale,
+                  "Zipf scale parameter for outgoing links, default: "
+                  + std::to_string(gg.link_zipf_scale));
+
+    clp.AddDouble(0, "link_exponent", gg.link_zipf_exponent,
+                  "Zipf exponent parameter for outgoing links, default: "
+                  + std::to_string(gg.link_zipf_exponent));
+
+    if (!clp.Process(argc, argv)) {
+        return -1;
+    }
+
+    // reinitialize graph generator with parameters from the command line.
+    gg.Initialize(pages);
+
+    //! underlying random number generator
+    std::default_random_engine rng(std::random_device { } ());
+
+    for (size_t p = 0; p < pages; ++p)
+    {
+        std::vector<size_t> result = gg.GenerateOutgoing(rng);
+        if (group) {
+            for (size_t i = 0; i < result.size(); ++i) {
+                if (i != 0) std::cout << ' ';
+                std::cout << result[i];
+            }
+            std::cout << '\n';
+        }
+        else {
+            for (const size_t& out : result) {
+                std::cout << p << '\t' << out << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
+
+/******************************************************************************/
---
+++ examples/page_rank/zipf_graph_gen.hpp
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * examples/page_rank/zipf_graph_gen.hpp
+ *
+ * A simple graph generator for the PageRank benchmark inspired by HiBench's
+ * generator. The number of outgoing links of each page is Gaussian distributed,
+ * by default with mean 50 and variance 10, and the link targets themselves
+ * follow a Zipf-Mandelbrot distribution with very small scale parameter, such
+ * that the pages with low id numbers have a slightly higher probability than
+ * the rest.
+ *
+ * Part of Project Thrill - http://project-thrill.org
+ *
+ * Copyright (C) 2016 Timo Bingmann <tb@panthema.net>
+ *
+ * All rights reserved. Published under the BSD-2 license in the LICENSE file.
+ ******************************************************************************/
+
+#pragma once
+#ifndef THRILL_EXAMPLES_PAGE_RANK_ZIPF_GRAPH_GEN_HEADER
+#define THRILL_EXAMPLES_PAGE_RANK_ZIPF_GRAPH_GEN_HEADER
+
+#include <thrill/common/string.hpp>
+#include <thrill/common/zipf_distribution.hpp>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+class ZipfGraphGen
+{
+public:
+    using ZipfDistribution = thrill::common::ZipfDistribution;
+
+    //! number of pages in graph
+    uint64_t pages;
+
+    //! Gaussian mean and variance of content length
+    size_t size_mean = 50;
+    double size_var = 10;
+
+    //! Zipf distribution scale and exponent for generating outgoing links over
+    //! the page number universe.
+    double link_zipf_scale = 0.3;
+    double link_zipf_exponent = 0.5;
+
+    explicit ZipfGraphGen(uint64_t _pages)
+        : pages(_pages) { Initialize(); }
+
+    //! reinitialize the random generator if parameters were changed.
+    void Initialize(uint64_t _pages) {
+        pages = _pages;
+
+        content_length_dist_ = std::normal_distribution<double>(
+            size_mean, size_var);
+
+        link_zipf_ = ZipfDistribution(
+            pages, link_zipf_scale, link_zipf_exponent);
+    }
+
+    //! reinitialize the random generator if parameters were changed.
+    void Initialize() {
+        return Initialize(pages);
+    }
+
+    template <typename Generator>
+    std::vector<size_t> GenerateOutgoing(Generator& rng) {
+        double dsize = content_length_dist_(rng);
+        if (dsize < 0) dsize = 0;
+
+        size_t size = std::round(dsize);
+
+        std::vector<size_t> result;
+        result.reserve(size);
+
+        for (size_t i = 0; i < size; ++i) {
+            result.emplace_back(link_zipf_(rng) - 1);
+        }
+        std::sort(result.begin(), result.end());
+        return result;
+    }
+
+private:
+    //! Gaussian random variable for content length of a page
+    std::normal_distribution<double> content_length_dist_;
+
+    //! Zipf random variable for outgoing links.
+    ZipfDistribution link_zipf_;
+};
+
+#endif // !THRILL_EXAMPLES_PAGE_RANK_ZIPF_GRAPH_GEN_HEADER
+
+/******************************************************************************/
