b355d80f89f5f2c69e951d0fed310af8063c8ebe
Timo Bingmann <tbgit@panthema.net>
2016-02-21 14:41:06 +0100

    enhancing to compile.sh to find compiler and number of cores

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@
 #
 # Part of Project Thrill - http://project-thrill.org
 #
+# Copyright (C) 2016 Timo Bingmann <tb@panthema.net>
 #
 # All rights reserved. Published under the BSD-2 license in the LICENSE file.
 ################################################################################
@@ -13,11 +14,25 @@ set -e
 git submodule init
 git submodule update
 
+CMAKE_OPTS=
+
+# try to find a modern C++ compiler
+for CMD in g++-5 g++-5.3.0 g++-5.2.0 g++-5.1.0 g++-4.9 g++-4.9.3; do
+    if command -v "$CMD" > /dev/null; then
+        CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_CXX_COMPILER=$CMD"
+        break
+    fi
+done
+
+# detect number of cores
+CORES=$(grep -c ^processor /proc/cpuinfo)
+MAKEOPTS=-j$CORES
+
 if [ ! -d "build" ]; then
     mkdir build
 fi
 cd build
-cmake .. $@
+cmake .. $CMAKE_OPTS $@
 make $MAKEOPTS
 ctest -V
 
