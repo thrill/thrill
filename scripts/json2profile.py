0f5a1761a708ece04c2e7ba83be279d674796b85
Timo Bingmann <tbgit@panthema.net>
2016-03-29 14:48:30 +0200

    scripts: fixing stream summary tables, pan chart with shift.

--- scripts/json2profile.py
+++ scripts/json2profile.py
@@ -37,9 +37,13 @@ def flatten_dicts(d, parent_key='', sep='_'):
 for input_file in sys.argv[1:]:
     with open(input_file, 'r') as json_file:
         for line in json_file:
-            r = json.loads(line)
-            if 'class' in r:
-                stats.append(flatten_dicts(r))
+            try:
+                r = json.loads(line)
+                if 'class' in r:
+                    stats.append(flatten_dicts(r))
+            except ValueError:
+                print("JSON line invalid: " + line)
+                pass
 
 print("Read " + str(len(stats)) + " json events from " + str(len(sys.argv) - 1) + " files")
 
@@ -95,7 +99,7 @@ def xy_aggregated(df):
 # generate table of DIAs
 def dia_table():
     df = df_stats
-    df = df[(df['class'] == 'DIABase') & (df['event'] == 'create') & (df['host_rank'] == 0)]
+    df = df[(df['class'] == 'DIABase') & (df['event'] == 'create') & (df['worker_rank'] == 0)]
     df = df[['id','label','type','parents']]
     df['id'] = df['id'].astype(int)
     df['label_id'] = df['label'] + "." + df['id'].map(str)
@@ -121,6 +125,26 @@ def stream_table():
 def stream_html_table():
     return stream_table().to_html()
 
+# generate table of Stream summary statistics
+def stream_summary_table():
+    df = df_stats
+    df = df[(df['class'] == 'Stream') & (df['event'] == 'close')]
+    df = df[['ts','id','dia_id','host_rank','worker_rank','rx_bytes','tx_bytes']].set_index('ts')
+    df['id'] = df['id'].astype(int)
+    df['dia_id'] = df['dia_id'].astype(int)
+    df['host_rank'] = df['host_rank'].astype(int)
+    df['worker_rank'] = df['worker_rank'].astype(int)
+    df['rx_bytes'] = df['rx_bytes'].astype(int)
+    df['tx_bytes'] = df['tx_bytes'].astype(int)
+    df = df.sort_values(by=['id','dia_id','host_rank','worker_rank'])
+    df = df.merge(right=dia_table(), how='left', left_on='dia_id', right_index=True)
+    df = df[['id','label_id','rx_bytes','tx_bytes']]
+    df = df.groupby(['id','label_id']).aggregate(np.sum)
+    return df
+
+def stream_summary_html_table():
+    return stream_summary_table().to_html()
+
 # generate table of File statistics
 def file_table():
     df = df_stats
@@ -141,7 +165,7 @@ def file_table():
 def file_html_table():
     return file_table().to_html()
 
-print( file_table() )
+#print( file_table() )
 #sys.exit(0)
 
 ##########################################################################
@@ -150,7 +174,7 @@ print( file_table() )
 @app.route('/index')
 def index(chartID = 'chart_ID'):
     title = {'text': 'My Title'}
-    chart = {'renderTo': chartID, 'zoomType': 'x'}
+    chart = {'renderTo': chartID, 'zoomType': 'x', 'panning': 'true', 'panKey': 'shift'}
     xAxis = {'type': 'datetime', 'title': {'text': 'Execution Time'}}
     yAxis = [
         { 'title': { 'text': 'CPU Load (%)' } },
@@ -195,6 +219,7 @@ def index(chartID = 'chart_ID'):
         title=title, xAxis=xAxis, yAxis=yAxis,
         legend=legend, plotOptions=plotOptions,
         series=series,
+        stream_summary_table=stream_summary_html_table(),
         stream_table=stream_html_table(),
         file_table=file_html_table()
     )
