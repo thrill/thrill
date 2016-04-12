19556c85b3130080aeaca99a69f53bcb18a4d48c
Timo Bingmann <tbgit@panthema.net>
2016-04-12 14:58:36 +0200

    fixup UIntPair on mac's clang

--- thrill/common/uint_types.hpp
+++ thrill/common/uint_types.hpp
@@ -109,13 +109,21 @@ public:
             low_ = a, high_ = (High)high_max();
     }
 
-    //! construct from an uint64 (unsigned long long)
-    UIntPair(const uint64_t& a) // NOLINT
+    //! construct from an 64-bit unsigned integer
+    UIntPair(const unsigned long long& a) // NOLINT
         : low_((Low)(a & low_max())),
           high_((High)((a >> low_bits) & high_max())) {
         // check for overflow
         assert((a >> (low_bits + high_bits)) == 0);
     }
+
+    //! construct from an 64-bit signed integer
+    UIntPair(const unsigned long& a) // NOLINT
+        : UIntPair(static_cast<unsigned long long>(a)) { }
+
+    //! construct from an 64-bit signed integer
+    UIntPair(const int64_t& a)       // NOLINT
+        : UIntPair(static_cast<unsigned long long>(a)) { }
 
     //! copy assignment operator
     UIntPair& operator = (const UIntPair&) = default;
