593b909406c2efdefbcbad539442eff9d9a65795
Alexander Noe <aleexnoe@gmail.com>
2016-06-23 13:01:03 +0200

    core: move GolombReader to own file

--- thrill/core/location_detection.hpp
+++ thrill/core/location_detection.hpp
@@ -5,7 +5,7 @@
  *
  * Part of Project Thrill - http://project-thrill.org
  *
- * Copyright (C) 2015 Alexander Noe <aleexnoe@gmail.com>
+ * Copyright (C) 2016 Alexander Noe <aleexnoe@gmail.com>
  *
  * All rights reserved. Published under the BSD-2 license in the LICENSE file.
  ******************************************************************************/
@@ -17,6 +17,7 @@
 #include <thrill/common/function_traits.hpp>
 #include <thrill/common/logger.hpp>
 #include <thrill/core/duplicate_detection.hpp>
+#include <thrill/core/golomb_reader.hpp>
 #include <thrill/core/multiway_merge.hpp>
 #include <thrill/core/reduce_functional.hpp>
 #include <thrill/core/reduce_table.hpp>
@@ -64,45 +65,6 @@ public:
 	std::vector<HashCounterPair>& vec_;
 	size_t modulo_ = 1;
 	HashFunction hash_function_;
-};
-
-template <typename CounterType>
-class GolombReader 
-{
-public:
-	GolombReader(size_t data_size,
-				 size_t* raw_data,
-				 size_t num_elements,
-				 size_t b,
-				 size_t bitsize):
-		golomb_code(raw_data,
-					common::IntegerDivRoundUp(data_size,
-											  sizeof(size_t)),
-					b, num_elements),
-		num_elements_(num_elements),
-		returned_elements_(0),
-		delta_(0),
-		bitsize_(bitsize) { }
-
-	bool HasNext() {
-		return returned_elements_ < num_elements_;
-	}
-
-	template <typename T> 
-	T Next() {
-		size_t new_element = golomb_code.golomb_out() + delta_;
-		delta_ = new_element;
-		CounterType ctr = golomb_code.stream_out(bitsize_);
-		returned_elements_++;
-		return std::make_pair(new_element, ctr);
-	}
-
-private:
-    DynamicBitset<size_t> golomb_code;
-	size_t num_elements_;
-	size_t returned_elements_;
-	size_t delta_;
-	size_t bitsize_;
 };
 
 template <typename ValueType, typename Key, bool UseLocationDetection,
