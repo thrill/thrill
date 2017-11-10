7af1df250e8b39e74e73fcbd6152e2d310911707
Timo Bingmann <tbgit@panthema.net>
2017-11-10 19:57:16 +0100

    logger: partially reverting "thread_local variables instead of a std::map" because APPLE does not support it.

--- thrill/common/logger.cpp
+++ thrill/common/logger.cpp
@@ -30,6 +30,8 @@ namespace common {
 //! memory manager singleton for Logger
 mem::Manager g_logger_mem_manager(nullptr, "Logger");
 
+#if !__APPLE__
+
 //! thread name
 static thread_local mem::by_string s_thread_name;
 
@@ -55,6 +57,51 @@ void FormatNameForThisThread(std::ostream& os) {
     os << std::setfill('0') << std::setw(6) << s_message_counter++;
     os.flags(flags);
 }
+
+#else
+
+// old std::map implementation, because APPLE does not support thread_local
+
+using StringCount = std::pair<mem::by_string, size_t>;
+
+template <typename Type>
+using LoggerAllocator = mem::FixedAllocator<Type, g_logger_mem_manager>;
+
+//! deque without malloc tracking
+template <typename Key, typename T, typename Compare = std::less<Key> >
+using logger_map = std::map<Key, T, Compare,
+                            LoggerAllocator<std::pair<const Key, T> > >;
+
+//! mutex for s_threads map
+static std::mutex s_mutex;
+
+//! map thread id -> (name, message counter)
+static logger_map<std::thread::id, StringCount> s_threads;
+
+//! Defines a name for the current thread, only if no name was set previously
+void NameThisThread(const mem::by_string& name) {
+    std::lock_guard<std::mutex> lock(s_mutex);
+    s_threads[std::this_thread::get_id()] = StringCount(name, 0);
+}
+
+//! Outputs the name of the current thread or 'unknown [id]'
+void FormatNameForThisThread(std::ostream& os) {
+    std::lock_guard<std::mutex> lock(s_mutex);
+
+    auto it = s_threads.find(std::this_thread::get_id());
+    if (it != s_threads.end()) {
+        StringCount& sc = it->second;
+        std::ios::fmtflags flags(os.flags());
+        os << sc.first << ' '
+           << std::setfill('0') << std::setw(6) << sc.second++;
+        os.flags(flags);
+    }
+    else {
+        os << "unknown " << std::this_thread::get_id();
+    }
+}
+
+#endif
 
 //! Returns the name of the current thread or 'unknown [id]'
 std::string GetNameForThisThread() {
