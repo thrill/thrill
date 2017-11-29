4cf871c8b166e0a920af74d5ba3f01a6f61c9633
Lorenz Hübschle-Schneider <huebschle@kit.edu>
2017-11-29 16:06:23 +0100

    replace sampler with Algorithm P from Sanders, Lamm, et. al. 2017

--- thrill/api/sample.hpp
+++ thrill/api/sample.hpp
@@ -14,7 +14,11 @@
 
 #include <thrill/api/dia.hpp>
 #include <thrill/api/dop_node.hpp>
+#include <thrill/common/hypergeometric_distribution.hpp>
 #include <thrill/common/logger.hpp>
+#include <thrill/common/reservoir_sampling.hpp>
+
+#include <tlx/math.hpp>
 
 #include <algorithm>
 #include <vector>
@@ -37,95 +41,175 @@ public:
     template <typename ParentDIA>
     SampleNode(const ParentDIA& parent, size_t sample_size)
         : Super(parent.ctx(), "Sample", { parent.id() }, { parent.node() }),
-          sample_size_(sample_size)
+          sample_size_(sample_size), hyp_(42 /* dummy seed */),
+          parent_stack_empty_(ParentDIA::stack_empty)
     {
-        samples_.reserve(sample_size);
-
-        // Hook PreOp(s)
-        auto pre_op_fn = [this](const ValueType& input) {
-                             PreOp(input);
-                         };
-
-        auto lop_chain = parent.stack().push(pre_op_fn).fold();
+        auto save_fn = [this](const ValueType& input) {
+            writer_.Put(input);
+        };
+        auto lop_chain = parent.stack().push(save_fn).fold();
         parent.node()->AddChild(this, lop_chain);
     }
 
-    DIAMemUse PreOpMemUse() final {
-        return sample_size_ * sizeof(ValueType);
+    bool OnPreOpFile(const data::File& file, size_t /* parent_index */) final {
+        if (!parent_stack_empty_) {
+            LOGC(common::g_debug_push_file)
+                << "Sample rejected File from parent "
+                << "due to non-empty function stack.";
+            return false;
+        }
+        assert(file_.num_items() == 0);
+        file_ = file.Copy();
+        return true;
     }
 
-    void PreOp(const ValueType& input) {
-        if (samples_.size() < sample_size_) {
-            samples_.emplace_back(input);
-        }
-        else {
-            samples_[rng_() % samples_.size()] = input;
-        }
+    void StopPreOp(size_t /* id */) final {
+        // Push local elements to children
+        writer_.Close();
     }
 
     void Execute() final {
+        LOG << "SampleNode::Execute() processing";
+        const size_t local_size = file_.num_items();
+        sLOG << "local_size" << local_size;
 
-        size_t local_size = samples_.size();
+        const size_t my_rank = context_.my_rank();
+        const size_t num_workers = context_.num_workers();
 
-        size_t local_rank = local_size;
-        size_t global_size = context_.net.ExPrefixSumTotal(local_rank);
+        if (num_workers == 1) {
+            sample_size_ = std::min(sample_size_, local_size);
+            local_samples_ = sample_size_;
+            sLOG << "SampleNode::Execute (alone)"
+                 << local_samples_ << "of" << sample_size_ << "samples";
 
-        // not enough items to discard some, done.
-        if (global_size < sample_size_) return;
-
-        // synchronize global random generator
-        size_t seed = context_.my_rank() == 0 ? rng_() : 0;
-        seed = context_.net.Broadcast(seed);
-        rng_.seed(static_cast<unsigned>(seed));
-
-        // globally select random samples among samples_
-        typename std::vector<ValueType>::iterator it = samples_.begin();
-
-        for (size_t i = 0; i < sample_size_; ++i) {
-            size_t r = rng_() % sample_size_;
-            if (r < local_rank || r >= local_rank + local_size) continue;
-
-            // swap selected item to front.
-            using std::swap;
-            if (it < samples_.end()) {
-                swap(*it, samples_[r - local_rank]);
-            }
-            else {
-                it = samples_.insert(it, samples_[r - local_rank]);
-            }
-            ++it;
+            return;
         }
 
-        samples_.erase(it, samples_.end());
+        std::vector<size_t> input_sizes(num_workers);
+        input_sizes[my_rank] = local_size;
+        // there's no group::all_gather... do it totally naively
+        for (size_t i = 0; i < num_workers; ++i) {
+            input_sizes[i] = context_.net.Broadcast(input_sizes[i], i);
+        }
+
+        // Determine and broadcast seed
+        size_t seed = 0;
+        if (context_.my_rank() == 0) {
+            seed = std::random_device{}();
+        }
+        seed = context_.net.Broadcast(seed, 0);
+        // Seed hypergeometric distribution with the same value on each PE
+        hyp_.seed(seed);
+
+        // Build size tree
+        const size_t num_leaves = tlx::round_up_to_power_of_two(num_workers);
+        std::vector<size_t> tree(2 * num_leaves - 1);
+        for (size_t i = 0; i < num_workers; ++i) {
+            tree[num_leaves + i - 1] = input_sizes[i];
+        }
+        const size_t height = tlx::integer_log2_ceil(num_workers);
+        for (size_t i = 0; i < height; ++i) {
+            const size_t min = num_leaves / (1ULL << (i + 1)) - 1;
+            for (size_t j = min; j <= 2 * min; ++j) {
+                tree[j] = tree[2 * j + 1] + tree[2 * j + 2];
+            }
+        }
+
+        LOG << "Input sizes: " << input_sizes;
+        LOG << "Tree: " << tree;
+
+        if (tree[0] < sample_size_) {
+            // more samples requested than there are elements
+            sample_size_ = tree[0];
+            local_samples_ = local_size;
+            sLOG << "SampleNode::Execute (underfull)"
+                 << local_samples_ << "of" << sample_size_ << "samples";
+            return;
+        }
+
+        size_t total = sample_size_, base = 1;
+        for (size_t i = 0; i < height; ++i) {
+            // get hypergeometric deviate. the sequence of calls is exactly the
+            // same up to two workers' LCA in the binary tree above all workers,
+            // i.e. until they no longer depend on each other
+            hyp_.seed(seed + base);
+            size_t left = hyp_(tree[base], tree[base + 1], total);
+            // descend into correct branch
+            const bool go_right = my_rank & (1ULL << (height - i - 1));
+
+            sLOG << "Level" << i << "distributing" << total << "samples,"
+                 << "base index:" << base
+                 << "left size:" << tree[base] << "right:" << tree[base + 1]
+                 << "result for left:" << left
+                 << "going" << (go_right ? "right" : "left");
+
+            if (go_right) {
+                base = 2 * base + 3;
+                total = total - left;
+            } else {
+                base = 2 * base + 1;
+                total = left;
+            }
+        }
+
+        local_samples_ = total;
+
+        if (local_samples_ > local_size) {
+            sLOG1 << "Sample error: need" << local_samples_ << ">"
+                  << local_size << "(=available) elements from this worker";
+        }
 
         sLOG << "SampleNode::Execute"
-             << "global_size" << global_size
-             << "local_rank" << local_rank
-             << "AllReduce" << context_.net.AllReduce(samples_.size());
-
-        assert(sample_size_ == context_.net.AllReduce(samples_.size()));
+             << local_samples_ << "of" << sample_size_ << "samples";
     }
 
     void PushData(bool consume) final {
-        for (const ValueType& v : samples_) {
+        samples_.clear();
+
+        const size_t local_size = file_.num_items();
+        if (local_samples_ == local_size) {
+            LOG << "Sample: returning all local items";
+            auto reader = file_.GetReader(consume);
+            while (reader.HasNext()) {
+                this->PushItem(reader.template Next<ValueType>());
+            }
+            return;
+        }
+
+        sLOG << "Drawing" << local_samples_ << "locally from" << local_size;
+        common::ReservoirSamplingFast<ValueType> sampler(
+            local_samples_, samples_, rng_);
+
+        auto reader = file_.GetReader(consume);
+        while (reader.HasNext()) {
+            sampler.add(reader.template Next<ValueType>());
+        }
+
+        for (const ValueType& v : sampler.samples()) {
             this->PushItem(v);
         }
-        if (consume)
-            std::vector<ValueType>().swap(samples_);
     }
 
     void Dispose() final {
-        std::vector<ValueType>().swap(samples_);
+        file_.Clear();
     }
 
 private:
-    size_t sample_size_;
-
+    //! Local data file
+    data::File file_ { context_.GetFile(this) };
+    //! Data writer to local file (only active in PreOp).
+    data::File::Writer writer_ { file_.GetWriter() };
+    //! number of samples to draw globally
+    size_t sample_size_, local_samples_;
     //! local samples
     std::vector<ValueType> samples_;
-
+    //! Hypergeometric distribution to calculate local sample sizes
+    common::hypergeometric hyp_;
     //! Random generator for eviction
     std::default_random_engine rng_ { std::random_device { } () };
+    //! Whether the parent stack is empty
+    const bool parent_stack_empty_;
+
 };
 
 template <typename ValueType, typename Stack>
