bea11a10289b9ac8ae09180eb51d924edfb3f211
Timo Bingmann <tbgit@panthema.net>
2018-05-29 16:41:56 +0200

    core: remove multiway_merge_attic.hpp

--- tests/core/multiway_merge_test.cpp
+++ tests/core/multiway_merge_test.cpp
@@ -12,7 +12,6 @@
 
 #include <thrill/common/function_traits.hpp>
 #include <thrill/core/multiway_merge.hpp>
-#include <thrill/core/multiway_merge_attic.hpp>
 #include <thrill/data/file.hpp>
 
 #include <thrill/common/logger.hpp>
@@ -30,51 +29,6 @@ using namespace thrill; // NOLINT
 struct MultiwayMerge : public ::testing::Test {
     data::BlockPool block_pool_;
 };
-
-TEST_F(MultiwayMerge, Basic) {
-    // static constexpr bool debug = false;
-    std::mt19937 gen(0);
-    size_t a = 2;
-    size_t b = 5;
-    size_t total = 2 * 5;
-
-    using iterator = std::vector<size_t>::iterator;
-    std::vector<std::vector<size_t> > in;
-    std::vector<size_t> ref;
-    std::vector<size_t> output;
-    std::vector<std::pair<iterator, iterator> > seq;
-
-    in.reserve(a);
-    ref.reserve(total);
-    seq.reserve(a);
-    output.resize(total);
-
-    for (size_t i = 0; i < a; ++i) {
-        std::vector<size_t> tmp;
-        tmp.reserve(b);
-        for (size_t j = 0; j < b; ++j) {
-            auto elem = gen() % 10;
-            tmp.push_back(elem);
-            ref.push_back(elem);
-        }
-        std::sort(tmp.begin(), tmp.end());
-        in.push_back(tmp);
-    }
-
-    for (auto& vec : in) {
-        seq.push_back(std::make_pair(vec.begin(), vec.end()));
-    }
-
-    std::sort(ref.begin(), ref.end());
-    core::sequential_multiway_merge<true, false>(seq.begin(),
-                                                 seq.end(),
-                                                 output.begin(),
-                                                 total,
-                                                 std::less<size_t>());
-    for (size_t i = 0; i < total; ++i) {
-        ASSERT_EQ(ref[i], output[i]);
-    }
-}
 
 TEST_F(MultiwayMerge, GetMultiwayMergePuller) {
     static constexpr bool debug = false;
